4ee79d963b8393f70a3e1ecbe42187007d8bc4f4
Dessalines <happydooby@gmail.com>
2019-04-06 16:32:11 -0700

    Adding Favicon.

--- ui/fuse.js
+++ ui/fuse.js
@@ -43,7 +43,7 @@ Sparky.task('config', _ => {
 });
 Sparky.task('clean', _ => Sparky.src('dist/').clean('dist/'));
 Sparky.task('env', _ => (isProduction = true));
-Sparky.task('copy-assets', () => Sparky.src('assets/*.ico').dest('dist/'));
+Sparky.task('copy-assets', () => Sparky.src('assets/*.svg').dest('dist/'));
 Sparky.task('dev', ['clean', 'config', 'copy-assets'], _ => {
   fuse.dev();
   app.hmr().watch();
