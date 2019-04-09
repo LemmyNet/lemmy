dae66517aab83b55141004e1152ffe7ba30c7c5b
Dessalines <happydooby@gmail.com>
2019-04-08 20:28:07 -0700

    Adding versioning to front page.
    - Fixes #51

---
+++ ui/set_version.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+exports.setVersion =  function() {
+  let revision = require('child_process')
+    .execSync('git describe --tags --long')
+    .toString().trim();
+  let line = `export let version: string = "${revision}";`;
+  fs.writeFileSync("./src/version.ts", line);
+}
--- ui/src/components/moment-time.tsx
+++ ui/src/components/moment-time.tsx
@@ -17,11 +17,11 @@ export class MomentTime extends Component<MomentTimeProps, any> {
   render() {
     if (this.props.data.updated) {
       return (
-        <span className="font-italics">modified {moment.utc(this.props.data.updated).fromNow()}</span>
+        <span title={this.props.data.updated} className="font-italics">modified {moment.utc(this.props.data.updated).fromNow()}</span>
       )
     } else {
       return (
-        <span>{moment.utc(this.props.data.published).fromNow()}</span>
+        <span title={this.props.data.published}>{moment.utc(this.props.data.published).fromNow()}</span>
       )
     }
   }
