85d16e8632f7cf44100d75cde4d89bfe986f4cbc
Dessalines <happydooby@gmail.com>
2019-04-09 08:03:58 -0700

    Fixing endpoint.

--- ui/src/env.ts
+++ ui/src/env.ts
@@ -1,2 +1,4 @@
-export const endpoint = `${window.location.hostname}:8536`;
+let host = `${window.location.hostname}`;
+let port = `${window.location.port == "4444" ? '8536' : window.location.port}`;
+let endpoint = `${host}:${port}`;
 export let wsUri = `${(window.location.protocol=='https:') ? 'wss://' : 'ws://'}${endpoint}/service/ws`;
