4ab28c01c6cf0ee376ec3f8a1423f1ea42b94589
Dessalines <tyhou13@gmx.com>
2019-04-18 09:23:19 -0700

    Showing how to do live update developing in the install script.

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@ yarn
 yarn build
 cd ../server
 cargo run
+
+# For live coding, where both the front and back end, automagically reload on any save, do:
+# cd ui && yarn start
+# cd server && cargo watch -x run
