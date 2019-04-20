760324c845af86af23090ac3aeabdd6dce5a466a
Dessalines <happydooby@gmail.com>
2019-04-20 13:23:42 -0700

    Adding version to footer

--- ui/src/components/footer.tsx
+++ ui/src/components/footer.tsx
@@ -16,6 +16,9 @@ export class Footer extends Component<any, any> {
         <div className="navbar-collapse">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
+              <span class="navbar-text">{version}</span>
+            </li>
+            <li class="nav-item">
               <Link class="nav-link" to="/modlog">Modlog</Link>
             </li>
             <li class="nav-item">
