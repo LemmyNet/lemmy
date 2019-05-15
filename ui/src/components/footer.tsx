13d7adab94b22fd0c6b166e49ea4be87d7b3b343
Dessalines <tyhou13@gmx.com>
2019-05-15 09:12:54 -0700

    Adding API to footer

--- ui/src/components/footer.tsx
+++ ui/src/components/footer.tsx
@@ -12,7 +12,7 @@ export class Footer extends Component<any, any> {
 
   render() {
     return (
-      <nav title={version} class="container navbar navbar-expand-md navbar-light navbar-bg p-0 px-3 my-2">
+      <nav class="container navbar navbar-expand-md navbar-light navbar-bg p-0 px-3 my-2">
         <div className="navbar-collapse">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
@@ -22,13 +22,13 @@ export class Footer extends Component<any, any> {
               <Link class="nav-link" to="/modlog">Modlog</Link>
             </li>
             <li class="nav-item">
+              <a class="nav-link" href={`${repoUrl}/blob/master/docs/api.md`}>API</a>
+            </li>
+            <li class="nav-item">
               <Link class="nav-link" to="/sponsors">Sponsors</Link>
             </li>
             <li class="nav-item">
               <a class="nav-link" href={repoUrl}>Code</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href={repoUrl}>About</a>
             </li>
           </ul>
         </div>
