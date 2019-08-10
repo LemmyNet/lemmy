4c978d74f96f6d2784a759e260b219cb9149a265
Dessalines <tyhou13@gmx.com>
2019-08-10 11:33:54 -0700

    Adding the imports.

--- ui/src/components/moment-time.tsx
+++ ui/src/components/moment-time.tsx
@@ -2,6 +2,7 @@ import { Component } from 'inferno';
 import * as moment from 'moment';
 // import 'moment/locale/de';
 import 'moment/locale/zh-cn';
+import 'moment/locale/fr';
 import { getLanguage } from '../utils';
 import { i18n } from '../i18next';
 
