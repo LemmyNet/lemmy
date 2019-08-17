d0a9cef72e81a68ad0603b83ed3ebeddce411240
Dessalines <happydooby@gmail.com>
2019-08-17 13:32:59 -0700

    Adding more settings for german.

--- ui/src/components/moment-time.tsx
+++ ui/src/components/moment-time.tsx
@@ -1,6 +1,6 @@
 import { Component } from 'inferno';
 import * as moment from 'moment';
-// import 'moment/locale/de';
+import 'moment/locale/de';
 import 'moment/locale/zh-cn';
 import 'moment/locale/fr';
 import 'moment/locale/sv';
