98b8793dece17483e08f166dfd4b248731ac8017
Dessalines <tyhou13@gmx.com>
2019-08-17 12:15:53 -0700

    Fixing navbar icon position.

--- ui/src/components/navbar.tsx
+++ ui/src/components/navbar.tsx
@@ -103,7 +103,7 @@ export class Navbar extends Component<any, NavbarState> {
             <>
               {
                 <li className="nav-item">
-                  <Link class="inbox nav-link" to="/inbox">
+                  <Link class="nav-link" to="/inbox">
                     <svg class="icon"><use xlinkHref="#icon-mail"></use></svg>
                     {this.state.unreadCount> 0 && <span class="ml-1 badge badge-light">{this.state.unreadCount}</span>}
                   </Link>
