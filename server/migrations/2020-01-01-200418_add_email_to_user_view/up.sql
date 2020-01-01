b63aabfdc26a85c91f6206505fed5b6f71d80158
Dessalines <happydooby@gmail.com>
2020-01-01 15:46:14 -0500

    Adding change password and email address from user settings.
    - Fixes #384
    - Fixes #385

---
+++ server/migrations/2020-01-01-200418_add_email_to_user_view/up.sql
@@ -0,0 +1,16 @@
+-- user
+drop view user_view;
+create view user_view as 
+select id,
+name,
+avatar,
+email,
+fedi_name,
+admin,
+banned,
+published,
+(select count(*) from post p where p.creator_id = u.id) as number_of_posts,
+(select coalesce(sum(score), 0) from post p, post_like pl where u.id = p.creator_id and p.id = pl.post_id) as post_score,
+(select count(*) from comment c where c.creator_id = u.id) as number_of_comments,
+(select coalesce(sum(score), 0) from comment c, comment_like cl where u.id = c.creator_id and c.id = cl.comment_id) as comment_score
+from user_ u;
