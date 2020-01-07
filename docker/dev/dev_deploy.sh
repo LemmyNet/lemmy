6f2f9995f20ff26245eeaa1b310710e3bcab529d
Dessalines <tyhou13@gmx.com>
2020-01-07 12:53:32 -0500

    Adding ssh docker commands to dev deploy.

--- docker/dev/dev_deploy.sh
+++ docker/dev/dev_deploy.sh
@@ -7,3 +7,6 @@ git checkout dev
 docker-compose build
 docker tag dev_lemmy:latest dessalines/lemmy:dev
 docker push dessalines/lemmy:dev
+
+# SSH and pull it
+ssh tyler@45.55.175.59 "cd ~/git/lemmy/docker/dev && docker pull dessalines/lemmy:dev && docker-compose up -d"
