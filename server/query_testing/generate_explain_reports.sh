253f4aa6a1f3d7d8e6477addd4331e5eb7118d28
Dessalines <tyhou13@gmx.com>
2020-01-11 17:30:44 -0500

    Back to just indexes.

--- server/query_testing/generate_explain_reports.sh
+++ server/query_testing/generate_explain_reports.sh
@@ -14,6 +14,9 @@ psql -qAt -U lemmy -f explain.sql > comment_view.json
 echo "explain (analyze, format json) select * from community_view where user_id is null order by hot_rank desc" > explain.sql
 psql -qAt -U lemmy -f explain.sql > community_view.json
 
+echo "explain (analyze, format json) select * from site_view limit 1" > explain.sql
+psql -qAt -U lemmy -f explain.sql > site_view.json
+
 grep "Execution Time" *.json
 
 rm explain.sql
