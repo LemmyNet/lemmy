3b8a2f61fc503829e9f5a734e718d80c6ac5a90d
Dessalines <happydooby@gmail.com>
2020-01-19 16:23:04 -0500

    Automatically update translation report. Fixes #439

--- ui/translation_report.ts
+++ ui/translation_report.ts
@@ -8,31 +8,50 @@ import { sv } from './src/translations/sv';
 import { ru } from './src/translations/ru';
 import { nl } from './src/translations/nl';
 import { it } from './src/translations/it';
+import fs from 'fs';
 
-let files = [
-  { t: de, n: 'de' },
-  { t: eo, n: 'eo' },
-  { t: es, n: 'es' },
-  { t: fr, n: 'fr' },
-  { t: it, n: 'it' },
-  { t: nl, n: 'nl' },
-  { t: ru, n: 'ru' },
-  { t: sv, n: 'sv' },
-  { t: zh, n: 'zh' },
-];
-let masterKeys = Object.keys(en.translation);
+let readmePath = '../README.md';
 
-let report = 'lang | done | missing\n';
-report += '--- | --- | ---\n';
+let open = '<!-- translations -->';
+let close = '<!-- translationsstop -->';
 
-for (let file of files) {
-  let keys = Object.keys(file.t.translation);
-  let pct: number = (keys.length / masterKeys.length) * 100;
-  let missing = difference(masterKeys, keys);
-  report += `${file.n} | ${pct.toFixed(0)}% | ${missing} \n`;
+let readmeTxt = fs.readFileSync(readmePath, { encoding: 'utf8' });
+
+let before = readmeTxt.split(open)[0];
+let after = readmeTxt.split(close)[1];
+
+let report = buildReport();
+
+let alteredReadmeTxt = `${before}${open}\n\n${report}\n${close}${after}`;
+
+fs.writeFileSync(readmePath, alteredReadmeTxt);
+
+function buildReport(): string {
+  let files = [
+    { t: de, n: 'de' },
+    { t: eo, n: 'eo' },
+    { t: es, n: 'es' },
+    { t: fr, n: 'fr' },
+    { t: it, n: 'it' },
+    { t: nl, n: 'nl' },
+    { t: ru, n: 'ru' },
+    { t: sv, n: 'sv' },
+    { t: zh, n: 'zh' },
+  ];
+  let masterKeys = Object.keys(en.translation);
+
+  let report = 'lang | done | missing\n';
+  report += '--- | --- | ---\n';
+
+  for (let file of files) {
+    let keys = Object.keys(file.t.translation);
+    let pct: number = (keys.length / masterKeys.length) * 100;
+    let missing = difference(masterKeys, keys);
+    report += `${file.n} | ${pct.toFixed(0)}% | ${missing} \n`;
+  }
+
+  return report;
 }
-
-console.log(report);
 
 function difference(a: Array<string>, b: Array<string>): Array<string> {
   return a.filter(x => !b.includes(x));
