8ad119f72ce38c8a04f39f42523ec7f8f11f9c96
Richie Zhang <12566991+StaticallyTypedRice@users.noreply.github.com>
2020-01-29 15:57:32 -0800

    Use "nicht speichern" for "unsave" in German.

--- ui/src/translations/de.ts
+++ ui/src/translations/de.ts
@@ -66,7 +66,7 @@ export const de = {
     unban_from_site: 'Von der Seite entbannen',
     banned: 'gesperrt',
     save: 'speichern',
-    unsave: 'rückgängig machen speichern',
+    unsave: 'nicht speichern',
     create: 'anlegen',
     creator: 'Ersteller',
     username: 'Benutzername',
