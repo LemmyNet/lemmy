8d4ab4555cbf026ea2c34ea4800f72dcd1fe4c8e
Richie Zhang <12566991+StaticallyTypedRice@users.noreply.github.com>
2020-05-01 07:33:28 -0700

    Improve the yes/no prompt in install.sh (#683)
    * Improve the yes/no prompt in install.sh
    
    The old implementation used a feature only present on Bash version 4, which not all systems have. The new version should work on all bash shells.
    
    * Update install.sh

--- install.sh
+++ install.sh
@@ -16,10 +16,10 @@ init_db_final=0
 while [ "$init_db_valid" == 0 ]
 do
   read -p "Initialize database (y/n)? " init_db
-  case "${init_db,,}" in
-    y|yes ) init_db_valid=1; init_db_final=1;;
-    n|no ) init_db_valid=1; init_db_final=0;;
-    * ) echo "Invalid input" 1>&2;;
+  case "$init_db" in
+    [yY]* ) init_db_valid=1; init_db_final=1;;
+    [nN]* ) init_db_valid=1; init_db_final=0;;
+    * ) echo "Invalid input. Please enter either \"y\" or \"n\"." 1>&2;;
   esac
   echo
 done
