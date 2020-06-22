234be6fb095d2cad1342a463c86ab032650915a7
Dessalines <tyhou13@gmx.com>
2020-06-22 17:52:39 -0400

    Changing to prod_lemmy:latest

--- docker/prod/deploy.sh
+++ docker/prod/deploy.sh
@@ -37,8 +37,8 @@ export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
 
 # Rebuilding docker
-docker build ../../ -f Dockerfile
-docker tag dev_lemmy:latest dessalines/lemmy:x64-$new_tag
+docker build ../../ -f Dockerfile --tag prod_lemmy:latest
+docker tag prod_lemmy:latest dessalines/lemmy:x64-$new_tag
 docker push dessalines/lemmy:x64-$new_tag
 
 # Build for Raspberry Pi / other archs
