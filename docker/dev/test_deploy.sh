0112d0d9551836c1a525203a8fef27c94e640405
Dessalines <tyhou13@gmx.com>
2020-06-28 13:44:42 -0400

    Fixing test deploy, adding a rate limiter to ansible nginx config.

--- docker/dev/test_deploy.sh
+++ docker/dev/test_deploy.sh
@@ -9,10 +9,10 @@ export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
 
 # Rebuilding dev docker
-sudo docker build . -f "docker/dev/Dockerfile" -t "dessalines/lemmy:$BRANCH"
+sudo docker build ../../ -f . -t "dessalines/lemmy:$BRANCH"
 sudo docker push "dessalines/lemmy:$BRANCH"
 
 # Run the playbook
-pushd ../lemmy-ansible
+pushd ../../../lemmy-ansible
 ansible-playbook -i test playbooks/site.yml
 popd
