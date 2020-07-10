181374d2cc5f751f0e41a8c4cb938cad118a5a4d
Dessalines <tyhou13@gmx.com>
2020-07-10 14:16:42 -0400

    Fixing travis, adding a test.sh script.

---
+++ server/test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export DATABASE_URL=postgres://lemmy:password@localhost:5432/lemmy
+diesel migration run
+export LEMMY_DATABASE_URL=postgres://lemmy:password@localhost:5432/lemmy
+RUST_TEST_THREADS=1 cargo test --workspace
