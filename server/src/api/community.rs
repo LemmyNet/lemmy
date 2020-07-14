ebba1c4409dd32bfbd99f704035c87ec1729e861
Ryexandra <RyRyRyex@protonmail.com>
2020-07-13 20:28:33 -0600

    prevent editing a community by a mod from transfering ownership to them

--- server/src/api/community.rs
+++ server/src/api/community.rs
@@ -5,14 +5,20 @@ use crate::{
   blocking,
   websocket::{
     server::{JoinCommunityRoom, SendCommunityRoomMessage},
-    UserOperation, WebsocketInfo,
+    UserOperation,
+    WebsocketInfo,
   },
   DbPool,
 };
 use lemmy_db::{naive_now, Bannable, Crud, Followable, Joinable, SortType};
 use lemmy_utils::{
-  generate_actor_keypair, is_valid_community_name, make_apub_endpoint, naive_from_unix, slur_check,
-  slurs_vec_to_str, EndpointType,
+  generate_actor_keypair,
+  is_valid_community_name,
+  make_apub_endpoint,
+  naive_from_unix,
+  slur_check,
+  slurs_vec_to_str,
+  EndpointType,
 };
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
@@ -386,7 +392,7 @@ impl Perform for Oper<EditCommunity> {
       title: data.title.to_owned(),
       description: data.description.to_owned(),
       category_id: data.category_id.to_owned(),
-      creator_id: user_id,
+      creator_id: read_community.creator_id,
       removed: data.removed.to_owned(),
       deleted: data.deleted.to_owned(),
       nsfw: data.nsfw,
