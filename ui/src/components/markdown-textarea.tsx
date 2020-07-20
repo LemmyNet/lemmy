68b934e36284d96999a865941bf1527a9d7c09c2
Coma Grayce <commagray@sunbutt.faith>
2020-07-21 02:40:01 +0300

    Add buttons.

--- ui/src/components/markdown-textarea.tsx
+++ ui/src/components/markdown-textarea.tsx
@@ -189,6 +189,24 @@ export class MarkdownTextArea extends Component<
             </button>
             <button
               class="btn btn-sm text-muted"
+              data-tippy-content={i18n.t('subscript')}
+              onClick={linkEvent(this, this.handleInsertSubscript)}
+            >
+              <svg class="icon icon-inline">
+                <use xlinkHref="#icon-subscript"></use>
+              </svg>
+            </button>
+            <button
+              class="btn btn-sm text-muted"
+              data-tippy-content={i18n.t('superscript')}
+              onClick={linkEvent(this, this.handleInsertSuperscript)}
+            >
+              <svg class="icon icon-inline">
+                <use xlinkHref="#icon-superscript"></use>
+              </svg>
+            </button>
+            <button
+              class="btn btn-sm text-muted"
               data-tippy-content={i18n.t('link')}
               onClick={linkEvent(this, this.handleInsertLink)}
             >
