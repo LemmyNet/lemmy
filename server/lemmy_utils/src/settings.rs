8ba9d5257422c7f33042b88339d5b184ddcf06ab
Dessalines <dessalines@users.noreply.github.com>
2020-07-22 20:17:41 -0400

    Fixing config_file env to use defaults instead (since its required). (#970)
    * Fixing config_file env to use defaults instead (since its required). Fixes #764
    
    * Changing the read / write to the custom config file one
    
    * Adding a config note.

--- server/lemmy_utils/src/settings.rs
+++ server/lemmy_utils/src/settings.rs
@@ -81,9 +81,9 @@ impl Settings {
   fn init() -> Result<Self, ConfigError> {
     let mut s = Config::new();
 
-    s.merge(File::with_name(CONFIG_FILE_DEFAULTS))?;
+    s.merge(File::with_name(&Self::get_config_defaults_location()))?;
 
-    s.merge(File::with_name(&Self::get_config_location()).required(false))?;
+    s.merge(File::with_name(CONFIG_FILE).required(false))?;
 
     // Add in settings from the environment (with a prefix of LEMMY)
     // Eg.. `LEMMY_DEBUG=1 ./target/app` would set the `debug` key
@@ -115,16 +115,16 @@ impl Settings {
     format!("{}/api/v1", self.hostname)
   }
 
-  pub fn get_config_location() -> String {
-    env::var("LEMMY_CONFIG_LOCATION").unwrap_or_else(|_| CONFIG_FILE.to_string())
+  pub fn get_config_defaults_location() -> String {
+    env::var("LEMMY_CONFIG_LOCATION").unwrap_or_else(|_| CONFIG_FILE_DEFAULTS.to_string())
   }
 
   pub fn read_config_file() -> Result<String, Error> {
-    fs::read_to_string(Self::get_config_location())
+    fs::read_to_string(CONFIG_FILE)
   }
 
   pub fn save_config_file(data: &str) -> Result<String, Error> {
-    fs::write(Self::get_config_location(), data)?;
+    fs::write(CONFIG_FILE, data)?;
 
     // Reload the new settings
     // From https://stackoverflow.com/questions/29654927/how-do-i-assign-a-string-to-a-mutable-static-variable/47181804#47181804
