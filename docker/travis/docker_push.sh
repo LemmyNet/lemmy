49892690ff0842d84b742eb6930014c942942a35
Dessalines <tyhou13@gmx.com>
2020-08-11 10:58:40 -0400

    Version v0.7.49

--- docker/travis/docker_push.sh
+++ docker/travis/docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag dessalines/lemmy:travis \
-  dessalines/lemmy:v0.7.48
-docker push dessalines/lemmy:v0.7.48
+  dessalines/lemmy:v0.7.49
+docker push dessalines/lemmy:v0.7.49
--- server/src/version.rs
+++ server/src/version.rs
@@ -1,1 +1,1 @@
-pub const VERSION: &str = "v0.7.48";
+pub const VERSION: &str = "v0.7.49";
