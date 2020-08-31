34e539cdc0d4192608555d2652a575eaafc1305b
Dessalines <tyhou13@gmx.com>
2020-08-31 13:39:01 -0500

    Updating dev docker-compose.

--- docker/dev/docker_update.sh
+++ docker/dev/docker_update.sh
@@ -3,4 +3,5 @@ set -e
 
 export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
-docker-compose up -d --no-deps --build
+sudo chown -R 991:991 volumes/pictrs
+sudo docker-compose up -d --no-deps --build
