400f949580b1d88370630b0319887febc73147ce
Dessalines <tyhou13@gmx.com>
2020-09-14 12:27:13 -0500

    Revert "Try to fix image uploads in federation test"
    This reverts commit a2ddf2a90b7ac718c3d9002986129e735f7b8c0a.

--- docker/federation/start-local-instances.bash
+++ docker/federation/start-local-instances.bash
@@ -3,7 +3,9 @@ set -e
 
 sudo docker build ../../ --file ../dev/Dockerfile -t lemmy-federation:latest
 
-sudo mkdir -p volumes/pictrs
-sudo chown -R 991:991 volumes/pictrs
+for Item in alpha beta gamma delta epsilon ; do
+  sudo mkdir -p volumes/pictrs_$Item
+  sudo chown -R 991:991 volumes/pictrs_$Item
+done
 
 sudo docker-compose up
