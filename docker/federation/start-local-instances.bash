a2ddf2a90b7ac718c3d9002986129e735f7b8c0a
Felix Ableitner <me@nutomic.com>
2020-09-14 18:32:38 +0200

    Try to fix image uploads in federation test

--- docker/federation/start-local-instances.bash
+++ docker/federation/start-local-instances.bash
@@ -3,9 +3,7 @@ set -e
 
 sudo docker build ../../ --file ../dev/Dockerfile -t lemmy-federation:latest
 
-for Item in alpha beta gamma delta epsilon ; do
-  sudo mkdir -p volumes/pictrs_$Item
-  sudo chown -R 991:991 volumes/pictrs_$Item
-done
+sudo mkdir -p volumes/pictrs
+sudo chown -R 991:991 volumes/pictrs
 
 sudo docker-compose up
