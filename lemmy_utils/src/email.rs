a02083e26b1439dc2f233f02e7b8c11f45a35a39
Dessalines <tyhou13@gmx.com>
2020-10-30 12:05:43 -0500

    Fixing lettre email.

--- lemmy_utils/src/email.rs
+++ lemmy_utils/src/email.rs
@@ -20,7 +20,17 @@ pub fn send_email(
   html: &str,
 ) -> Result<(), String> {
   let email_config = Settings::get().email.ok_or("no_email_setup")?;
-  let domain = &Settings::get().hostname;
+  let domain = Settings::get().hostname;
+
+  let (smtp_server, smtp_port) = {
+    let email_and_port = email_config.smtp_server.split(':').collect::<Vec<&str>>();
+    (
+      email_and_port[0],
+      email_and_port[1]
+        .parse::<u16>()
+        .expect("email needs a port"),
+    )
+  };
 
   let email = Message::builder()
     .from(
@@ -63,11 +73,11 @@ pub fn send_email(
 
   // don't worry about 'dangeous'. it's just that leaving it at the default configuration
   // is bad.
-  let mut builder = SmtpTransport::builder_dangerous(domain);
+  let mut builder = SmtpTransport::builder_dangerous(smtp_server).port(smtp_port);
 
   // Set the TLS
   if email_config.use_tls {
-    let tls_config = TlsParameters::new(domain.to_string()).expect("the TLS backend is happy");
+    let tls_config = TlsParameters::new(smtp_server.to_string()).expect("the TLS backend is happy");
     builder = builder.tls(Tls::Wrapper(tls_config));
   }
 
@@ -76,9 +86,7 @@ pub fn send_email(
     builder = builder.credentials(Credentials::new(username, password));
   }
 
-  let mailer = builder
-    .hello_name(ClientId::Domain(domain.to_string()))
-    .build();
+  let mailer = builder.hello_name(ClientId::Domain(domain)).build();
 
   let result = mailer.send(&email);
 
