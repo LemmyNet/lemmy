88dc33a56e4dc195233e79f391554fbe0fe409b3
Dessalines <tyhou13@gmx.com>
2020-11-23 09:55:45 -0600

    Adding some script comments.

--- docker/dev/docker_update.sh
+++ docker/dev/docker_update.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# This script uses a docker file that builds with musl, and runs on linux alpine
+# Its a bit slower for development than the volume mount.
+
 set -e
 
 mkdir -p volumes/pictrs
