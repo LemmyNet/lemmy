2e9164584bb1fda100e00b0f4e43973297ab4701
Dessalines <tyhou13@gmx.com>
2020-11-30 12:56:05 -0600

    Version v0.8.8

--- docker/travis/docker_push.sh
+++ docker/travis/docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag dessalines/lemmy:travis \
-  dessalines/lemmy:v0.8.7
-docker push dessalines/lemmy:v0.8.7
+  dessalines/lemmy:v0.8.8
+docker push dessalines/lemmy:v0.8.8
