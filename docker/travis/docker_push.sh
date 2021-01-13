eafdf3033fa46c31afced9ca7d12fe8d6f1d91e4
Dessalines <tyhou13@gmx.com>
2021-01-12 19:29:48 -0500

    Version v0.9.0-rc.2

--- docker/travis/docker_push.sh
+++ docker/travis/docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag dessalines/lemmy:travis \
-  dessalines/lemmy:v0.9.0-rc.1
-docker push dessalines/lemmy:v0.9.0-rc.1
+  dessalines/lemmy:v0.9.0-rc.2
+docker push dessalines/lemmy:v0.9.0-rc.2
--- lemmy_api/src/version.rs
+++ lemmy_api/src/version.rs
@@ -1,1 +1,1 @@
-pub const VERSION: &str = "v0.9.0-rc.1";
+pub const VERSION: &str = "v0.9.0-rc.2";
