8f61a148f66e4318122ae98394503e3647f10e40
Dessalines <tyhou13@gmx.com>
2021-01-15 11:58:56 -0500

    Fixing comment count necro-bump issue.

--- migrations/2020-12-10-152350_create_post_aggregates/up.sql
+++ migrations/2020-12-10-152350_create_post_aggregates/up.sql
@@ -66,10 +66,13 @@ as $$
 begin
   IF (TG_OP = 'INSERT') THEN
     update post_aggregates pa
-    set comments = comments + 1,
-    newest_comment_time = NEW.published
-    where pa.post_id = NEW.post_id
+    set comments = comments + 1
+    where pa.post_id = NEW.post_id;
+
     -- A 2 day necro-bump limit
+    update post_aggregates pa
+    set newest_comment_time = NEW.published
+    where pa.post_id = NEW.post_id
     and published > ('now'::timestamp - '2 days'::interval);
   ELSIF (TG_OP = 'DELETE') THEN
     -- Join to post because that post may not exist anymore
