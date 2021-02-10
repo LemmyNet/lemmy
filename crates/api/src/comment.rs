acadf0289e970263fda813ae33c8653297e08a56
Dessalines <tyhou13@gmx.com>
2021-02-10 10:36:22 -0500

    Fixing reason lengths to char counts.

--- crates/api/src/comment.rs
+++ crates/api/src/comment.rs
@@ -672,7 +672,7 @@ impl Perform for CreateCommentReport {
     if reason.is_empty() {
       return Err(APIError::err("report_reason_required").into());
     }
-    if reason.len() > 1000 {
+    if reason.chars().count() > 1000 {
       return Err(APIError::err("report_too_long").into());
     }
 
