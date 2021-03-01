c70d748b6245cc445c59cc91f13a0e1adcc97ed1
Dessalines <tyhou13@gmx.com>
2021-03-01 12:48:42 -0500

    Fixing comment.

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -474,8 +474,8 @@ pub(crate) fn espeak_wav_base64(text: &str) -> Result<String, LemmyError> {
   Ok(base64)
 }
 
+/// Checks the password length
 pub(crate) fn password_length_check(pass: &str) -> Result<(), LemmyError> {
-  // Password length check
   if pass.len() > 60 {
     Err(ApiError::err("invalid_password").into())
   } else {
