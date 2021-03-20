53ea383b4e139ebbe8c81ea9b1a4acd6faa47f9d
Dessalines <tyhou13@gmx.com>
2021-03-20 16:32:52 -0400

    Forgot to regenerate person_alias views

--- migrations/2021-03-20-185321_move_matrix_id_to_person/down.sql
+++ migrations/2021-03-20-185321_move_matrix_id_to_person/down.sql
@@ -5,4 +5,9 @@ set matrix_user_id = p.matrix_user_id
 from person p
 where p.id = lu.person_id;
 
+drop view person_alias_1, person_alias_2;
 alter table person drop column matrix_user_id;
+
+-- Regenerate the person_alias views
+create view person_alias_1 as select * from person;
+create view person_alias_2 as select * from person;
--- migrations/2021-03-20-185321_move_matrix_id_to_person/up.sql
+++ migrations/2021-03-20-185321_move_matrix_id_to_person/up.sql
@@ -6,3 +6,8 @@ from local_user lu
 where p.id = lu.person_id;
 
 alter table local_user drop column matrix_user_id;
+
+-- Regenerate the person_alias views
+drop view person_alias_1, person_alias_2;
+create view person_alias_1 as select * from person;
+create view person_alias_2 as select * from person;
