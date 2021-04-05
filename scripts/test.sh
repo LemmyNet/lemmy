74a63804dc7ae8984e334561164214d305a361bd
Nutomic <me@nutomic.com>
2021-04-05 17:21:56 +0000

    Fix test failure due to config loading error (#1547)
    * Crash if config fails to load
    
    * fix previous commit
    
    * Fix test failure due to config loading error
    
    * Adding test fix to drone.
    
    * Logging line.
    
    * Trying to fix drone 2
    
    * Fixing deploy script.
    
    Co-authored-by: Dessalines <tyhou13@gmx.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,5 +5,8 @@ psql -U lemmy -d postgres -c "DROP DATABASE lemmy;"
 psql -U lemmy -d postgres -c "CREATE DATABASE lemmy;"
 
 export LEMMY_DATABASE_URL=postgres://lemmy:password@localhost:5432/lemmy
+# tests are executed in working directory crates/api (or similar),
+# so to load the config we need to traverse to the repo root
+export LEMMY_CONFIG_LOCATION=../../config/config.hjson
 RUST_BACKTRACE=1 \
   cargo test --workspace --no-fail-fast
