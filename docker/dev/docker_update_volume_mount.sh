b8a6592369e0704812478574ed207e9aedf32b64
Nutomic <me@nutomic.com>
2021-04-08 14:33:58 +0000

    Drone release main (ref #1556) (#1557)
    * publish docker images from main
    
    * try with separate step
    
    * redo, lots of boilerplate
    
    * try to fix syntax
    
    * unique step names
    
    * fix docker tags, remove cargo-chef
    
    * only build dev image on main branch
    
    * use `ref` for condition, as `branch` uses the target branch for PRs
    
    * consistent indents
    
    * fix tag
    
    * use lemmy-ui:dev image for `docker/dev` and `docker/federation`

--- docker/dev/docker_update_volume_mount.sh
+++ docker/dev/docker_update_volume_mount.sh
@@ -8,4 +8,5 @@ set -e
 mkdir -p volumes/pictrs
 sudo chown -R 991:991 volumes/pictrs
 sudo docker build ../../ --file ../dev/volume_mount.dockerfile -t lemmy-dev:latest
+sudo docker-compose pull --ignore-pull-failures || true
 sudo docker-compose up
