8a1af056e2a833db88976ad7144deddca74758c0
Dessalines <dessalines@users.noreply.github.com>
2021-08-13 13:39:56 -0400

    When banning a user, remove communities they've created (#1700)
    - Fixes #1659

--- crates/db_views_actor/src/community_moderator_view.rs
+++ crates/db_views_actor/src/community_moderator_view.rs
@@ -49,6 +49,28 @@ impl CommunityModeratorView {
 
     Ok(Self::from_tuple_to_vec(res))
   }
+
+  /// Finds all communities first mods / creators
+  /// Ideally this should be a group by, but diesel doesn't support it yet
+  pub fn get_community_first_mods(conn: &PgConnection) -> Result<Vec<Self>, Error> {
+    let res = community_moderator::table
+      .inner_join(community::table)
+      .inner_join(person::table)
+      .select((
+        Community::safe_columns_tuple(),
+        Person::safe_columns_tuple(),
+      ))
+      // A hacky workaround instead of group_bys
+      // https://stackoverflow.com/questions/24042359/how-to-join-only-one-row-in-joined-table-with-postgres
+      .distinct_on(community_moderator::community_id)
+      .order_by((
+        community_moderator::community_id,
+        community_moderator::person_id,
+      ))
+      .load::<CommunityModeratorViewTuple>(conn)?;
+
+    Ok(Self::from_tuple_to_vec(res))
+  }
 }
 
 impl ViewToVec for CommunityModeratorView {
