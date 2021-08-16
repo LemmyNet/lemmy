2029c853b93b32fa6fba1e9a1396c95378d2a8cb
Dessalines <tyhou13@gmx.com>
2021-08-16 12:46:50 -0400

    Cleaning up post_link_tags

--- crates/routes/src/post_link_tags.rs
+++ crates/routes/src/post_link_tags.rs
@@ -12,7 +12,6 @@ struct Params {
 
 pub fn config(cfg: &mut web::ServiceConfig) {
   cfg.route("/post_link_tags", web::get().to(get_post_links_response));
-  // .app_data(Data::new(client));
 }
 
 async fn get_post_links_response(
@@ -21,12 +20,10 @@ async fn get_post_links_response(
 ) -> Result<HttpResponse, Error> {
   let url =
     Url::parse(&info.url).map_err(|_| ErrorBadRequest(LemmyError::from(anyhow!("not_found"))))?;
-  println!("url: {:?}", url);
 
   let json = fetch_post_link_tags(context.client(), &url)
     .await
     .map_err(|_| ErrorBadRequest(LemmyError::from(anyhow!("not_found"))))?;
-  println!("json: {:?}", json);
 
   Ok(HttpResponse::Ok().json(json))
 }
