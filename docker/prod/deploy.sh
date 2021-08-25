af225a42d0863a4362967b8e035e5b1a4643b725
Dessalines <dessalines@users.noreply.github.com>
2021-08-25 17:26:10 -0400

    Fix prod deploy script and clippy (#1724)
    * Updating lemmy-js-client.
    
    * Fix prod deploy script and clippy
    
    * Try using buster.
    
    * Using more generic and updated images.
    
    * again.
    
    * again.
    
    * Try alternate env
    
    * again.
    
    * again.
    
    * Version 0.11.4-rc.24
    
    * again.
    
    * Better old_tag
    
    * Version 0.11.4-rc.27
    
    * Updating cargo.lock
    
    * Trying to run a cargo check to update the lock
    
    * Version 0.11.4-rc.28

--- docker/prod/deploy.sh
+++ docker/prod/deploy.sh
@@ -2,7 +2,6 @@
 #git checkout main
 
 # Creating the new tag
-old_tag=$(cat "ansible/VERSION")
 new_tag="$1"
 third_semver=$(echo $new_tag | cut -d "." -f 3)
 
@@ -21,6 +20,8 @@ if [ ! -z "${third_semver##*[!0-9]*}" ]; then
 fi
 
 # Update crate versions for crates.io
+pushd ../../
+old_tag=$(head -3 Cargo.toml | tail -1 | cut -d'"' -f 2)
 for crate in crates/*; do
   pushd $crate
   # update version of the crate itself (only first occurence)
@@ -28,12 +29,16 @@ for crate in crates/*; do
   sed -i "0,/version = \"$old_tag\"/s//version = \"$new_tag\"/g" Cargo.toml
   # update version of lemmy dependencies
   sed -i "s/{ version = \"=$old_tag\", path/{ version = \"=$new_tag\", path/g" Cargo.toml
+  git add Cargo.toml
   popd
 done
 # same as above, for the main cargo.toml
 sed -i "s/{ version = \"=$old_tag\", path/{ version = \"=$new_tag\", path/g" Cargo.toml
 sed -i "s/version = \"$old_tag\"/version = \"$new_tag\"/g" Cargo.toml
-
+git add Cargo.toml
+cargo check
+git add Cargo.lock
+popd
 
 # The commit
 git commit -m"Version $new_tag"
