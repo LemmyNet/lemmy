03e52b4824dc14bb2ff722633a86c7e43f86dcce
Richard O'Dwyer <richard@richard.do>
2021-08-31 22:19:01 +0300

    turn off censor tests

--- crates/utils/src/test.rs
+++ crates/utils/src/test.rs
@@ -55,33 +55,33 @@ fn test_valid_matrix_id() {
   assert!(!is_valid_matrix_id("@dess:matrix.org t"));
 }
 
-#[test]
-fn test_slur_filter() {
-  let test =
-      "faggot test kike tranny cocksucker retardeds. Capitalized Niggerz. This is a bunch of other safe text.";
-  let slur_free = "No slurs here";
-  assert_eq!(
-      remove_slurs(test),
-      "*removed* test *removed* *removed* *removed* *removed*. Capitalized *removed*. This is a bunch of other safe text."
-        .to_string()
-    );
+// #[test]
+// fn test_slur_filter() {
+//   let test =
+//       "faggot test kike tranny cocksucker retardeds. Capitalized Niggerz. This is a bunch of other safe text.";
+//   let slur_free = "No slurs here";
+//   assert_eq!(
+//       remove_slurs(test),
+//       "*removed* test *removed* *removed* *removed* *removed*. Capitalized *removed*. This is a bunch of other safe text."
+//         .to_string()
+//     );
 
-  let has_slurs_vec = vec![
-    "Niggerz",
-    "cocksucker",
-    "faggot",
-    "kike",
-    "retardeds",
-    "tranny",
-  ];
-  let has_slurs_err_str = "No slurs - Niggerz, cocksucker, faggot, kike, retardeds, tranny";
+//   let has_slurs_vec = vec![
+//     "Niggerz",
+//     "cocksucker",
+//     "faggot",
+//     "kike",
+//     "retardeds",
+//     "tranny",
+//   ];
+//   let has_slurs_err_str = "No slurs - Niggerz, cocksucker, faggot, kike, retardeds, tranny";
 
-  assert_eq!(slur_check(test), Err(has_slurs_vec));
-  assert_eq!(slur_check(slur_free), Ok(()));
-  if let Err(slur_vec) = slur_check(test) {
-    assert_eq!(&slurs_vec_to_str(slur_vec), has_slurs_err_str);
-  }
-}
+//   assert_eq!(slur_check(test), Err(has_slurs_vec));
+//   assert_eq!(slur_check(slur_free), Ok(()));
+//   if let Err(slur_vec) = slur_check(test) {
+//     assert_eq!(&slurs_vec_to_str(slur_vec), has_slurs_err_str);
+//   }
+// }
 
 // These helped with testing
 // #[test]
