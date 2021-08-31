c492a98552582a93bb285894e77d886466d97477
Richard O'Dwyer <richard@richard.do>
2021-08-31 23:49:02 +0300

    deploy steps

--- docker/prod/docker_deploy.sh
+++ docker/prod/docker_deploy.sh
@@ -3,10 +3,10 @@ set -e
 
 export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
-
+new_tag="$1"
 # Rebuilding dev docker
-docker build ../../ -f ./Dockerfile -t "richardarpanet/lemmy:dev"
-docker push "richardarpanet/lemmy:dev"
+docker build ../../ -f . -t "richardarpanet/lemmy:$new_tag"
+docker push "richardarpanet/lemmy:$new_tag"
 
 # Run the playbook
 # pushd ../../../lemmy-ansible
