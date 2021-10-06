855ac1ae93b9db309ef45688ea122c81156c788f
LunaticHacker <lunatichacker@tutanota.com>
2021-10-06 18:50:07 +0530

    mark parent as read on reply

--- crates/api_crud/src/comment/create.rs
+++ crates/api_crud/src/comment/create.rs
@@ -168,6 +168,14 @@ impl PerformCrud for CreateComment {
       .await?
       .map_err(|_| ApiError::err("couldnt_update_comment"))?;
     }
+     // If its a reply, mark the parent as read
+     if  let Some(parent_id) = data.parent_id {
+      blocking(context.pool(), move |conn| {
+        Comment::update_read(conn, parent_id, true)
+      })
+      .await?
+      .map_err(|_| ApiError::err("couldnt_update_parent_comment"))?;
+    } 
 
     send_comment_ws_message(
       inserted_comment.id,
