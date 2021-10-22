32970d735de123b9b8cf8927c8ea3dab05daaaab
Felix Ableitner <me@nutomic.com>
2021-10-22 12:23:18 +0200

    Remove CommentInReplyToMigration

--- crates/apub/src/objects/comment.rs
+++ crates/apub/src/objects/comment.rs
@@ -2,7 +2,6 @@ use crate::{
   activities::{verify_is_public, verify_person_in_community},
   context::lemmy_context,
   fetcher::object_id::ObjectId,
-  migrations::CommentInReplyToMigration,
   objects::{create_tombstone, person::ApubPerson, post::ApubPost, Source},
   PostOrComment,
 };
@@ -14,7 +13,7 @@ use activitystreams::{
   public,
   unparsed::Unparsed,
 };
-use anyhow::{anyhow, Context};
+use anyhow::anyhow;
 use chrono::{DateTime, FixedOffset};
 use html2md::parse_html;
 use lemmy_api_common::blocking;
@@ -35,7 +34,6 @@ use lemmy_db_schema::{
   DbPool,
 };
 use lemmy_utils::{
-  location_info,
   utils::{convert_datetime, remove_slurs},
   LemmyError,
 };
@@ -61,7 +59,7 @@ pub struct Note {
   content: String,
   media_type: Option<MediaTypeHtml>,
   source: SourceCompat,
-  in_reply_to: CommentInReplyToMigration,
+  in_reply_to: ObjectId<PostOrComment>,
   published: Option<DateTime<FixedOffset>>,
   updated: Option<DateTime<FixedOffset>>,
   #[serde(flatten)]
@@ -91,43 +89,25 @@ impl Note {
     context: &LemmyContext,
     request_counter: &mut i32,
   ) -> Result<(ApubPost, Option<CommentId>), LemmyError> {
-    match &self.in_reply_to {
-      CommentInReplyToMigration::Old(in_reply_to) => {
-        // This post, or the parent comment might not yet exist on this server yet, fetch them.
-        let post_id = in_reply_to.get(0).context(location_info!())?;
-        let post_id = ObjectId::new(post_id.clone());
-        let post = Box::pin(post_id.dereference(context, request_counter)).await?;
-
-        // The 2nd item, if it exists, is the parent comment apub_id
-        // Nested comments will automatically get fetched recursively
-        let parent_id: Option<CommentId> = match in_reply_to.get(1) {
-          Some(comment_id) => {
-            let comment_id = ObjectId::<ApubComment>::new(comment_id.clone());
-            let parent_comment = Box::pin(comment_id.dereference(context, request_counter)).await?;
-
-            Some(parent_comment.id)
-          }
-          None => None,
-        };
-
-        Ok((post, parent_id))
+    // Fetch parent comment chain in a box, otherwise it can cause a stack overflow.
+    let parent = Box::pin(
+      self
+        .in_reply_to
+        .dereference(context, request_counter)
+        .await?,
+    );
+    match parent.deref() {
+      PostOrComment::Post(p) => {
+        // Workaround because I cant figure out how to get the post out of the box (and we dont
+        // want to stackoverflow in a deep comment hierarchy).
+        let post_id = p.id;
+        let post = blocking(context.pool(), move |conn| Post::read(conn, post_id)).await??;
+        Ok((post.into(), None))
       }
-      CommentInReplyToMigration::New(in_reply_to) => {
-        let parent = Box::pin(in_reply_to.dereference(context, request_counter).await?);
-        match parent.deref() {
-          PostOrComment::Post(p) => {
-            // Workaround because I cant figure out how to get the post out of the box (and we dont
-            // want to stackoverflow in a deep comment hierarchy).
-            let post_id = p.id;
-            let post = blocking(context.pool(), move |conn| Post::read(conn, post_id)).await??;
-            Ok((post.into(), None))
-          }
-          PostOrComment::Comment(c) => {
-            let post_id = c.post_id;
-            let post = blocking(context.pool(), move |conn| Post::read(conn, post_id)).await??;
-            Ok((post.into(), Some(c.id)))
-          }
-        }
+      PostOrComment::Comment(c) => {
+        let post_id = c.post_id;
+        let post = blocking(context.pool(), move |conn| Post::read(conn, post_id)).await??;
+        Ok((post.into(), Some(c.id)))
       }
     }
   }
@@ -219,15 +199,12 @@ impl ToApub for ApubComment {
     let post_id = self.post_id;
     let post = blocking(pool, move |conn| Post::read(conn, post_id)).await??;
 
-    // Add a vector containing some important info to the "in_reply_to" field
-    // [post_ap_id, Option(parent_comment_ap_id)]
-    let mut in_reply_to_vec = vec![post.ap_id.into_inner()];
-
-    if let Some(parent_id) = self.parent_id {
-      let parent_comment = blocking(pool, move |conn| Comment::read(conn, parent_id)).await??;
-
-      in_reply_to_vec.push(parent_comment.ap_id.into_inner());
-    }
+    let in_reply_to = if let Some(comment_id) = self.parent_id {
+      let parent_comment = blocking(pool, move |conn| Comment::read(conn, comment_id)).await??;
+      ObjectId::<PostOrComment>::new(parent_comment.ap_id.into_inner())
+    } else {
+      ObjectId::<PostOrComment>::new(post.ap_id.into_inner())
+    };
 
     let note = Note {
       context: lemmy_context(),
@@ -241,7 +218,7 @@ impl ToApub for ApubComment {
         content: self.content.clone(),
         media_type: MediaTypeMarkdown::Markdown,
       }),
-      in_reply_to: CommentInReplyToMigration::Old(in_reply_to_vec),
+      in_reply_to,
       published: Some(convert_datetime(self.published)),
       updated: self.updated.map(convert_datetime),
       unparsed: Default::default(),
