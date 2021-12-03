be951a76c83bc1c8cf74c2ff7cff4b222dae3747
Colin Reeder <vpzomtrrfrt@gmail.com>
2021-12-02 21:53:16 -0700

    Add lotide activities to tests

--- crates/apub/src/protocol/activities/create_or_update/mod.rs
+++ crates/apub/src/protocol/activities/create_or_update/mod.rs
@@ -26,5 +26,8 @@ mod tests {
     file_to_json_object::<CreateOrUpdateComment>("assets/pleroma/activities/create_note.json");
     file_to_json_object::<CreateOrUpdateComment>("assets/smithereen/activities/create_note.json");
     file_to_json_object::<CreateOrUpdateComment>("assets/mastodon/activities/create_note.json");
+
+    file_to_json_object::<CreateOrUpdatePost>("assets/lotide/activities/create_page.json");
+    file_to_json_object::<CreateOrUpdateComment>("assets/lotide/activities/create_note_reply.json");
   }
 }
