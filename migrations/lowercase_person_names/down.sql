9e6a391b68b91327ecaa112b8c280317dc684825
Kradyz <81911574+Kradyz@users.noreply.github.com>
2021-12-16 01:03:09 +0100

    Update down.sql

--- migrations/lowercase_person_names/down.sql
+++ migrations/lowercase_person_names/down.sql
@@ -1,8 +1,18 @@
 -- This file should undo anything in `up.sql`
 
 
--- Remove column constraints
+-- Recover the old values
+update person set name = old_name;
+update person set actor_id = old_actor_id;
+update person set inbox_url = old_inbox_url;
 
-ALTER TABLE person DROP CONSTRAINT person_inbox_url_lowercase_ck;
-ALTER TABLE person DROP CONSTRAINT person_actor_id_lowercase_ck;
-ALTER TABLE person DROP CONSTRAINT person_actor_id_lowercase_ck;
+-- Drop the "old" indices
+drop index old_name;
+drop index old_actor_id;
+drop index old_inbox_url;
+
+-- Remove column constraints
+alter table person drop constraint person_inbox_url_lowercase_ck;
+alter table person drop constraint person_actor_id_lowercase_ck;
+alter table person drop constraint person_inbox_url_lowercase_ck;
+
