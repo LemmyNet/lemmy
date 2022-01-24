5fba734cd983532b3205f0c2eb1a3121c04769ee
Thor Odinson <odinson@asgard.com>
2022-01-24 09:58:40 -0600

    Formatting fix

--- migrations/2022-01-04-034553_add_hidden_column/up.sql
+++ migrations/2022-01-04-034553_add_hidden_column/up.sql
@@ -3,7 +3,7 @@ alter table community add column hidden boolean default false;
 
 create table mod_hide_community
 (
-   id serial primary key,
+    id serial primary key,
     community_id int references community on update cascade on delete cascade not null,
     person_id int references person on update cascade on delete cascade not null,
     when_ timestamp not null default now(),
