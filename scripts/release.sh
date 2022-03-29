f6b543ae0fe2787d8be130878bbf222237c220ed
Dessalines <dessalines@users.noreply.github.com>
2022-03-29 15:36:59 +0000

    Add tag to lemmy-ansible. (#2172)

--- scripts/release.sh
+++ scripts/release.sh
@@ -23,6 +23,8 @@ if [ ! -z "${third_semver##*[!0-9]*}" ]; then
   echo $new_tag > "VERSION"
   git add "VERSION"
   git commit -m"Updating VERSION"
+  git tag $new_tag
+  git push origin $new_tag
   git push
   popd
 fi
