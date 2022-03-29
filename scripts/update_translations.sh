11b7525c419a3e451e58e29fb1b997621184c8a1
Dessalines <dessalines@users.noreply.github.com>
2022-03-29 16:54:16 +0000

    Add update translations script. (#2173)

---
+++ scripts/update_translations.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+pushd ../../lemmy-translations
+git fetch weblate
+git merge weblate/main
+git push
+popd
+
+git submodule update --remote
+git add ../crates/utils/translations
+git commit -m"Updating translations."
+git push
