3ad172e8ed9e0458bb21daaa5fd745b9159a98da
Dessalines <tyhou13@gmx.com>
2022-03-30 09:56:23 -0400

    Adding a ban expires update job. Fixes #2177

--- src/scheduled_tasks.rs
+++ src/scheduled_tasks.rs
@@ -13,12 +13,14 @@ pub fn setup(pool: DbPool) -> Result<(), LemmyError> {
 
   let conn = pool.get()?;
   active_counts(&conn);
+  update_banned_when_expired(&conn);
 
   // On startup, reindex the tables non-concurrently
   // TODO remove this for now, since it slows down startup a lot on lemmy.ml
   reindex_aggregates_tables(&conn, true);
   scheduler.every(1.hour()).run(move || {
     active_counts(&conn);
+    update_banned_when_expired(&conn);
     reindex_aggregates_tables(&conn, true);
   });
 
@@ -90,4 +92,14 @@ fn active_counts(conn: &PgConnection) {
   }
 
   info!("Done.");
+}
+
+/// Set banned to false after ban expires
+fn update_banned_when_expired(conn: &PgConnection) {
+  info!("Updating banned column if it expires ...");
+  let update_ban_expires_stmt =
+    format!("update person set banned = false where banned = true and ban_expires < now()");
+  sql_query(update_ban_expires_stmt)
+    .execute(conn)
+    .expect("update banned when expires");
 }
