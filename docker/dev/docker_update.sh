abcbcc29ccd4eb9446733ece264f369ffd177d8c
Felix Ableitner <me@nutomic.com>
2022-04-11 19:00:50 +0200

    Write mod log for federated sticky/lock post actions

--- docker/dev/docker_update.sh
+++ docker/dev/docker_update.sh
@@ -7,6 +7,7 @@ set -e
 
 mkdir -p volumes/pictrs
 sudo chown -R 991:991 volumes/pictrs
+sudo docker-compose down
 sudo docker build ../../ --file ../dev/Dockerfile -t lemmy-dev:latest
 sudo docker-compose pull --ignore-pull-failures || true
 sudo docker-compose up -d
