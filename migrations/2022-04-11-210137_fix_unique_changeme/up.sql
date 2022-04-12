041dce7065a76677e131b8623fbf56f96e50c72f
Dessalines <tyhou13@gmx.com>
2022-04-12 13:03:49 -0400

    Changing link to .invalid domain.

--- migrations/2022-04-11-210137_fix_unique_changeme/up.sql
+++ migrations/2022-04-11-210137_fix_unique_changeme/up.sql
@@ -1,5 +1,5 @@
 create or replace function generate_unique_changeme() 
 returns text language sql 
 as $$
-  select 'http://changeme.ml/' || substr(md5(random()::text), 0, 25);
+  select 'http://changeme.invalid/' || substr(md5(random()::text), 0, 25);
 $$;
