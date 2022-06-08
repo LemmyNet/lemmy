7c09900f9381d6b781adfdbe023bded0971c039d
Felix Ableitner <me@nutomic.com>
2022-06-08 13:21:32 +0200

    Add pub use for db crates in api_common

--- crates/api_common/src/lib.rs
+++ crates/api_common/src/lib.rs
@@ -9,3 +9,8 @@ pub mod site;
 #[cfg(feature = "full")]
 pub mod utils;
 pub mod websocket;
+
+pub extern crate lemmy_db_schema;
+pub extern crate lemmy_db_views;
+pub extern crate lemmy_db_views_actor;
+pub extern crate lemmy_db_views_moderator;
