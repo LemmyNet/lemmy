4067e9b2ecf01c060266e7077152a6f88cdd1c59
Niharika <niharika@fsfe.org>
2022-07-11 18:06:44 +0200

    making a .sql file REUSE compliant

--- migrations/2019-03-05-233828_create_comment/up.sql
+++ migrations/2019-03-05-233828_create_comment/up.sql
@@ -1,3 +1,8 @@
+-- SPDX-FileCopyrightText: 2019-2022 2019 Felix Ableitner, <me@nutomic.com> et al.
+--
+-- SPDX-License-Identifier: AGPL-3.0-only
+
+
 create table comment (
   id serial primary key,
   creator_id int references user_ on update cascade on delete cascade not null,
