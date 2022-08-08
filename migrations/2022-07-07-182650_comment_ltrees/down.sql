0901ad1d5ec8ce3eacf8859fdcf5b43ba41ce5e4
Niharika <niharika@fsfe.org>
2022-08-08 13:00:06 +0200

    adding all modified files

--- migrations/2022-07-07-182650_comment_ltrees/down.sql
+++ migrations/2022-07-07-182650_comment_ltrees/down.sql
@@ -1,3 +1,7 @@
+-- SPDX-FileCopyrightText: 2019-2022 2019 Felix Ableitner, <me@nutomic.com> et al.
+--
+-- SPDX-License-Identifier: AGPL-3.0-only
+
 alter table comment add column parent_id integer;
 
 -- Constraints and index
