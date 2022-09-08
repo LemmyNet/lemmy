3e48c5b308798d9ea6e2059c891459d2f4c393fe
Felix Ableitner <me@nutomic.com>
2022-09-08 23:08:27 +0200

    during db migration, update existing users, sites, communities to have all languages enabled

--- migrations/2022-09-08-102358_site-and-community-languages/up.sql
+++ migrations/2022-09-08-102358_site-and-community-languages/up.sql
@@ -11,3 +11,43 @@ create table community_language (
   language_id int references language on update cascade on delete cascade not null,
   unique (community_id, language_id)
 );
+
+-- existing users get all languages enabled
+do $$
+    declare
+        uid integer;
+begin
+    for uid in select id from local_user
+    loop
+        insert into local_user_language (local_user_id, language_id)
+        (select uid, language.id as lid from language);
+    end loop;
+end;
+$$;
+
+-- existing sites get all languages enabled
+do $$
+    declare
+        sid integer;
+begin
+    for sid in select id from site
+    loop
+        insert into site_language (site_id, language_id)
+        (select sid, language.id as lid from language);
+    end loop;
+end;
+$$;
+
+
+-- existing communities get all languages enabled
+do $$
+    declare
+        cid integer;
+begin
+    for cid in select id from community
+    loop
+        insert into community_language (community_id, language_id)
+        (select cid, language.id as lid from language);
+    end loop;
+end;
+$$;
