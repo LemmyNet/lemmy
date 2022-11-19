a39cf314661dea65c1922209772fc38b5ed31a59
Nutomic <me@nutomic.com>
2022-11-19 01:30:59 +0000

    Sort vecs before assert to avoid random test failures (#2569)
    * Sort language read queries to avoid random test failures
    
    * change sort columns

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,5 +9,5 @@ export LEMMY_DATABASE_URL=postgres://lemmy:password@localhost:5432/lemmy
 # so to load the config we need to traverse to the repo root
 export LEMMY_CONFIG_LOCATION=../../config/config.hjson
 RUST_BACKTRACE=1 \
-  cargo test --workspace --no-fail-fast
+  cargo test --workspace --all-features --no-fail-fast
 # Add this to do printlns: -- --nocapture
