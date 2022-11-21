65041a20bbcdf82ab5f53364460155c7991e5861
Dessalines <dessalines@users.noreply.github.com>
2022-11-21 05:21:09 -0500

    Fix clippy lints. (#2572)
    * Fix clippy lints.
    
    * Fix fmt.
    
    * Dont use cargo-workspaces for fix-clippy script.

--- crates/db_schema/src/impls/tagline.rs
+++ crates/db_schema/src/impls/tagline.rs
@@ -1,7 +1,7 @@
 use crate::{
   newtypes::LocalSiteId,
-  schema::tagline::dsl::*,
-  source::tagline::*,
+  schema::tagline::dsl::{local_site_id, tagline},
+  source::tagline::{Tagline, TaglineForm},
   utils::{get_conn, DbPool},
 };
 use diesel::{insert_into, result::Error, ExpressionMethods, QueryDsl};
@@ -45,7 +45,6 @@ impl Tagline {
     diesel::delete(tagline).execute(conn).await
   }
   pub async fn get_all(pool: &DbPool, for_local_site_id: LocalSiteId) -> Result<Vec<Self>, Error> {
-    use crate::schema::tagline::dsl::*;
     let conn = &mut get_conn(pool).await?;
     tagline
       .filter(local_site_id.eq(for_local_site_id))
