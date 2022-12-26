a6da447a78ef0356905b8009b7d65d8838893f40
Felix Ableitner <me@nutomic.com>
2022-12-26 16:38:32 +0100

    Add feature to embed pictrs in lemmy binary (fixes #2627)

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,14 @@ use lemmy_utils::{error::LemmyError, settings::SETTINGS};
 #[actix_web::main]
 pub async fn main() -> Result<(), LemmyError> {
   init_logging(&SETTINGS.opentelemetry_url)?;
-  start_lemmy_server().await
+  #[cfg(not(feature = "embed-pictrs"))]
+  start_lemmy_server().await?;
+  #[cfg(feature = "embed-pictrs")]
+  {
+    pict_rs::init_config::<String, String>(None, None).unwrap();
+    let (lemmy, pictrs) = tokio::join!(start_lemmy_server(), pict_rs::run());
+    lemmy?;
+    pictrs.expect("run pictrs");
+  }
+  Ok(())
 }
