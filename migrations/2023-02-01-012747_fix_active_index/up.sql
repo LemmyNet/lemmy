b22b89862ee470ed5a0840b736b4906549468347
Dessalines <tyhou13@gmx.com>
2023-01-31 21:05:43 -0500

    Fixing other sort.

--- migrations/2023-02-01-012747_fix_active_index/up.sql
+++ migrations/2023-02-01-012747_fix_active_index/up.sql
@@ -3,5 +3,5 @@ drop index
   idx_post_aggregates_featured_local_active,
   idx_post_aggregates_featured_community_active;
 
-create index idx_post_aggregates_featured_local_active on post_aggregates (featured_local desc, hot_rank(score, newest_comment_time_necro) desc, newest_comment_time desc);
-create index idx_post_aggregates_featured_community_active on post_aggregates (featured_community desc, hot_rank(score, newest_comment_time_necro) desc, newest_comment_time desc);
+create index idx_post_aggregates_featured_local_active on post_aggregates (featured_local desc, hot_rank(score, newest_comment_time_necro) desc, newest_comment_time_necro desc);
+create index idx_post_aggregates_featured_community_active on post_aggregates (featured_community desc, hot_rank(score, newest_comment_time_necro) desc, newest_comment_time_necro desc);
