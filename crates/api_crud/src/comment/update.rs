b15ed1460c84d11b5535c9ece18d903eac579085
Dessalines <tyhou13@gmx.com>
2023-02-14 16:39:20 -0500

    Adding back in community ban checks.

--- crates/api_crud/src/comment/update.rs
+++ crates/api_crud/src/comment/update.rs
@@ -3,7 +3,12 @@ use actix_web::web::Data;
 use lemmy_api_common::{
   comment::{CommentResponse, EditComment},
   context::LemmyContext,
-  utils::{get_local_user_view_from_jwt, is_mod_or_admin, local_site_to_slur_regex},
+  utils::{
+    check_community_ban,
+    get_local_user_view_from_jwt,
+    is_mod_or_admin,
+    local_site_to_slur_regex,
+  },
   websocket::{
     send::{send_comment_ws_message, send_local_notifs},
     UserOperationCrud,
@@ -42,6 +47,13 @@ impl PerformCrud for EditComment {
 
     let comment_id = data.comment_id;
     let orig_comment = CommentView::read(context.pool(), comment_id, None).await?;
+
+    check_community_ban(
+      local_user_view.person.id,
+      orig_comment.community.id,
+      context.pool(),
+    )
+    .await?;
 
     // Verify that only the creator can edit
     if local_user_view.person.id != orig_comment.creator.id {
--- crates/api_crud/src/post/update.rs
+++ crates/api_crud/src/post/update.rs
@@ -4,7 +4,7 @@ use lemmy_api_common::{
   context::LemmyContext,
   post::{EditPost, PostResponse},
   request::fetch_site_data,
-  utils::{get_local_user_view_from_jwt, local_site_to_slur_regex},
+  utils::{check_community_ban, get_local_user_view_from_jwt, local_site_to_slur_regex},
   websocket::{send::send_post_ws_message, UserOperationCrud},
 };
 use lemmy_db_schema::{
@@ -56,6 +56,13 @@ impl PerformCrud for EditPost {
 
     let post_id = data.post_id;
     let orig_post = Post::read(context.pool(), post_id).await?;
+
+    check_community_ban(
+      local_user_view.person.id,
+      orig_post.community_id,
+      context.pool(),
+    )
+    .await?;
 
     // Verify that only the creator can edit
     if !Post::is_post_creator(local_user_view.person.id, orig_post.creator_id) {
