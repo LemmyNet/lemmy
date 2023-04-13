844cde9db3ceec713799499279a97d8b62d28f8c
Nutomic <me@nutomic.com>
2023-04-13 02:17:23 +0200

    Use pretty print for federation json responses (#2801)

--- crates/apub/src/http/person.rs
+++ crates/apub/src/http/person.rs
@@ -37,9 +37,9 @@ pub(crate) async fn get_apub_person_http(
   if !person.deleted {
     let apub = person.into_json(&context).await?;
 
-    Ok(create_apub_response(&apub))
+    create_apub_response(&apub)
   } else {
-    Ok(create_apub_tombstone_response(person.actor_id.clone()))
+    create_apub_tombstone_response(person.actor_id.clone())
   }
 }
 
@@ -63,5 +63,5 @@ pub(crate) async fn get_apub_person_outbox(
   let person = Person::read_from_name(context.pool(), &info.user_name, false).await?;
   let outbox_id = generate_outbox_url(&person.actor_id)?.into();
   let outbox = EmptyOutbox::new(outbox_id)?;
-  Ok(create_apub_response(&outbox))
+  create_apub_response(&outbox)
 }
