cf53c0cd603cd6e5c7f37b3b1bff394594d8c22f
JetpackJackson <88674707+JetpackJackson@users.noreply.github.com>
2023-06-13 19:17:03 -0400

    Add files via upload

--- crates/routes/src/feeds.rs
+++ crates/routes/src/feeds.rs
@@ -401,7 +401,7 @@ fn create_reply_and_mention_items(
   let mut reply_items: Vec<Item> = replies
     .iter()
     .map(|r| {
-      let reply_url = format!("{}/comment/{}", protocol_and_hostname, r.comment.id);
+      let reply_url = format!("/comment/{}", protocol_and_hostname, r.comment.id);
       build_item(
         &r.creator.name,
         &r.comment.published,
@@ -415,10 +415,7 @@ fn create_reply_and_mention_items(
   let mut mention_items: Vec<Item> = mentions
     .iter()
     .map(|m| {
-      let mention_url = format!(
-        "/comment/{}",
-        protocol_and_hostname, m.post.id, m.comment.id
-      );
+      let mention_url = format!("/comment/{}", protocol_and_hostname, m.comment.id);
       build_item(
         &m.creator.name,
         &m.comment.published,
