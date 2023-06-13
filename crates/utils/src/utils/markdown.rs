8169224c375ba6d37094eb9ac04c2b9179ae54fa
Nina Blanson <nina.m.blanson@gmail.com>
2023-06-12 20:11:51 -0500

    Consolidate tests, add comments

--- crates/utils/src/utils/markdown.rs
+++ crates/utils/src/utils/markdown.rs
@@ -1,6 +1,6 @@
 use markdown_it::MarkdownIt;
 
-pub mod spoiler_rule;
+mod spoiler_rule;
 
 pub fn markdown_to_html(text: &str) -> String {
   let md = &mut MarkdownIt::new();
@@ -8,7 +8,7 @@ pub fn markdown_to_html(text: &str) -> String {
   markdown_it::plugins::extra::add(md);
   spoiler_rule::add(md);
 
-  md.parse(text).render()
+  md.parse(text).xrender()
 }
 
 #[cfg(test)]
@@ -16,7 +16,7 @@ mod tests {
   use crate::utils::markdown::markdown_to_html;
 
   #[test]
-  fn test_markdown() {
+  fn test_basic_markdown() {
     let tests: Vec<_> = vec![
       (
         "headings",
@@ -33,43 +33,19 @@ mod tests {
         "<p><strong>bold</strong> <strong>bold</strong> <em>italic</em> <em><strong>bold+italic</strong></em></p>\n"
       ),
       (
-        "basic blockquotes",
-        ">Nice quote",
-        "<blockquote>\n<p>Nice quote</p>\n</blockquote>\n"),
-      (
-        "blockquotes with multiple paragraphs",
-        "> Hello\n > \n > Goodbye",
-        "<blockquote>\n<p>Hello</p>\n<p>Goodbye</p>\n</blockquote>\n"
+        "blockquotes",
+        "> #### Hello\n > \n > - Hola\n > - 안영 \n>> Goodbye\n",
+        "<blockquote>\n<h4>Hello</h4>\n<ul>\n<li>Hola</li>\n<li>안영</li>\n</ul>\n<blockquote>\n<p>Goodbye</p>\n</blockquote>\n</blockquote>\n"
       ),
       (
-        "nested blockquotes",
-        "> Hello\n > \n >> Goodbye",
-        "<blockquote>\n<p>Hello</p>\n<blockquote>\n<p>Goodbye</p>\n</blockquote>\n</blockquote>\n"
+        "lists (ordered, unordered)",
+        "1. pen\n2. apple\n3. apple pen\n- pen\n- pineapple\n- pineapple pen",
+        "<ol>\n<li>pen</li>\n<li>apple</li>\n<li>apple pen</li>\n</ol>\n<ul>\n<li>pen</li>\n<li>pineapple</li>\n<li>pineapple pen</li>\n</ul>\n"
       ),
       (
-        "blockquotes with other elements",
-        "> #### Hello\n > \n > - Hola\n > - 안영\n",
-        "<blockquote>\n<h4>Hello</h4>\n<ul>\n<li>Hola</li>\n<li>안영</li>\n</ul>\n</blockquote>\n"
-      ),
-      (
-        "ordered lists",
-        "1. pen\n2. apple\n3. apple pen",
-        "<ol>\n<li>pen</li>\n<li>apple</li>\n<li>apple pen</li>\n</ol>\n"
-      ),
-      (
-        "unordered lists",
-        "- pen\n- apple\n- apple pen",
-        "<ul>\n<li>pen</li>\n<li>apple</li>\n<li>apple pen</li>\n</ul>\n"
-      ),
-      (
-        "code",
-        "this is my amazing `code snippet`",
-        "<p>this is my amazing <code>code snippet</code></p>\n"
-      ),
-      (
-        "code block",
-        "this is my amazing ```code block```",
-        "<p>this is my amazing <code>code block</code></p>\n"
+        "code and code blocks",
+        "this is my amazing `code snippet` and my amazing ```code block```",
+        "<p>this is my amazing <code>code snippet</code> and my amazing <code>code block</code></p>\n"
       ),
       (
         "links",
@@ -79,42 +55,7 @@ mod tests {
       (
         "images",
         "![My linked image](https://image.com \"image alt text\")",
-        "<p><img src=\"https://image.com\" alt=\"My linked image\" title=\"image alt text\"></p>\n"
-      ),
-      (
-        "invalid spoiler",
-        "::: spoiler click to see more\nbut I never finished",
-        "<p>::: spoiler click to see more\nbut I never finished</p>\n",
-      ),
-      (
-        "another invalid spoiler",
-        "::: spoiler\nnever added the lead in\n:::",
-        "<p>::: spoiler\nnever added the lead in\n:::</p>\n",
-      ),
-      (
-        "basic spoiler, but no newline at the end",
-        "::: spoiler click to see more\nhow spicy!\n:::",
-        "<details><summary>click to see more</summary><p>how spicy!\n</p></details>\n"
-      ),
-      (
-        "basic spoiler with a newline at the end.",
-        "::: spoiler click to see more\nhow spicy!\n:::\n",
-        "<details><summary>click to see more</summary><p>how spicy!\n</p></details>\n"
-      ),
-      (
-        "spoiler with extra markdown on the call to action. No special parsing will be done.",
-        "::: spoiler _click to see more_\nhow spicy!\n:::\n",
-        "<details><summary>_click to see more_</summary><p>how spicy!\n</p></details>\n"
-      ),
-      (
-        "spoiler with extra markdown in the fenced spoiler block.",
-        "::: spoiler click to see more\n**how spicy!**\n*i have many lines*\n:::\n",
-        "<details><summary>click to see more</summary><p><strong>how spicy!</strong>\n<em>i have many lines</em>\n</p></details>\n"
-      ),
-      (
-        "spoiler mixed with other content.",
-        "hey you\npsst, wanna hear a secret?\n::: spoiler lean in and i'll tell you\n**you are breathtaking!**\n:::\nwhatcha think about that?",
-        "<p>hey you\npsst, wanna hear a secret?</p>\n<details><summary>lean in and i'll tell you</summary><p><strong>you are breathtaking!</strong>\n</p></details>\n<p>whatcha think about that?</p>\n"
+        "<p><img src=\"https://image.com\" alt=\"My linked image\" title=\"image alt text\" /></p>\n"
       ),
     ];
 
