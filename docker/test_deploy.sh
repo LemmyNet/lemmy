b214d3dc00c269d7987ace7f5522e2ff406eec03
Dessalines <dessalines@users.noreply.github.com>
2023-06-20 20:29:48 -0400

    Adding current domain from settings to CORs. (#3231)

--- docker/test_deploy.sh
+++ docker/test_deploy.sh
@@ -5,8 +5,8 @@ export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
 
 # Rebuilding dev docker
-sudo docker build ../ -f . -t "dessalines/lemmy:dev"
-sudo docker push "dessalines/lemmy:dev"
+pushd ..
+sudo docker build . -f docker/Dockerfile --build-arg RUST_RELEASE_MODE=release -t "dessalines/lemmy:dev" --platform=linux/amd64 --push
 
 # Run the playbook
 # pushd ../../../lemmy-ansible
