bd441b9113c8b072152aa1f42597cfc9c88cc9e8
estork09 <estork@live.com>
2023-06-23 19:47:30 -0400

    Updated opentelemetry block to properly include log

--- src/lib.rs
+++ src/lib.rs
@@ -214,9 +214,10 @@ pub fn init_logging(opentelemetry_url: &Option<Url>) -> Result<(), LemmyError> {
   if let Some(_url) = opentelemetry_url {
     #[cfg(feature = "console")]
     telemetry::init_tracing(_url.as_ref(), subscriber, targets)?;
-    #[cfg(not(feature = "console"))]
+    #[cfg(not(feature = "console"))] {
     set_global_default(subscriber)?;
     tracing::error!("Feature `console` must be enabled for opentelemetry tracing");
+    }
   } else {
     set_global_default(subscriber)?;
   }
