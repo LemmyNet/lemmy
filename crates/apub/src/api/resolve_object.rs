8b97511c0671689a28056c31844490081c92e259
Scott Merchant <scott.merchant@octopus.com>
2023-06-24 12:48:28 +0930

    formatting

--- crates/apub/src/api/resolve_object.rs
+++ crates/apub/src/api/resolve_object.rs
@@ -4,7 +4,6 @@ use crate::{
 };
 use activitypub_federation::config::Data;
 use diesel::NotFound;
-use tracing::log::{log, logger, warn};
 use lemmy_api_common::{
   context::LemmyContext,
   site::{ResolveObject, ResolveObjectResponse},
@@ -14,6 +13,7 @@ use lemmy_db_schema::{newtypes::PersonId, source::local_site::LocalSite, utils::
 use lemmy_db_views::structs::{CommentView, PostView};
 use lemmy_db_views_actor::structs::{CommunityView, PersonView};
 use lemmy_utils::error::LemmyError;
+use tracing::log::warn;
 
 #[async_trait::async_trait]
 impl PerformApub for ResolveObject {
@@ -27,7 +27,9 @@ impl PerformApub for ResolveObject {
     let local_user_view = local_user_view_from_jwt(&self.auth, context).await?;
     let local_site = LocalSite::read(context.pool()).await?;
     let person_id = local_user_view.person.id;
+
     let check_private_instance_result = check_private_instance(&Some(local_user_view), &local_site);
+
     if let Err(e) = check_private_instance_result {
       warn!("Suppressed Error from check_private_instance: {}", e);
     }
@@ -35,15 +37,17 @@ impl PerformApub for ResolveObject {
     let object_id_result = search_query_to_object_id(&self.q, context)
       .await
       .map_err(|e| e.with_message("couldnt_find_object"));
+
     match object_id_result {
       Err(e) => warn!("Suppressed Error from search_query_to_object_id: {}", e),
       Ok(object_id) => {
         let convert_result = convert_response(object_id, person_id, context.pool())
-            .await
-            .map_err(|e| e.with_message("couldnt_find_object"));
+          .await
+          .map_err(|e| e.with_message("couldnt_find_object"));
+
         match convert_result {
           Err(e) => warn!("Suppressed Error from convert_response: {}", e),
-          Ok(convert) => return Ok(convert)
+          Ok(convert) => return Ok(convert),
         }
       }
     }
