203e35899e12e8696d0c871b5c12b27a5751b4a2
Neshura <neshura@protonmail.ch>
2023-06-26 10:47:01 +0200

    Add separate Post check for is_valid_body_field (#3263)
    * Add separate Post check for is_valid_body_field
    
    * Modify is_valid_body_check for posts only
    
    * Fix check var reinit in validation.rs
    
    * Extra empty line to rerun woodpecker with changes
    
    * Change Option to bool, add false to non-post calls
    
    * Woodpecker trick.. again
    
    * Probable rust_fmt fail fixed
    
    * cargo_clippy changes
    
    * Missing space between = and if
    
    * Remove ; after body length checks

--- crates/api_crud/src/site/update.rs
+++ crates/api_crud/src/site/update.rs
@@ -67,7 +67,7 @@ impl PerformCrud for EditSite {
       site_description_length_check(desc)?;
     }
 
-    is_valid_body_field(&data.sidebar)?;
+    is_valid_body_field(&data.sidebar, false)?;
 
     let application_question = diesel_option_overwrite(&data.application_question);
     check_application_question(
