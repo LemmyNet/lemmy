67f113728c4f00393c398249c3f8a78d8ace7cbf
dullbananas <dull.bananas0@gmail.com>
2023-06-27 13:17:38 -0700

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,10 +4,11 @@ set -e
 PACKAGE="$1"
 echo "$PACKAGE"
 
+source scripts/start_dev_db.sh
+
 psql -U lemmy -d postgres -c "DROP DATABASE lemmy;"
 psql -U lemmy -d postgres -c "CREATE DATABASE lemmy;"
 
-export LEMMY_DATABASE_URL=postgres://lemmy:password@localhost:5432/lemmy
 # tests are executed in working directory crates/api (or similar),
 # so to load the config we need to traverse to the repo root
 export LEMMY_CONFIG_LOCATION=../../config/config.hjson
