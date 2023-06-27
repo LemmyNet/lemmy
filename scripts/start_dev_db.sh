6d8813ef1568ea012dd8aa5ae01e69a042a7927d
dullbananas <dull.bananas0@gmail.com>
2023-06-27 12:51:23 -0700

    Update start_dev_db.sh

--- scripts/start_dev_db.sh
+++ scripts/start_dev_db.sh
@@ -5,4 +5,4 @@ export PGDATA="$PWD/dev_pgdata"
 rm -rf $PGDATA
 initdb --username=lemmy --auth=trust
 
-postgres -c listen-addresses=''
+postgres -c listen_addresses='' -c "unix_socket_directories='$PWD'"
