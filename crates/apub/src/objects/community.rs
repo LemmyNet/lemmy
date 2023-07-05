45b1a0d4fb491abe9aef3e70ada27d8bc8d2658e
phiresky <phireskyde+git@gmail.com>
2023-07-05 17:50:26 +0200

    improve performance of community followers inbox query (#3482)
    * improve performance of community followers inbox query
    
    * nightly format
    
    * force woodpecker to retry
    
    ---------
    
    Co-authored-by: Dessalines <dessalines@users.noreply.github.com>

--- crates/apub/src/objects/community.rs
+++ crates/apub/src/objects/community.rs
@@ -14,7 +14,6 @@ use activitypub_federation::{
   traits::{Actor, Object},
 };
 use chrono::NaiveDateTime;
-use itertools::Itertools;
 use lemmy_api_common::{
   context::LemmyContext,
   utils::{generate_featured_url, generate_moderators_url, generate_outbox_url},
@@ -188,17 +187,11 @@ impl ApubCommunity {
     let id = self.id;
 
     let local_site_data = local_site_data_cached(context.pool()).await?;
-    let follows = CommunityFollowerView::for_community(context.pool(), id).await?;
+    let follows = CommunityFollowerView::get_community_follower_inboxes(context.pool(), id).await?;
+
     let inboxes: Vec<Url> = follows
       .into_iter()
-      .filter(|f| !f.follower.local)
-      .map(|f| {
-        f.follower
-          .shared_inbox_url
-          .unwrap_or(f.follower.inbox_url)
-          .into()
-      })
-      .unique()
+      .map(Into::into)
       .filter(|inbox: &Url| inbox.host_str() != Some(&context.settings().hostname))
       // Don't send to blocked instances
       .filter(|inbox| check_apub_id_valid(inbox, &local_site_data).is_ok())
