88d60d22c56f638ec6dc62fa29f08f11614b4300
Nutomic <me@nutomic.com>
2023-07-10 15:00:55 +0200

    Upgrade all dependencies (#3526)
    * Upgrade all dependencies
    
    * as base64

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -1,4 +1,5 @@
 use actix_web::web::Data;
+use base64::{engine::general_purpose::STANDARD_NO_PAD as base64, Engine};
 use captcha::Captcha;
 use lemmy_api_common::{context::LemmyContext, utils::local_site_to_slur_regex};
 use lemmy_db_schema::source::local_site::LocalSite;
@@ -58,7 +59,7 @@ pub(crate) fn captcha_as_wav_base64(captcha: &Captcha) -> Result<String, LemmyEr
     ));
   }
 
-  Ok(base64::encode(output_buffer.into_inner()))
+  Ok(base64.encode(output_buffer.into_inner()))
 }
 
 /// Check size of report and remove whitespace
