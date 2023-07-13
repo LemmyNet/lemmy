7d8cb93b5323eb50bd90afde270685d1ecd07061
Nutomic <me@nutomic.com>
2023-07-13 16:12:01 +0200

    Check for dead federated instances (fixes #2221) (#3427)
    * Check for dead federated instances (fixes #2221)
    
    * move to apub crate, use timestamp
    
    * make it compile
    
    * clippy
    
    * use moka to cache blocklists, dead instances, restore orig scheduled tasks
    
    * remove leftover last_alive var
    
    * error handling
    
    * wip
    
    * fix alive check for instances without nodeinfo, add coalesce
    
    * clippy
    
    * move federation blocklist cache to #3486
    
    * unused deps

--- crates/db_schema/src/impls/instance.rs
+++ crates/db_schema/src/impls/instance.rs
@@ -1,10 +1,17 @@
 use crate::{
+  diesel::dsl::IntervalDsl,
   newtypes::InstanceId,
   schema::{federation_allowlist, federation_blocklist, instance},
   source::instance::{Instance, InstanceForm},
   utils::{get_conn, naive_now, DbPool},
 };
-use diesel::{dsl::insert_into, result::Error, ExpressionMethods, QueryDsl};
+use diesel::{
+  dsl::{insert_into, now},
+  result::Error,
+  sql_types::{Nullable, Timestamp},
+  ExpressionMethods,
+  QueryDsl,
+};
 use diesel_async::RunQueryDsl;
 
 impl Instance {
@@ -46,6 +53,24 @@ impl Instance {
       .execute(conn)
       .await
   }
+
+  pub async fn read_all(pool: &mut DbPool<'_>) -> Result<Vec<Instance>, Error> {
+    let conn = &mut get_conn(pool).await?;
+    instance::table
+      .select(instance::all_columns)
+      .get_results(conn)
+      .await
+  }
+
+  pub async fn dead_instances(pool: &mut DbPool<'_>) -> Result<Vec<String>, Error> {
+    let conn = &mut get_conn(pool).await?;
+    instance::table
+      .select(instance::domain)
+      .filter(coalesce(instance::updated, instance::published).lt(now - 3.days()))
+      .get_results(conn)
+      .await
+  }
+
   #[cfg(test)]
   pub async fn delete_all(pool: &mut DbPool<'_>) -> Result<usize, Error> {
     let conn = &mut get_conn(pool).await?;
@@ -79,3 +104,5 @@ impl Instance {
       .await
   }
 }
+
+sql_function! { fn coalesce(x: Nullable<Timestamp>, y: Timestamp) -> Timestamp; }
