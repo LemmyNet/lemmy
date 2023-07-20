71d5ae72a54dcf4765815e6707e5a3acf272b1ed
Felix Ableitner <me@nutomic.com>
2023-07-20 12:49:33 +0200

    Rewrite activity lists to fix delete federation (fixes #3625)

--- api_tests/run-federation-test.sh
+++ api_tests/run-federation-test.sh
@@ -13,7 +13,7 @@ popd
 yarn
 yarn api-test || true
 
-killall lemmy_server
+killall -s1 lemmy_server
 
 for INSTANCE in lemmy_alpha lemmy_beta lemmy_gamma lemmy_delta lemmy_epsilon; do
   psql "$LEMMY_DATABASE_URL" -c "DROP DATABASE $INSTANCE"
