4cd95af98fe2c9729783acfa0e27e927bb238068
dull b <dull.bananas0@gmail.com>
2023-07-21 19:25:04 +0000

    person_mention_view

--- crates/db_views_actor/src/person_mention_view.rs
+++ crates/db_views_actor/src/person_mention_view.rs
@@ -1,7 +1,9 @@
 use crate::structs::PersonMentionView;
 use diesel::{
   dsl::now,
+  pg::Pg,
   result::Error,
+  sql_types,
   BoolExpressionMethods,
   ExpressionMethods,
   JoinOnDsl,
@@ -11,6 +13,7 @@ use diesel::{
 use diesel_async::RunQueryDsl;
 use lemmy_db_schema::{
   aggregates::structs::CommentAggregates,
+  aliases,
   newtypes::{PersonId, PersonMentionId},
   schema::{
     comment,
@@ -34,7 +37,7 @@ use lemmy_db_schema::{
     post::Post,
   },
   traits::JoinView,
-  utils::{get_conn, limit_and_offset, DbPool},
+  utils::{get_conn, limit_and_offset, DbConn, DbPool, ListFn, Queries, ReadFn},
   CommentSortType,
 };
 
@@ -53,127 +56,22 @@ type PersonMentionViewTuple = (
   Option<i16>,
 );
 
-impl PersonMentionView {
-  pub async fn read(
-    pool: &mut DbPool<'_>,
-    person_mention_id: PersonMentionId,
-    my_person_id: Option<PersonId>,
-  ) -> Result<Self, Error> {
-    let conn = &mut get_conn(pool).await?;
-    let person_alias_1 = diesel::alias!(person as person1);
-
+fn queries<'a>() -> Queries<
+  impl ReadFn<'a, PersonMentionView, (PersonMentionId, Option<PersonId>)>,
+  impl ListFn<'a, PersonMentionView, PersonMentionQuery>,
+> {
+  let all_joins = |query: person_mention::BoxedQuery<'a, Pg>,
+                   include_expired_bans: bool,
+                   my_person_id: Option<PersonId>| {
     // The left join below will return None in this case
     let person_id_join = my_person_id.unwrap_or(PersonId(-1));
 
-    let res = person_mention::table
-      .find(person_mention_id)
+    query
       .inner_join(comment::table)
       .inner_join(person::table.on(comment::creator_id.eq(person::id)))
       .inner_join(post::table.on(comment::post_id.eq(post::id)))
       .inner_join(community::table.on(post::community_id.eq(community::id)))
-      .inner_join(person_alias_1)
-      .inner_join(comment_aggregates::table.on(comment::id.eq(comment_aggregates::comment_id)))
-      .left_join(
-        community_person_ban::table.on(
-          community::id
-            .eq(community_person_ban::community_id)
-            .and(community_person_ban::person_id.eq(comment::creator_id)),
-        ),
-      )
-      .left_join(
-        community_follower::table.on(
-          post::community_id
-            .eq(community_follower::community_id)
-            .and(community_follower::person_id.eq(person_id_join)),
-        ),
-      )
-      .left_join(
-        comment_saved::table.on(
-          comment::id
-            .eq(comment_saved::comment_id)
-            .and(comment_saved::person_id.eq(person_id_join)),
-        ),
-      )
-      .left_join(
-        person_block::table.on(
-          comment::creator_id
-            .eq(person_block::target_id)
-            .and(person_block::person_id.eq(person_id_join)),
-        ),
-      )
-      .left_join(
-        comment_like::table.on(
-          comment::id
-            .eq(comment_like::comment_id)
-            .and(comment_like::person_id.eq(person_id_join)),
-        ),
-      )
-      .select((
-        person_mention::all_columns,
-        comment::all_columns,
-        person::all_columns,
-        post::all_columns,
-        community::all_columns,
-        person_alias_1.fields(person::all_columns),
-        comment_aggregates::all_columns,
-        community_person_ban::all_columns.nullable(),
-        community_follower::all_columns.nullable(),
-        comment_saved::all_columns.nullable(),
-        person_block::all_columns.nullable(),
-        comment_like::score.nullable(),
-      ))
-      .first::<PersonMentionViewTuple>(conn)
-      .await?;
-
-    Ok(Self::from_tuple(res))
-  }
-
-  /// Gets the number of unread mentions
-  pub async fn get_unread_mentions(
-    pool: &mut DbPool<'_>,
-    my_person_id: PersonId,
-  ) -> Result<i64, Error> {
-    use diesel::dsl::count;
-    let conn = &mut get_conn(pool).await?;
-
-    person_mention::table
-      .inner_join(comment::table)
-      .filter(person_mention::recipient_id.eq(my_person_id))
-      .filter(person_mention::read.eq(false))
-      .filter(comment::deleted.eq(false))
-      .filter(comment::removed.eq(false))
-      .select(count(person_mention::id))
-      .first::<i64>(conn)
-      .await
-  }
-}
-
-#[derive(Default)]
-pub struct PersonMentionQuery {
-  pub my_person_id: Option<PersonId>,
-  pub recipient_id: Option<PersonId>,
-  pub sort: Option<CommentSortType>,
-  pub unread_only: Option<bool>,
-  pub show_bot_accounts: Option<bool>,
-  pub page: Option<i64>,
-  pub limit: Option<i64>,
-}
-
-impl PersonMentionQuery {
-  pub async fn list(self, pool: &mut DbPool<'_>) -> Result<Vec<PersonMentionView>, Error> {
-    let conn = &mut get_conn(pool).await?;
-
-    let person_alias_1 = diesel::alias!(person as person1);
-
-    // The left join below will return None in this case
-    let person_id_join = self.my_person_id.unwrap_or(PersonId(-1));
-
-    let mut query = person_mention::table
-      .inner_join(comment::table)
-      .inner_join(person::table.on(comment::creator_id.eq(person::id)))
-      .inner_join(post::table.on(comment::post_id.eq(post::id)))
-      .inner_join(community::table.on(post::community_id.eq(community::id)))
-      .inner_join(person_alias_1)
+      .inner_join(aliases::person1)
       .inner_join(comment_aggregates::table.on(comment::id.eq(comment_aggregates::comment_id)))
       .left_join(
         community_person_ban::table.on(
@@ -183,7 +81,9 @@ impl PersonMentionQuery {
             .and(
               community_person_ban::expires
                 .is_null()
-                .or(community_person_ban::expires.gt(now)),
+                .or(community_person_ban::expires.gt(now))
+                // TODO: avoid evaluation of expiration condition if include_expired_bans is true
+                .or::<_, sql_types::Nullable<sql_types::Bool>>(include_expired_bans),
             ),
         ),
       )
@@ -221,7 +121,7 @@ impl PersonMentionQuery {
         person::all_columns,
         post::all_columns,
         community::all_columns,
-        person_alias_1.fields(person::all_columns),
+        aliases::person1.fields(person::all_columns),
         comment_aggregates::all_columns,
         community_person_ban::all_columns.nullable(),
         community_follower::all_columns.nullable(),
@@ -229,36 +129,103 @@ impl PersonMentionQuery {
         person_block::all_columns.nullable(),
         comment_like::score.nullable(),
       ))
-      .into_boxed();
+  };
 
-    if let Some(recipient_id) = self.recipient_id {
+  let read =
+    move |mut conn: DbConn<'a>,
+          (person_mention_id, my_person_id): (PersonMentionId, Option<PersonId>)| async move {
+      all_joins(
+        person_mention::table.find(person_mention_id).into_boxed(),
+        true,
+        my_person_id,
+      )
+      .first::<PersonMentionViewTuple>(&mut conn)
+      .await
+    };
+
+  let list = move |mut conn: DbConn<'a>, options: PersonMentionQuery| async move {
+    let mut query = all_joins(
+      person_mention::table.into_boxed(),
+      false,
+      options.my_person_id,
+    );
+
+    if let Some(recipient_id) = options.recipient_id {
       query = query.filter(person_mention::recipient_id.eq(recipient_id));
     }
 
-    if self.unread_only.unwrap_or(false) {
+    if options.unread_only.unwrap_or(false) {
       query = query.filter(person_mention::read.eq(false));
     }
 
-    if !self.show_bot_accounts.unwrap_or(true) {
+    if !options.show_bot_accounts.unwrap_or(true) {
       query = query.filter(person::bot_account.eq(false));
     };
 
-    query = match self.sort.unwrap_or(CommentSortType::Hot) {
+    query = match options.sort.unwrap_or(CommentSortType::Hot) {
       CommentSortType::Hot => query.then_order_by(comment_aggregates::hot_rank.desc()),
       CommentSortType::New => query.then_order_by(comment::published.desc()),
       CommentSortType::Old => query.then_order_by(comment::published.asc()),
       CommentSortType::Top => query.order_by(comment_aggregates::score.desc()),
     };
 
-    let (limit, offset) = limit_and_offset(self.page, self.limit)?;
+    let (limit, offset) = limit_and_offset(options.page, options.limit)?;
 
-    let res = query
+    query
       .limit(limit)
       .offset(offset)
-      .load::<PersonMentionViewTuple>(conn)
-      .await?;
+      .load::<PersonMentionViewTuple>(&mut conn)
+      .await
+  };
 
-    Ok(res.into_iter().map(PersonMentionView::from_tuple).collect())
+  Queries::new(read, list)
+}
+
+impl PersonMentionView {
+  pub async fn read(
+    pool: &mut DbPool<'_>,
+    person_mention_id: PersonMentionId,
+    my_person_id: Option<PersonId>,
+  ) -> Result<Self, Error> {
+    queries()
+      .read(pool, (person_mention_id, my_person_id))
+      .await
+  }
+
+  /// Gets the number of unread mentions
+  pub async fn get_unread_mentions(
+    pool: &mut DbPool<'_>,
+    my_person_id: PersonId,
+  ) -> Result<i64, Error> {
+    use diesel::dsl::count;
+    let conn = &mut get_conn(pool).await?;
+
+    person_mention::table
+      .inner_join(comment::table)
+      .filter(person_mention::recipient_id.eq(my_person_id))
+      .filter(person_mention::read.eq(false))
+      .filter(comment::deleted.eq(false))
+      .filter(comment::removed.eq(false))
+      .select(count(person_mention::id))
+      .first::<i64>(conn)
+      .await
+  }
+}
+
+#[derive(Default)]
+pub struct PersonMentionQuery {
+  pub my_person_id: Option<PersonId>,
+  pub recipient_id: Option<PersonId>,
+  pub sort: Option<CommentSortType>,
+  pub unread_only: Option<bool>,
+  pub show_bot_accounts: Option<bool>,
+  pub page: Option<i64>,
+  pub limit: Option<i64>,
+}
+
+impl PersonMentionQuery {
+  pub async fn list(self, pool: &mut DbPool<'_>) -> Result<Vec<PersonMentionView>, Error> {
+    queries().list(pool, self).await
   }
 }
 
