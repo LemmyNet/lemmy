c80cc3a9f4d36983c5b3b2c7438da5f19ec7ae6f
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2023-07-22 09:28:52 -0700

    killall moved earlier in bash script

--- api_tests/run-federation-test.sh
+++ api_tests/run-federation-test.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-export LEMMY_DATABASE_URL=postgres://lemmy:password@localhost:5432
+export LEMMY_DATABASE_URL=postgres://lemmy:zbbs84952@localhost:5432
 export LEMMY_SYNCHRONOUS_FEDERATION=1 # currently this is true in debug by default, but still.
 pushd ..
 cargo build
 rm target/lemmy_server || true
+# remove any locks on database from previous failed tests
+killall -s1 lemmy_server
 cp target/debug/lemmy_server target/lemmy_server
 ./api_tests/prepare-drone-federation-test.sh
 popd
 
 yarn
 yarn api-test || true
-
-killall lemmy_server
 
 for INSTANCE in lemmy_alpha lemmy_beta lemmy_gamma lemmy_delta lemmy_epsilon; do
   psql "$LEMMY_DATABASE_URL" -c "DROP DATABASE $INSTANCE"
