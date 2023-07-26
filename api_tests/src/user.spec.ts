4bac4eae07454e7cdfb5faf8cc0422fcdc77794e
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2023-07-25 18:30:49 -0700

    enhanced user testing regarding user account delete impacting aggregates. two possible bugs identified in comments

--- api_tests/src/user.spec.ts
+++ api_tests/src/user.spec.ts
@@ -17,6 +17,7 @@ import {
   resolveComment,
   saveUserSettingsFederated,
   setupLogins,
+  followCommunity,
 } from "./shared";
 import { LemmyHttp } from "lemmy-js-client";
 import { GetPosts } from "lemmy-js-client/dist/types/GetPosts";
@@ -90,6 +91,19 @@ test("Delete user", async () => {
     .comment;
   expect(remoteComment).toBeDefined();
 
+  // subscribe to alpha community from user
+  await followCommunity(user, true, alphaCommunity.community.id);
+
+  // for aggregate checking, refresh community before account delete
+  let beforeAlphaCommunity = (
+    await resolveCommunity(alpha, "!main@lemmy-alpha:8541")
+  ).community;
+  if (!beforeAlphaCommunity) {
+    throw "Missing alpha community";
+  }
+  // also getSite
+  let beforeSite = await getSite(alpha);
+
   await deleteUser(user);
 
   await expect(resolvePost(alpha, localPost)).rejects.toBe(
@@ -104,6 +118,46 @@ test("Delete user", async () => {
   await expect(resolveComment(alpha, remoteComment)).rejects.toBe(
     "couldnt_find_object",
   );
+  // try to use the account that is now deleted
+  await expect(resolveBetaCommunity(user)).rejects.toBe("deleted");
+
+  // let alpha view the aftermath
+  let afterAlphaCommunity = (
+    await resolveCommunity(alpha, "!main@lemmy-alpha:8541")
+  ).community;
+  if (!afterAlphaCommunity) {
+    throw "Missing alpha community";
+  }
+  let afterSite = await getSite(alpha);
+
+  // possible bug in 0.18.2
+  // Does lemmy_server do a SQL DELETE of a person or only mark the deleted=true column? This trigger does not seem to actually fire:
+  //    CREATE TRIGGER site_aggregates_person_delete AFTER DELETE ON public.person FOR EACH ROW WHEN ((old.local = true)) EXECUTE FUNCTION public.site_aggregates_person_delete();
+  //  expect(beforeSite.site_view.counts.users).toBe(afterSite.site_view.counts.users + 1);
+
+  console.log(
+    "%d vs %d and %d vs %d",
+    beforeSite.site_view.counts.posts,
+    afterSite.site_view.counts.posts,
+    beforeAlphaCommunity?.counts.subscribers,
+    afterAlphaCommunity?.counts.subscribers,
+  );
+  expect(beforeSite.site_view.counts.posts).toBe(
+    afterSite.site_view.counts.posts + 2,
+  );
+  expect(beforeSite.site_view.counts.comments).toBe(
+    afterSite.site_view.counts.comments + 2,
+  );
+  expect(beforeAlphaCommunity?.counts.comments).toBe(
+    afterAlphaCommunity?.counts.comments + 1,
+  );
+  expect(beforeAlphaCommunity?.counts.posts).toBe(
+    afterAlphaCommunity?.counts.posts + 1,
+  );
+
+  // possible bug in 0.18.2
+  // does deleting a user account unsubscribe from communities?
+  //  expect(beforeAlphaCommunity?.counts.subscribers).toBe(afterAlphaCommunity?.counts.subscribers + 1);
 });
 
 test("Requests with invalid auth should be treated as unauthenticated", async () => {
