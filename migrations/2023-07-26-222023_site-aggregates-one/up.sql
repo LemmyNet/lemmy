666cb0b04f1831b81d42a373c7d4cbb4eb50f2cf
Dessalines <tyhou13@gmx.com>
2023-07-28 13:49:23 -0400

    Fixing sql lints.

--- migrations/2023-07-26-222023_site-aggregates-one/up.sql
+++ migrations/2023-07-26-222023_site-aggregates-one/up.sql
@@ -1,15 +1,32 @@
-create or replace function site_aggregates_site()
-returns trigger language plpgsql
-as $$
-begin
-  -- we only ever want to have a single value in site_aggregate because the site_aggregate triggers update all rows in that table.
-  -- a cleaner check would be to insert it for the local_site but that would break assumptions at least in the tests
-  IF (TG_OP = 'INSERT') AND NOT EXISTS (select id from site_aggregates limit 1) THEN
-    insert into site_aggregates (site_id) values (NEW.id);
-  ELSIF (TG_OP = 'DELETE') THEN
-    delete from site_aggregates where site_id = OLD.id;
-  END IF;
-  return null;
-end $$;
+CREATE OR REPLACE FUNCTION site_aggregates_site ()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    -- we only ever want to have a single value in site_aggregate because the site_aggregate triggers update all rows in that table.
+    -- a cleaner check would be to insert it for the local_site but that would break assumptions at least in the tests
+    IF (TG_OP = 'INSERT') AND NOT EXISTS (
+    SELECT
+        id
+    FROM
+        site_aggregates
+    LIMIT 1) THEN
+        INSERT INTO site_aggregates (site_id)
+            VALUES (NEW.id);
+    ELSIF (TG_OP = 'DELETE') THEN
+        DELETE FROM site_aggregates
+        WHERE site_id = OLD.id;
+    END IF;
+    RETURN NULL;
+END
+$$;
 
-delete from site_aggregates a where not exists (select id from local_site s where s.site_id = a.site_id);
+DELETE FROM site_aggregates a
+WHERE NOT EXISTS (
+        SELECT
+            id
+        FROM
+            local_site s
+        WHERE
+            s.site_id = a.site_id);
+
