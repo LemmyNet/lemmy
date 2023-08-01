d82194cfe9f98984a64353f6511bb73b4f08baa4
Nutomic <me@nutomic.com>
2023-08-01 15:53:36 +0200

    Rewrite some federation actions to remove Perform/SendActivity (ref #3670) (#3758)

--- crates/api_crud/src/comment/create.rs
+++ crates/api_crud/src/comment/create.rs
@@ -36,7 +36,6 @@ use lemmy_utils::{
     validation::is_valid_body_field,
   },
 };
-use std::ops::Deref;
 
 const MAX_COMMENT_DEPTH_LIMIT: usize = 100;
 
@@ -196,7 +195,7 @@ pub async fn create_comment(
 
   Ok(Json(
     build_comment_response(
-      context.deref(),
+      &context,
       inserted_comment.id,
       Some(local_user_view),
       data.form_id.clone(),
