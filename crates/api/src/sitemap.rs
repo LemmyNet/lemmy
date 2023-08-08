ff98b304ba7bad6c80651326f18709a750b2eaff
Lukas Trombach <TrombachL@gmail.com>
2023-08-09 00:13:38 +1200

    update sitemap generation
    - remove config value for query limit
    - adjust sitemap generation to query changes
    - tidy up error handling

--- crates/api/src/sitemap.rs
+++ crates/api/src/sitemap.rs
@@ -2,21 +2,15 @@ use actix_web::{
   http::header::{self, CacheDirective},
   web::Data,
   HttpResponse,
-  ResponseError,
 };
 use chrono::{DateTime, FixedOffset};
 use lemmy_api_common::context::LemmyContext;
 use lemmy_db_schema::{source::post::Post, utils::DbPool};
-use lemmy_utils::{error::LemmyResult, settings::SETTINGS};
+use lemmy_utils::error::LemmyResult;
 use sitemap_rs::{url::Url, url_set::UrlSet};
-use std::io::Write;
-use tracing::{error, info};
+use tracing::info;
 
-async fn generate_urlset<W: Write>(
-  pool: &mut DbPool<'_>,
-  writer: &mut W,
-  limit: i64,
-) -> LemmyResult<()> {
+async fn generate_urlset(pool: &mut DbPool<'_>, limit: i64) -> LemmyResult<UrlSet> {
   info!("Generating sitemap with latest {} posts...", limit);
 
   let posts = Post::list_for_sitemap(pool, limit).await?;
@@ -25,43 +19,32 @@ async fn generate_urlset<W: Write>(
 
   let mut urls = vec![];
   for post in posts {
-    if let Some(url) = post.url {
-      let entry = match Url::builder(url.to_string())
-        .last_modified(DateTime::from_utc(
-          post.published,
-          FixedOffset::east_opt(0).expect("Error setting timezone offset"), // TODO what is the proper timezone offset here?
-        ))
-        .build()
-      {
-        Ok(url_builder) => url_builder,
-        Err(_) => continue,
-      };
+    let entry = Url::builder(post.0.to_string())
+      .last_modified(DateTime::from_utc(
+        post.1,
+        FixedOffset::east_opt(0).expect("Error setting timezone offset"), // TODO what is the proper timezone offset here?
+      ))
+      .build()
+      .ok();
 
+    if let Some(entry) = entry {
       urls.push(entry);
     }
   }
 
-  let url_set = UrlSet::new(urls)?;
-
-  match url_set.write(writer) {
-    Ok(_) => {
-      info!("Successfully generated sitemap.xml");
-      Ok(())
-    }
-    Err(err) => Err(err.into()),
-  }
+  Ok(UrlSet::new(urls)?)
 }
 
-pub async fn get_sitemap(context: Data<LemmyContext>) -> HttpResponse {
+pub async fn get_sitemap(context: Data<LemmyContext>) -> LemmyResult<HttpResponse> {
   let mut buf = Vec::<u8>::new();
-  match generate_urlset(&mut context.pool(), &mut buf, SETTINGS.sitemap.max_posts).await {
-    Ok(_) => HttpResponse::Ok()
+  generate_urlset(&mut context.pool(), 50_000) // max number of entries for sitemap.xml
+    .await?
+    .write(&mut buf)?;
+
+  Ok(
+    HttpResponse::Ok()
       .content_type("application/xml")
-      .insert_header(header::CacheControl(vec![CacheDirective::MaxAge(3600u32)])) // TODO should max-age cache directive be configurable?
+      .insert_header(header::CacheControl(vec![CacheDirective::MaxAge(86_400)])) // 24 h
       .body(buf),
-    Err(err) => {
-      error!("{}", err);
-      err.error_response()
-    }
-  }
+  )
 }
