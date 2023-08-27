70a67c128f945c26af955c41d761fb41ba43720a
dullbananas <dull.bananas0@gmail.com>
2023-08-27 11:51:52 -0700

    Update site_view.rs

--- crates/db_views/src/site_view.rs
+++ crates/db_views/src/site_view.rs
@@ -11,7 +11,7 @@ use lemmy_db_schema::{
 impl SiteView {
   pub async fn read_local(pool: &mut DbPool<'_>) -> Result<Self, Error> {
     let conn = &mut get_conn(pool).await?;
-    let (mut site, local_site, local_site_rate_limit, counts) = site::table
+    let mut res = site::table
       .inner_join(local_site::table)
       .inner_join(
         local_site_rate_limit::table.on(local_site::id.eq(local_site_rate_limit::local_site_id)),
@@ -23,15 +23,10 @@ impl SiteView {
         local_site_rate_limit::all_columns,
         site_aggregates::all_columns,
       ))
-      .first::<(Site, LocalSite, LocalSiteRateLimit, SiteAggregates)>(conn)
+      .first::<SiteView>(conn)
       .await?;
 
-    site.private_key = None;
-    Ok(SiteView {
-      site,
-      local_site,
-      local_site_rate_limit,
-      counts,
-    })
+    res.site.private_key = None;
+    Ok(res)
   }
 }
