30b6e2305834ac0f65fe35212ad6ff6ea113a93a
dullbananas <dull.bananas0@gmail.com>
2023-08-27 14:17:56 -0700

    Update community_follower_view.rs

--- crates/db_views_actor/src/community_follower_view.rs
+++ crates/db_views_actor/src/community_follower_view.rs
@@ -11,11 +11,8 @@ use lemmy_db_schema::{
   newtypes::{CommunityId, DbUrl, PersonId},
   schema::{community, community_follower, person},
   source::{community::Community, person::Person},
-  traits::JoinView,
   utils::{get_conn, DbPool},
 };
-
-type CommunityFollowerViewTuple = (Community, Person);
 
 sql_function!(fn coalesce(x: diesel::sql_types::Nullable<diesel::sql_types::Text>, y: diesel::sql_types::Text) -> diesel::sql_types::Text);
 
@@ -52,7 +49,7 @@ impl CommunityFollowerView {
 
   pub async fn for_person(pool: &mut DbPool<'_>, person_id: PersonId) -> Result<Vec<Self>, Error> {
     let conn = &mut get_conn(pool).await?;
-    let res = community_follower::table
+    community_follower::table
       .inner_join(community::table)
       .inner_join(person::table)
       .select((community::all_columns, person::all_columns))
@@ -60,19 +57,7 @@ impl CommunityFollowerView {
       .filter(community::deleted.eq(false))
       .filter(community::removed.eq(false))
       .order_by(community::title)
-      .load::<CommunityFollowerViewTuple>(conn)
-      .await?;
-
-    Ok(res.into_iter().map(Self::from_tuple).collect())
-  }
-}
-
-impl JoinView for CommunityFollowerView {
-  type JoinTuple = CommunityFollowerViewTuple;
-  fn from_tuple(a: Self::JoinTuple) -> Self {
-    Self {
-      community: a.0,
-      follower: a.1,
-    }
+      .load::<CommunityFollowerView>(conn)
+      .await
   }
 }
