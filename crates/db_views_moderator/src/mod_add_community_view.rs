42d9938a655efd21105cc67da2ae6a07791a93fb
dullbananas <dull.bananas0@gmail.com>
2023-08-27 14:30:44 -0700

    Update mod_add_community_view.rs

--- crates/db_views_moderator/src/mod_add_community_view.rs
+++ crates/db_views_moderator/src/mod_add_community_view.rs
@@ -13,11 +13,8 @@ use lemmy_db_schema::{
   newtypes::PersonId,
   schema::{community, mod_add_community, person},
   source::{community::Community, moderator::ModAddCommunity, person::Person},
-  traits::JoinView,
   utils::{get_conn, limit_and_offset, DbPool},
 };
-
-type ModAddCommunityViewTuple = (ModAddCommunity, Option<Person>, Community, Person);
 
 impl ModAddCommunityView {
   pub async fn list(pool: &mut DbPool<'_>, params: ModlogListParams) -> Result<Vec<Self>, Error> {
@@ -58,26 +55,11 @@ impl ModAddCommunityView {
 
     let (limit, offset) = limit_and_offset(params.page, params.limit)?;
 
-    let res = query
+    query
       .limit(limit)
       .offset(offset)
       .order_by(mod_add_community::when_.desc())
-      .load::<ModAddCommunityViewTuple>(conn)
-      .await?;
-
-    let results = res.into_iter().map(Self::from_tuple).collect();
-    Ok(results)
-  }
-}
-
-impl JoinView for ModAddCommunityView {
-  type JoinTuple = ModAddCommunityViewTuple;
-  fn from_tuple(a: Self::JoinTuple) -> Self {
-    Self {
-      mod_add_community: a.0,
-      moderator: a.1,
-      community: a.2,
-      modded_person: a.3,
-    }
+      .load::<ModAddCommunityView>(conn)
+      .await
   }
 }
