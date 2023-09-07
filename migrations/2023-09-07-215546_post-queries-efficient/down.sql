b2bf8de3e6588ede20ced35912ebcd9fcae0754a
phiresky <phireskyde+git@gmail.com>
2023-09-07 20:19:03 +0200

    sql format

--- migrations/2023-09-07-215546_post-queries-efficient/down.sql
+++ migrations/2023-09-07-215546_post-queries-efficient/down.sql
@@ -16,23 +16,23 @@ DROP INDEX idx_post_aggregates_featured_community_published;
 
 DROP INDEX idx_post_aggregates_featured_community_score;
 
-CREATE INDEX idx_post_aggregates_featured_community_active ON post_aggregates(featured_community DESC, hot_rank_active DESC, published DESC);
+CREATE INDEX idx_post_aggregates_featured_community_active ON post_aggregates (featured_community DESC, hot_rank_active DESC, published DESC);
 
-CREATE INDEX idx_post_aggregates_featured_community_controversy ON post_aggregates(featured_community DESC, controversy_rank DESC);
+CREATE INDEX idx_post_aggregates_featured_community_controversy ON post_aggregates (featured_community DESC, controversy_rank DESC);
 
-CREATE INDEX idx_post_aggregates_featured_community_hot ON post_aggregates(featured_community DESC, hot_rank DESC, published DESC);
+CREATE INDEX idx_post_aggregates_featured_community_hot ON post_aggregates (featured_community DESC, hot_rank DESC, published DESC);
 
-CREATE INDEX idx_post_aggregates_featured_community_scaled ON post_aggregates(featured_community DESC, scaled_rank DESC, published DESC);
+CREATE INDEX idx_post_aggregates_featured_community_scaled ON post_aggregates (featured_community DESC, scaled_rank DESC, published DESC);
 
-CREATE INDEX idx_post_aggregates_featured_community_most_comments ON post_aggregates(featured_community DESC, comments DESC, published DESC);
+CREATE INDEX idx_post_aggregates_featured_community_most_comments ON post_aggregates (featured_community DESC, comments DESC, published DESC);
 
-CREATE INDEX idx_post_aggregates_featured_community_newest_comment_time ON post_aggregates(featured_community DESC, newest_comment_time DESC);
+CREATE INDEX idx_post_aggregates_featured_community_newest_comment_time ON post_aggregates (featured_community DESC, newest_comment_time DESC);
 
-CREATE INDEX idx_post_aggregates_featured_community_newest_comment_time_necro ON post_aggregates(featured_community DESC, newest_comment_time_necro DESC);
+CREATE INDEX idx_post_aggregates_featured_community_newest_comment_time_necro ON post_aggregates (featured_community DESC, newest_comment_time_necro DESC);
 
-CREATE INDEX idx_post_aggregates_featured_community_published ON post_aggregates(featured_community DESC, published DESC);
+CREATE INDEX idx_post_aggregates_featured_community_published ON post_aggregates (featured_community DESC, published DESC);
 
-CREATE INDEX idx_post_aggregates_featured_community_score ON post_aggregates(featured_community DESC, score DESC, published DESC);
+CREATE INDEX idx_post_aggregates_featured_community_score ON post_aggregates (featured_community DESC, score DESC, published DESC);
 
 DROP INDEX idx_post_aggregates_community_active;
 
