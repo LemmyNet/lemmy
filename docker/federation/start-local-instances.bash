a866b3424dd8fe22c8120ecc2a7e46ae821ce864
SleeplessOne1917 <abias1122@gmail.com>
2023-09-20 13:14:51 +0000

    Make local federated instances use comnpose file for build (#3968)
    * Make local federated instances use comnpose file for build
    
    * Remove commented out part of script

--- docker/federation/start-local-instances.bash
+++ docker/federation/start-local-instances.bash
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -e
 
-sudo docker-compose down
-
-sudo docker build ../../ --file ../Dockerfile -t lemmy-federation:latest
+sudo docker compose down
 
 for Item in alpha beta gamma delta epsilon ; do
   sudo mkdir -p volumes/pictrs_$Item
   sudo chown -R 991:991 volumes/pictrs_$Item
 done
 
-#sudo docker-compose pull --ignore-pull-failures || true
-sudo docker-compose up
+sudo docker compose up
