d17a190ad565a7f12aee46d6d0abae179e56ef55
SleeplessOne1917 <abias1122@gmail.com>
2023-09-20 08:38:30 -0400

    Remove commented out part of script

--- docker/federation/start-local-instances.bash
+++ docker/federation/start-local-instances.bash
@@ -8,5 +8,4 @@ for Item in alpha beta gamma delta epsilon ; do
   sudo chown -R 991:991 volumes/pictrs_$Item
 done
 
-#sudo docker compose pull --ignore-pull-failures || true
 sudo docker compose up
