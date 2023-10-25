73c7ac028811268e6a3f5dd243cca4f31ad082a6
dull b <dull.bananas0@gmail.com>
2023-10-25 03:13:17 +0000

    fmt

--- migrations/2023-10-24-030352_change_primary_keys_and_remove_some_id_columns/down.sql
+++ migrations/2023-10-24-030352_change_primary_keys_and_remove_some_id_columns/down.sql
@@ -1,3 +1,4 @@
 ALTER TABLE post_saved
     DROP CONSTRAINT post_saved_pkey,
     ADD COLUMN id serial PRIMARY KEY;
+
--- migrations/2023-10-24-030352_change_primary_keys_and_remove_some_id_columns/up.sql
+++ migrations/2023-10-24-030352_change_primary_keys_and_remove_some_id_columns/up.sql
@@ -1,3 +1,4 @@
 ALTER TABLE post_saved
     DROP COLUMN id,
     ADD PRIMARY KEY (post_id, person_id);
+
