f298e6c4d8dcd27aa0419a51b5db21ed2cec6ee1
phiresky <phireskyde+git@gmail.com>
2023-10-25 00:04:03 +0000

    feature gate

--- crates/db_schema/src/source/federation_queue_state.rs
+++ crates/db_schema/src/source/federation_queue_state.rs
@@ -1,10 +1,15 @@
 use crate::newtypes::{ActivityId, InstanceId};
 use chrono::{DateTime, Utc};
+#[cfg(feature = "full")]
 use diesel::prelude::*;
 
-#[derive(Queryable, Selectable, Insertable, AsChangeset, Clone)]
-#[diesel(table_name = crate::schema::federation_queue_state)]
-#[diesel(check_for_backend(diesel::pg::Pg))]
+#[derive(Clone)]
+#[cfg_attr(
+  feature = "full",
+  derive(Queryable, Selectable, Insertable, AsChangeset)
+)]
+#[cfg_attr(feature = "full", diesel(table_name = crate::schema::federation_queue_state))]
+#[cfg_attr(feature = "full", diesel(check_for_backend(diesel::pg::Pg)))]
 pub struct FederationQueueState {
   pub instance_id: InstanceId,
   pub last_successful_id: ActivityId, // todo: i64
