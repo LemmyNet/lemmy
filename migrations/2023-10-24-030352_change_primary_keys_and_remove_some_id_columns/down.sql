924900dbe96f31d11a9b4cb9ba7580ed6c733eff
dull b <dull.bananas0@gmail.com>
2023-10-28 16:00:12 +0000

    remove removal of not null

--- migrations/2023-10-24-030352_change_primary_keys_and_remove_some_id_columns/down.sql
+++ migrations/2023-10-24-030352_change_primary_keys_and_remove_some_id_columns/down.sql
@@ -7,8 +7,6 @@ CREATE INDEX idx_post_saved_person_id ON post_saved (person_id);
 
 ALTER TABLE post_saved
     ADD UNIQUE (post_id, person_id),
-    ALTER COLUMN post_id SET NOT NULL,
-    ALTER COLUMN person_id SET NOT NULL,
     DROP CONSTRAINT post_saved_pkey,
     ADD COLUMN id serial PRIMARY KEY;
 
--- migrations/2023-10-24-030352_change_primary_keys_and_remove_some_id_columns/up.sql
+++ migrations/2023-10-24-030352_change_primary_keys_and_remove_some_id_columns/up.sql
@@ -6,9 +6,7 @@ ALTER TABLE captcha_answer
 ALTER TABLE post_saved
     DROP COLUMN id,
     ADD PRIMARY KEY (person_id, post_id),
-    DROP CONSTRAINT post_saved_post_id_person_id_key,
-    ALTER COLUMN post_id DROP NOT NULL,
-    ALTER COLUMN person_id DROP NOT NULL;
+    DROP CONSTRAINT post_saved_post_id_person_id_key;
 
 DROP INDEX idx_post_saved_person_id;
 
