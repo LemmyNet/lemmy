b5d97b7a2629e48bafd445d01346ca02dfe5dc52
dull b <dull.bananas0@gmail.com>
2023-11-04 15:34:57 +0000

    fix site_aggregates_site function

--- migrations/2023-10-24-030352_change_primary_keys_and_remove_some_id_columns/down.sql
+++ migrations/2023-10-24-030352_change_primary_keys_and_remove_some_id_columns/down.sql
@@ -166,3 +166,26 @@ ALTER TABLE site_language
     DROP CONSTRAINT site_language_pkey,
     ADD COLUMN id serial PRIMARY KEY;
 
+CREATE OR REPLACE FUNCTION site_aggregates_site ()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    -- we only ever want to have a single value in site_aggregate because the site_aggregate triggers update all rows in that table.
+    -- a cleaner check would be to insert it for the local_site but that would break assumptions at least in the tests
+    IF (TG_OP = 'INSERT') AND NOT EXISTS (
+    SELECT
+        id
+    FROM
+        site_aggregates
+    LIMIT 1) THEN
+        INSERT INTO site_aggregates (site_id)
+            VALUES (NEW.id);
+    ELSIF (TG_OP = 'DELETE') THEN
+        DELETE FROM site_aggregates
+        WHERE site_id = OLD.id;
+    END IF;
+    RETURN NULL;
+END
+$$;
+
--- migrations/2023-10-24-030352_change_primary_keys_and_remove_some_id_columns/up.sql
+++ migrations/2023-10-24-030352_change_primary_keys_and_remove_some_id_columns/up.sql
@@ -181,3 +181,28 @@ ALTER TABLE site_language
     ADD PRIMARY KEY (site_id, language_id),
     DROP CONSTRAINT site_language_site_id_language_id_key;
 
+-- Change functions to not use the removed columns
+
+CREATE OR REPLACE FUNCTION site_aggregates_site ()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    -- we only ever want to have a single value in site_aggregate because the site_aggregate triggers update all rows in that table.
+    -- a cleaner check would be to insert it for the local_site but that would break assumptions at least in the tests
+    IF (TG_OP = 'INSERT') AND NOT EXISTS (
+    SELECT
+        *
+    FROM
+        site_aggregates
+    LIMIT 1) THEN
+        INSERT INTO site_aggregates (site_id)
+            VALUES (NEW.id);
+    ELSIF (TG_OP = 'DELETE') THEN
+        DELETE FROM site_aggregates
+        WHERE site_id = OLD.id;
+    END IF;
+    RETURN NULL;
+END
+$$;
+
