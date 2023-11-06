39b6fe9acb2c7d9e39f24438057f33bf3ad04a6d
Dessalines <tyhou13@gmx.com>
2023-11-06 16:13:53 -0500

    Adding is_debug_mode var.

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -85,8 +85,9 @@ pub fn read_auth_token(req: &HttpRequest) -> Result<Option<String>, LemmyError> 
     // ensure that its marked as httponly and secure
     let secure = cookie.secure().unwrap_or_default();
     let http_only = cookie.http_only().unwrap_or_default();
+    let is_debug_mode = cfg!(debug_assertions);
 
-    if !cfg!(debug_assertions) && (!secure || !http_only) {
+    if !is_debug_mode && (!secure || !http_only) {
       Err(LemmyError::from(LemmyErrorType::AuthCookieInsecure))
     } else {
       Ok(Some(cookie.value().to_string()))
