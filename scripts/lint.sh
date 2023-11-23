39621ed8fb79a7c13838c3cf5cb0e5c4cae2915b
Felix Ableitner <me@nutomic.com>
2023-11-23 12:11:08 +0100

    fix

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,7 +7,7 @@ cd $CWD/../
 
 # Run clippy with automatic fixing of problems. Specify features manually to exclude pict-rs
 cargo clippy --workspace --fix --allow-staged --allow-dirty --tests --all-targets \
-    --features console json-log -- -D warnings
+    --features console,json-log -- -D warnings
 
 # Format rust files
 cargo +nightly fmt
