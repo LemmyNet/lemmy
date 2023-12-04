e6c0751f6cdfc1792c713492a94f05e5c0693696
Kroese <kroese@users.noreply.github.com>
2023-12-04 16:35:59 +0100

    Keep cargo_fmt happy

--- crates/db_schema/src/impls/post.rs
+++ crates/db_schema/src/impls/post.rs
@@ -111,7 +111,7 @@ impl Post {
       .filter(removed.eq(false))
       .filter(published.ge(Utc::now().naive_utc() - Duration::days(7)))
       .order(published.desc())
-      .limit(50000)    
+      .limit(50000)
       .load::<(DbUrl, chrono::DateTime<Utc>)>(conn)
       .await
   }
