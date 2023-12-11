dba9445c5461d92b7ec4ef1f8eef90b86217064e
Felix Ableitner <me@nutomic.com>
2023-12-11 16:49:06 +0100

    Cache site response

--- crates/api_crud/src/site/read.rs
+++ crates/api_crud/src/site/read.rs
@@ -22,14 +22,48 @@ use lemmy_utils::{
   error::{LemmyError, LemmyErrorExt, LemmyErrorType},
   version,
 };
+use moka::future::Cache;
+use once_cell::sync::Lazy;
+use std::time::Duration;
 
 #[tracing::instrument(skip(context))]
 pub async fn get_site(
   local_user_view: Option<LocalUserView>,
   context: Data<LemmyContext>,
 ) -> Result<Json<GetSiteResponse>, LemmyError> {
-  // Build the local user
-  let my_user = if let Some(local_user_view) = local_user_view {
+  static CACHE: Lazy<Cache<(), GetSiteResponse>> = Lazy::new(|| {
+    Cache::builder()
+      .max_capacity(1)
+      .time_to_live(Duration::from_secs(60))
+      .build()
+  });
+
+  // This data is independent from the user account so we can cache it across requests
+  let mut site_response = CACHE
+    .try_get_with::<_, LemmyError>((), async {
+      let site_view = SiteView::read_local(&mut context.pool()).await?;
+      let admins = PersonView::admins(&mut context.pool()).await?;
+      let all_languages = Language::read_all(&mut context.pool()).await?;
+      let discussion_languages = SiteLanguage::read_local_raw(&mut context.pool()).await?;
+      let taglines = Tagline::get_all(&mut context.pool(), site_view.local_site.id).await?;
+      let custom_emojis =
+        CustomEmojiView::get_all(&mut context.pool(), site_view.local_site.id).await?;
+      Ok(GetSiteResponse {
+        site_view,
+        admins,
+        version: version::VERSION.to_string(),
+        my_user: None,
+        all_languages,
+        discussion_languages,
+        taglines,
+        custom_emojis,
+      })
+    })
+    .await
+    .map_err(|e| anyhow::anyhow!("Failed to construct site response: {e}"))?;
+
+  // Build the local user with parallel queries and add it to site response
+  site_response.my_user = if let Some(local_user_view) = local_user_view {
     let person_id = local_user_view.person.id;
     let local_user_id = local_user_view.local_user.id;
 
@@ -63,22 +97,5 @@ pub async fn get_site(
     None
   };
 
-  let site_view = SiteView::read_local(&mut context.pool()).await?;
-  let admins = PersonView::admins(&mut context.pool()).await?;
-  let all_languages = Language::read_all(&mut context.pool()).await?;
-  let discussion_languages = SiteLanguage::read_local_raw(&mut context.pool()).await?;
-  let taglines = Tagline::get_all(&mut context.pool(), site_view.local_site.id).await?;
-  let custom_emojis =
-    CustomEmojiView::get_all(&mut context.pool(), site_view.local_site.id).await?;
-
-  Ok(Json(GetSiteResponse {
-    site_view,
-    admins,
-    version: version::VERSION.to_string(),
-    my_user,
-    all_languages,
-    discussion_languages,
-    taglines,
-    custom_emojis,
-  }))
+  Ok(Json(site_response))
 }
