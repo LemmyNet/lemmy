7c56a13e9d1eff4ca7ef282cb530d82c0121dfb7
İsmail Karslı <cszn@pm.me>
2023-12-15 00:52:29 +0300

    Add local_subscribers count to follow.spec.ts

--- api_tests/src/follow.spec.ts
+++ api_tests/src/follow.spec.ts
@@ -24,17 +24,20 @@ test("Follow local community", async () => {
 
   let community = (await resolveBetaCommunity(user)).community!;
   expect(community.counts.subscribers).toBe(1);
+  expect(community.counts.local_subscribers).toBe(1);
   let follow = await followCommunity(user, true, community.community.id);
 
   // Make sure the follow response went through
   expect(follow.community_view.community.local).toBe(true);
   expect(follow.community_view.subscribed).toBe("Subscribed");
   expect(follow.community_view.counts.subscribers).toBe(2);
+  expect(follow.community_view.counts.local_subscribers).toBe(2);
 
   // Test an unfollow
   let unfollow = await followCommunity(user, false, community.community.id);
   expect(unfollow.community_view.subscribed).toBe("NotSubscribed");
   expect(unfollow.community_view.counts.subscribers).toBe(1);
+  expect(unfollow.community_view.counts.local_subscribers).toBe(1);
 });
 
 test("Follow federated community", async () => {
@@ -42,6 +45,8 @@ test("Follow federated community", async () => {
   if (!betaCommunity) {
     throw "Missing beta community";
   }
+  expect(betaCommunity.counts.subscribers).toBe(1);
+  expect(betaCommunity.counts.local_subscribers).toBe(0);
   let follow = await followCommunity(alpha, true, betaCommunity.community.id);
   expect(follow.community_view.subscribed).toBe("Pending");
   betaCommunity = (
@@ -55,10 +60,12 @@ test("Follow federated community", async () => {
   expect(betaCommunity?.community.local).toBe(false);
   expect(betaCommunity?.community.name).toBe("main");
   expect(betaCommunity?.subscribed).toBe("Subscribed");
+  expect(betaCommunity?.counts.local_subscribers).toBe(1);
 
   // check that unfollow was federated
   let communityOnBeta1 = await resolveBetaCommunity(beta);
   expect(communityOnBeta1.community?.counts.subscribers).toBe(2);
+  expect(communityOnBeta1.community?.counts.local_subscribers).toBe(1);
 
   // Check it from local
   let site = await getSite(alpha);
@@ -83,4 +90,5 @@ test("Follow federated community", async () => {
   // check that unfollow was federated
   let communityOnBeta2 = await resolveBetaCommunity(beta);
   expect(communityOnBeta2.community?.counts.subscribers).toBe(1);
+  expect(communityOnBeta2.community?.counts.local_subscribers).toBe(1);
 });
