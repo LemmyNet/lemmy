4a55d4f87185a38e00a8cc32c69040c7f869b531
Nutomic <me@nutomic.com>
2023-12-14 13:25:47 +0100

    Quick fix for Peertube federation (fixes #4261) (#4264)
    * Quick fix for Peertube federation (fixes #4261)
    
    * prettier
    
    * apub assets should be included in slow check paths

--- crates/apub/src/protocol/objects/group.rs
+++ crates/apub/src/protocol/objects/group.rs
@@ -57,6 +57,7 @@ pub struct Group {
   pub(crate) summary: Option<String>,
   #[serde(deserialize_with = "deserialize_skip_error", default)]
   pub(crate) source: Option<Source>,
+  #[serde(deserialize_with = "deserialize_skip_error", default)]
   pub(crate) icon: Option<ImageObject>,
   /// banner
   pub(crate) image: Option<ImageObject>,
