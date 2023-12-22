d858b87ae3bb9cc76afa7b0e8c19f31f41368cfd
dullbananas <dull.bananas0@gmail.com>
2023-12-22 00:16:20 -0700

    Update up.sql

--- migrations/2023-12-22-040137_make-mixed-sorting-directions-work-with-tuple-comparison/up.sql
+++ migrations/2023-12-22-040137_make-mixed-sorting-directions-work-with-tuple-comparison/up.sql
@@ -2,11 +2,11 @@ CREATE FUNCTION reverse_timestamp_sort (t timestamp with time zone)
     RETURNS numeric
     AS $$
 BEGIN
-    RETURN -EXTRACT(EPOCH FROM t);
+    RETURN - EXTRACT(EPOCH FROM t);
 END;
 $$
 LANGUAGE plpgsql
 IMMUTABLE PARALLEL SAFE;
 
-CREATE INDEX idx_post_aggregates_community_published_asc ON public.post_aggregates USING btree (community_id, featured_local DESC, reverse_timestamp_sort(published) DESC);
+CREATE INDEX idx_post_aggregates_community_published_asc ON public.post_aggregates USING btree (community_id, featured_local DESC, reverse_timestamp_sort (published) DESC);
 
