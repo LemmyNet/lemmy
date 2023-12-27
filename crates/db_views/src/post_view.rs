fb283215840a3c8ce49526bb47ce8b85fd715bde
dullbananas <dull.bananas0@gmail.com>
2023-12-26 20:14:42 -0700

    Update post_view.rs

--- crates/db_views/src/post_view.rs
+++ crates/db_views/src/post_view.rs
@@ -482,6 +482,12 @@ fn queries<'a>() -> Queries<
       SortType::TopNineMonths => query.then_desc(key::score).filter(time(9.months())),
     };
 
+    query = options.sort.unwrap_or(SortType::Hot) {
+      // A second time-based sort would not be very useful
+      SortType::New | SortType::Old | SortType::NewComments => query,
+      _ => query.then_desc(key::published),
+    };
+
     query = query.then_desc(key::post_id);
 
     // Not done by debug_query
