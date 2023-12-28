0de659cf3cc6403bc78c4c7e52b04a39942c599c
İsmail Karslı <cszn@pm.me>
2023-12-28 11:23:30 +0300

    Add subscribers_local to community_aggregates

--- migrations/2023-12-12-150828_community_aggregates_add_local_subscribers/down.sql
+++ migrations/2023-12-12-150828_community_aggregates_add_local_subscribers/down.sql
@@ -1,7 +1,38 @@
 ALTER TABLE community_aggregates
     DROP COLUMN subscribers_local;
 
-DROP TRIGGER IF EXISTS community_aggregates_subscriber_local_count ON community_follower;
-
-DROP FUNCTION IF EXISTS community_aggregates_subscriber_local_count;
+-- old function from migrations/2023-10-02-145002_community_followers_count_federated/up.sql
+-- The subscriber count should only be updated for local communities. For remote
+-- communities it is read over federation from the origin instance.
+CREATE OR REPLACE FUNCTION community_aggregates_subscriber_count ()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    IF (TG_OP = 'INSERT') THEN
+        UPDATE
+            community_aggregates
+        SET
+            subscribers = subscribers + 1
+        FROM
+            community
+        WHERE
+            community.id = community_id
+            AND community.local
+            AND community_id = NEW.community_id;
+    ELSIF (TG_OP = 'DELETE') THEN
+        UPDATE
+            community_aggregates
+        SET
+            subscribers = subscribers - 1
+        FROM
+            community
+        WHERE
+            community.id = community_id
+            AND community.local
+            AND community_id = OLD.community_id;
+    END IF;
+    RETURN NULL;
+END
+$$;
 
--- migrations/2023-12-12-150828_community_aggregates_add_local_subscribers/up.sql
+++ migrations/2023-12-12-150828_community_aggregates_add_local_subscribers/up.sql
@@ -1,10 +1,10 @@
 -- Couldn't find a way to put subscribers_local right after subscribers except recreating the table.
 ALTER TABLE community_aggregates
-    ADD COLUMN subscribers_local int8 NOT NULL DEFAULT 0;
+    ADD COLUMN subscribers_local bigint NOT NULL DEFAULT 0;
 
 -- update initial value
 UPDATE
-    community_aggregates
+    community_aggregates ca
 SET
     subscribers_local = (
         SELECT
@@ -12,7 +12,7 @@ SET
         FROM
             community_follower cf
         WHERE
-            cf.community_id = community_aggregates.community_id
+            cf.community_id = ca.community_id
             AND (
                 SELECT
                     local
@@ -21,50 +21,39 @@ SET
                 WHERE
                     person.id = cf.person_id));
 
-CREATE OR REPLACE FUNCTION community_aggregates_subscriber_local_count ()
+-- subscribers should be updated only when a local community is followed by a local or remote person
+-- subscribers_local should be updated only when a local person follows a local or remote community
+CREATE OR REPLACE FUNCTION community_aggregates_subscriber_count ()
     RETURNS TRIGGER
     LANGUAGE plpgsql
     AS $$
 BEGIN
     IF (TG_OP = 'INSERT') THEN
         UPDATE
-            community_aggregates
+            community_aggregates ca
         SET
-            subscribers_local = subscribers_local + 1
+            subscribers = subscribers + community.local::int,
+            subscribers_local = subscribers_local + person.local::int
         FROM
-            community c
-        WHERE
-            c.id = NEW.community_id
-            AND (
-                SELECT
-                    local
-                FROM
-                    person
-                WHERE
-                    id = NEW.person_id);
+            community
+        LEFT JOIN person ON person.id = NEW.person_id
+    WHERE
+        community.id = NEW.community_id
+            AND community.id = ca.community_id;
     ELSIF (TG_OP = 'DELETE') THEN
         UPDATE
-            community_aggregates
+            community_aggregates ca
         SET
-            subscribers_local = subscribers_local - 1
+            subscribers = subscribers - community.local::int,
+            subscribers_local = subscribers_local - person.local::int
         FROM
-            community c
-        WHERE
-            c.id = OLD.community_id
-            AND (
-                SELECT
-                    local
-                FROM
-                    person
-                WHERE
-                    id = OLD.person_id);
+            community
+        LEFT JOIN person ON person.id = OLD.person_id
+    WHERE
+        community.id = OLD.community_id
+            AND community.id = ca.community_id;
     END IF;
     RETURN NULL;
 END
 $$;
-
-CREATE TRIGGER community_aggregates_subscriber_local_count
-    AFTER INSERT OR DELETE ON community_follower
-    FOR EACH ROW
-    EXECUTE PROCEDURE community_aggregates_subscriber_local_count ();
 
