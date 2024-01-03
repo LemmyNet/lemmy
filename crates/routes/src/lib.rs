4163e0465e11ae78c0c0d9931205179605637590
Nutomic <me@nutomic.com>
2024-01-03 16:34:03 +0100

    Dont ignore errors during login (fixes #4319) (#4321)
    * Dont ignore errors during login (fixes #4319)
    
    * fix test
    
    * fmt

--- crates/routes/src/lib.rs
+++ crates/routes/src/lib.rs
@@ -1,6 +1,6 @@
 use lemmy_api_common::{claims::Claims, context::LemmyContext, utils::check_user_valid};
 use lemmy_db_views::structs::LocalUserView;
-use lemmy_utils::error::LemmyError;
+use lemmy_utils::error::{LemmyError, LemmyErrorExt2, LemmyErrorType};
 
 pub mod feeds;
 pub mod images;
@@ -12,7 +12,9 @@ async fn local_user_view_from_jwt(
   jwt: &str,
   context: &LemmyContext,
 ) -> Result<LocalUserView, LemmyError> {
-  let local_user_id = Claims::validate(jwt, context).await?;
+  let local_user_id = Claims::validate(jwt, context)
+    .await
+    .with_lemmy_type(LemmyErrorType::NotLoggedIn)?;
   let local_user_view = LocalUserView::read(&mut context.pool(), local_user_id).await?;
   check_user_valid(&local_user_view.person)?;
 
