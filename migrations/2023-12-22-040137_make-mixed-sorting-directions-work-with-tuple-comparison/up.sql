a3844ecfb0211579362ad944cd76c51eba3a2803
dullbananas <dull.bananas0@gmail.com>
2024-01-12 19:57:34 -0700

    Update up.sql

--- migrations/2023-12-22-040137_make-mixed-sorting-directions-work-with-tuple-comparison/up.sql
+++ migrations/2023-12-22-040137_make-mixed-sorting-directions-work-with-tuple-comparison/up.sql
@@ -10,3 +10,9 @@ IMMUTABLE PARALLEL SAFE;
 
 CREATE INDEX idx_post_aggregates_community_published_asc ON public.post_aggregates USING btree (community_id, featured_local DESC, reverse_timestamp_sort (published) DESC);
 
+CREATE INDEX idx_post_aggregates_featured_community_published_asc ON public.post_aggregates USING btree (community_id, featured_community DESC, reverse_timestamp_sort (published) DESC);
+
+CREATE INDEX idx_post_aggregates_featured_local_published_asc ON public.post_aggregates USING btree (featured_local DESC, reverse_timestamp_sort (published) DESC);
+
+CREATE INDEX idx_post_aggregates_published_asc ON public.post_aggregates USING btree (reverse_timestamp_sort (published) DESC);
+
