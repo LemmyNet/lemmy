c31226182e1ca12596a5982330fd4bb07903abf2
SleeplessOne1917 <insomnia-void@protonmail.com>
2024-01-22 09:13:02 -0500

    Allow community mods to see votes in addition to admins

--- crates/api/src/post/list_post_likes.rs
+++ crates/api/src/post/list_post_likes.rs
@@ -2,9 +2,9 @@ use actix_web::web::{Data, Json, Query};
 use lemmy_api_common::{
   context::LemmyContext,
   post::{ListPostLikes, ListPostLikesResponse},
-  utils::is_admin,
+  utils::is_mod_or_admin,
 };
-use lemmy_db_views::structs::{LocalUserView, VoteView};
+use lemmy_db_views::structs::{LocalUserView, PostView, VoteView};
 use lemmy_utils::error::LemmyError;
 
 /// Lists likes for a post
@@ -14,8 +14,19 @@ pub async fn list_post_likes(
   context: Data<LemmyContext>,
   local_user_view: LocalUserView,
 ) -> Result<Json<ListPostLikesResponse>, LemmyError> {
-  // Make sure user is an admin
-  is_admin(&local_user_view)?;
+  let post_view = PostView::read(
+    &mut context.pool(),
+    data.post_id,
+    Some(local_user_view.person.id),
+    false,
+  )
+  .await?;
+  is_mod_or_admin(
+    &mut context.pool(),
+    &local_user_view.person,
+    post_view.community.id,
+  )
+  .await?;
 
   let post_likes =
     VoteView::list_for_post(&mut context.pool(), data.post_id, data.page, data.limit).await?;
