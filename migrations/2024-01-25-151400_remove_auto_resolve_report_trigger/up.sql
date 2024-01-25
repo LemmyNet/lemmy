e3511e3e26639bffbd96ce63a12be9bd7599af5d
Dessalines <tyhou13@gmx.com>
2024-01-25 10:18:39 -0500

    Removing auto-resolve report triggers.

---
+++ migrations/2024-01-25-151400_remove_auto_resolve_report_trigger/up.sql
@@ -0,0 +1,7 @@
+DROP TRIGGER IF EXISTS post_removed_resolve_reports ON mod_remove_post;
+
+DROP FUNCTION IF EXISTS post_removed_resolve_reports;
+
+DROP TRIGGER IF EXISTS comment_removed_resolve_reports ON mod_remove_comment;
+
+DROP FUNCTION IF EXISTS comment_removed_resolve_reports;
