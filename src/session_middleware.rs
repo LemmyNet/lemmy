173a871585d2874c55b96cf1ee0b18fc1a9b43d9
dullbananas <dull.bananas0@gmail.com>
2024-01-25 07:19:42 -0700

    Update session_middleware.rs

--- src/session_middleware.rs
+++ src/session_middleware.rs
@@ -71,6 +71,7 @@ where
         // TODO: this means it will be impossible to get any error message for invalid jwt. Need
         //       to add a separate endpoint for that.
         //       https://github.com/LemmyNet/lemmy/issues/3702
+        panic!("e");
         let local_user_view = local_user_view_from_jwt(jwt, &context).await.ok();
         if let Some(local_user_view) = local_user_view {
           req.extensions_mut().insert(local_user_view);
