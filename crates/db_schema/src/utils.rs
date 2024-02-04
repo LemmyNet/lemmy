c7c4f73119ac5f5fb22b552ca2d4b5b576c93f66
dullbananas <dull.bananas0@gmail.com>
2024-02-04 09:29:56 -0700

    Update utils.rs

--- crates/db_schema/src/utils.rs
+++ crates/db_schema/src/utils.rs
@@ -339,7 +339,9 @@ fn establish_connection(config: &str) -> BoxFuture<ConnectionResult<AsyncPgConne
       }
     });
     let mut conn = AsyncPgConnection::try_from(client).await?;
-    conn.batch_execute("SET geqo_threshold=12;SET fro_collapse_limit=11;SET join_collapse_limit=11;").await?;
+    conn
+      .batch_execute("SET geqo_threshold=12;SET fro_collapse_limit=11;SET join_collapse_limit=11;")
+      .await?;
     Ok(conn)
   };
   fut.boxed()
