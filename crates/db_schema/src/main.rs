acd543fa42899ed93ab7adecb3fce7c1f0e6efdb
dullbananas <dull.bananas0@gmail.com>
2024-02-04 10:23:33 -0700

    Update main.rs

--- crates/db_schema/src/main.rs
+++ crates/db_schema/src/main.rs
@@ -3,7 +3,7 @@ use lemmy_utils::error::LemmyError;
 /// Runs schema setup
 #[tokio::main]
 async fn main() -> Result<(), LemmyError> {
-  crate::utils::build_db_pool().await?;
+  lemmy_db_schema::utils::build_db_pool().await?;
 
   Ok(())
 }
