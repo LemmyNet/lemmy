8edd94c5dc6df267381456f49bb552bd318ee7a0
dullbananas <dull.bananas0@gmail.com>
2024-02-10 14:49:18 -0700

    Delete crates/db_schema/src/main.rs

--- crates/db_schema/src/main.rs
+++
@@ -1,9 +0,0 @@
-use lemmy_utils::error::LemmyError;
-
-/// Runs schema setup
-#[tokio::main]
-async fn main() -> Result<(), LemmyError> {
-  lemmy_db_schema::utils::build_db_pool().await?;
-
-  Ok(())
-}
