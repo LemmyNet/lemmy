c93994b7c92860f46edd688ec886f1e2ee75d36c
Dull Bananas <dull.bananas0@gmail.com>
2024-02-25 16:45:38 +0000

    community_block

--- crates/db_schema/src/impls/community_block.rs
+++ crates/db_schema/src/impls/community_block.rs
@@ -1,19 +1,34 @@
 use crate::{
   newtypes::{CommunityId, PersonId},
-  schema::community_block::dsl::{community_block, community_id, person_id},
+  schema::community_actions,
   source::community_block::{CommunityBlock, CommunityBlockForm},
   traits::Blockable,
-  utils::{get_conn, DbPool},
+  utils::{get_conn, now, DbPool},
 };
+use chrono::{DateTime, Utc};
 use diesel::{
-  dsl::{exists, insert_into},
+  dsl::{self, exists, insert_into},
   result::Error,
   select,
+  ExpressionMethods,
+  NullableExpressionMethods,
   QueryDsl,
 };
 use diesel_async::RunQueryDsl;
 
 impl CommunityBlock {
+  fn as_select_unwrap() -> (
+    community_actions::person_id,
+    community_actions::community_id,
+    dsl::AssumeNotNull<community_actions::blocked>,
+  ) {
+    (
+      community_actions::person_id,
+      community_actions::community_id,
+      community_actions::blocked.assume_not_null(),
+    )
+  }
+
   pub async fn read(
     pool: &mut DbPool<'_>,
     for_person_id: PersonId,
@@ -21,7 +36,9 @@ impl CommunityBlock {
   ) -> Result<bool, Error> {
     let conn = &mut get_conn(pool).await?;
     select(exists(
-      community_block.find((for_person_id, for_community_id)),
+      community_actions::table
+        .find((for_person_id, for_community_id))
+        .filter(community_actions::blocked.is_not_null()),
     ))
     .get_result(conn)
     .await
@@ -33,9 +50,16 @@ impl Blockable for CommunityBlock {
   type Form = CommunityBlockForm;
   async fn block(pool: &mut DbPool<'_>, community_block_form: &Self::Form) -> Result<Self, Error> {
     let conn = &mut get_conn(pool).await?;
-    insert_into(community_block)
+    let community_block_form = (
+      community_block_form,
+      community_actions::blocked.eq(now().nullable()),
+    );
+    insert_into(community_actions::table)
       .values(community_block_form)
-      .on_conflict((person_id, community_id))
+      .on_conflict((
+        community_actions::person_id,
+        community_actions::community_id,
+      ))
       .do_update()
       .set(community_block_form)
       .get_result::<Self>(conn)
@@ -46,10 +70,11 @@ impl Blockable for CommunityBlock {
     community_block_form: &Self::Form,
   ) -> Result<usize, Error> {
     let conn = &mut get_conn(pool).await?;
-    diesel::delete(community_block.find((
+    diesel::update(community_actions::table.find((
       community_block_form.person_id,
       community_block_form.community_id,
     )))
+    .set(community_actions::blocked.eq(None::<DateTime<Utc>>))
     .execute(conn)
     .await
   }
--- crates/db_schema/src/source/community_block.rs
+++ crates/db_schema/src/source/community_block.rs
@@ -1,19 +1,16 @@
 use crate::newtypes::{CommunityId, PersonId};
 #[cfg(feature = "full")]
-use crate::schema::community_block;
+use crate::schema::community_actions;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
-#[cfg_attr(
-  feature = "full",
-  derive(Queryable, Selectable, Associations, Identifiable)
-)]
+#[cfg_attr(feature = "full", derive(Queryable, Associations, Identifiable))]
 #[cfg_attr(
   feature = "full",
   diesel(belongs_to(crate::source::community::Community))
 )]
-#[cfg_attr(feature = "full", diesel(table_name = community_block))]
+#[cfg_attr(feature = "full", diesel(table_name = community_actions))]
 #[cfg_attr(feature = "full", diesel(primary_key(person_id, community_id)))]
 #[cfg_attr(feature = "full", diesel(check_for_backend(diesel::pg::Pg)))]
 pub struct CommunityBlock {
@@ -23,7 +20,7 @@ pub struct CommunityBlock {
 }
 
 #[cfg_attr(feature = "full", derive(Insertable, AsChangeset))]
-#[cfg_attr(feature = "full", diesel(table_name = community_block))]
+#[cfg_attr(feature = "full", diesel(table_name = community_actions))]
 pub struct CommunityBlockForm {
   pub person_id: PersonId,
   pub community_id: CommunityId,
