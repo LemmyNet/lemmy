5293929644c11666702219df4f9f2e68c8e43291
Dull Bananas <dull.bananas0@gmail.com>
2024-02-25 16:54:29 +0000

    instance_block

--- crates/db_schema/src/impls/instance_block.rs
+++ crates/db_schema/src/impls/instance_block.rs
@@ -1,19 +1,34 @@
 use crate::{
   newtypes::{InstanceId, PersonId},
-  schema::instance_block::dsl::{instance_block, instance_id, person_id},
+  schema::instance_actions,
   source::instance_block::{InstanceBlock, InstanceBlockForm},
   traits::Blockable,
-  utils::{get_conn, DbPool},
+  utils::{get_conn, now, DbPool},
 };
+use chrono::{DateTime, Utc};
 use diesel::{
-  dsl::{exists, insert_into},
+  dsl::{self, exists, insert_into},
   result::Error,
   select,
+  ExpressionMethods,
+  NullableExpressionMethods,
   QueryDsl,
 };
 use diesel_async::RunQueryDsl;
 
 impl InstanceBlock {
+  fn as_select_unwrap() -> (
+    instance_actions::person_id,
+    instance_actions::instance_id,
+    dsl::AssumeNotNull<instance_actions::blocked>,
+  ) {
+    (
+      instance_actions::person_id,
+      instance_actions::instance_id,
+      instance_actions::blocked.assume_not_null(),
+    )
+  }
+
   pub async fn read(
     pool: &mut DbPool<'_>,
     for_person_id: PersonId,
@@ -21,7 +36,9 @@ impl InstanceBlock {
   ) -> Result<bool, Error> {
     let conn = &mut get_conn(pool).await?;
     select(exists(
-      instance_block.find((for_person_id, for_instance_id)),
+      instance_actions::table
+        .find((for_person_id, for_instance_id))
+        .filter(instance_actions::blocked.is_not_null()),
     ))
     .get_result(conn)
     .await
@@ -33,11 +50,16 @@ impl Blockable for InstanceBlock {
   type Form = InstanceBlockForm;
   async fn block(pool: &mut DbPool<'_>, instance_block_form: &Self::Form) -> Result<Self, Error> {
     let conn = &mut get_conn(pool).await?;
-    insert_into(instance_block)
+    let instance_block_form = (
+      instance_block_form,
+      instance_actions::blocked.eq(now().nullable()),
+    );
+    insert_into(instance_actions::table)
       .values(instance_block_form)
-      .on_conflict((person_id, instance_id))
+      .on_conflict((instance_actions::person_id, instance_actions::instance_id))
       .do_update()
       .set(instance_block_form)
+      .returning(Self::as_select_unwrap())
       .get_result::<Self>(conn)
       .await
   }
@@ -46,10 +68,11 @@ impl Blockable for InstanceBlock {
     instance_block_form: &Self::Form,
   ) -> Result<usize, Error> {
     let conn = &mut get_conn(pool).await?;
-    diesel::delete(instance_block.find((
+    diesel::update(instance_actions::table.find((
       instance_block_form.person_id,
       instance_block_form.instance_id,
     )))
+    .set(instance_actions::blocked.eq(None::<DateTime<Utc>>))
     .execute(conn)
     .await
   }
--- crates/db_schema/src/source/instance_block.rs
+++ crates/db_schema/src/source/instance_block.rs
@@ -1,19 +1,16 @@
 use crate::newtypes::{InstanceId, PersonId};
 #[cfg(feature = "full")]
-use crate::schema::instance_block;
+use crate::schema::instance_actions;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
-#[cfg_attr(
-  feature = "full",
-  derive(Queryable, Selectable, Associations, Identifiable)
-)]
+#[cfg_attr(feature = "full", derive(Queryable, Associations, Identifiable))]
 #[cfg_attr(
   feature = "full",
   diesel(belongs_to(crate::source::instance::Instance))
 )]
-#[cfg_attr(feature = "full", diesel(table_name = instance_block))]
+#[cfg_attr(feature = "full", diesel(table_name = instance_actions))]
 #[cfg_attr(feature = "full", diesel(primary_key(person_id, instance_id)))]
 #[cfg_attr(feature = "full", diesel(check_for_backend(diesel::pg::Pg)))]
 pub struct InstanceBlock {
@@ -23,7 +20,7 @@ pub struct InstanceBlock {
 }
 
 #[cfg_attr(feature = "full", derive(Insertable, AsChangeset))]
-#[cfg_attr(feature = "full", diesel(table_name = instance_block))]
+#[cfg_attr(feature = "full", diesel(table_name = instance_actions))]
 pub struct InstanceBlockForm {
   pub person_id: PersonId,
   pub instance_id: InstanceId,
