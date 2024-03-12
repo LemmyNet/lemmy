9e1a7f2f56e92fe9a884e5e0d6ad35d6f002d0b3
flamingos-cant <flamingos-cant@flamingos-cant.xyz>
2024-03-12 02:31:16 +0000

    SQL format.

--- migrations/2024-03-06-201637_url_blocklist/up.sql
+++ migrations/2024-03-06-201637_url_blocklist/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE local_site_url_blocklist (
     id serial NOT NULL PRIMARY KEY,
-    url TEXT NOT NULL UNIQUE,
+    url text NOT NULL UNIQUE,
     published timestamp with time zone NOT NULL DEFAULT now(),
     updated timestamp with time zone
 );
