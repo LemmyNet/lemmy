d6cbd1baa4b7d243a2f6a5ebc12450b240dd7d72
flamingos-cant <flamingos-cant@flamingos-cant.xyz>
2024-03-13 16:15:15 +0000

    Add missing macro.

--- crates/db_schema/src/source/local_site_url_blocklist.rs
+++ crates/db_schema/src/source/local_site_url_blocklist.rs
@@ -2,9 +2,11 @@
 use crate::schema::local_site_url_blocklist;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 #[cfg(feature = "full")]
 use ts_rs::TS;
 
+#[skip_serializing_none]
 #[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
 #[cfg_attr(feature = "full", derive(Queryable, Selectable, Identifiable, TS))]
 #[cfg_attr(feature = "full", diesel(table_name = local_site_url_blocklist))]
