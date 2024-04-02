14ec2845ebedf074c877361b47517f3f2eb64cdf
Felix Ableitner <me@nutomic.com>
2024-04-02 12:56:47 +0200

    Cache result of LocalSite::read to avoid unnecessary db calls

--- crates/db_schema/src/impls/local_site.rs
+++ crates/db_schema/src/impls/local_site.rs
@@ -5,6 +5,10 @@ use crate::{
 };
 use diesel::{dsl::insert_into, result::Error};
 use diesel_async::RunQueryDsl;
+use lemmy_utils::error::LemmyError;
+use moka::future::Cache;
+use once_cell::sync::Lazy;
+use std::time::Duration;
 
 impl LocalSite {
   pub async fn create(pool: &mut DbPool<'_>, form: &LocalSiteInsertForm) -> Result<Self, Error> {
@@ -14,9 +18,21 @@ impl LocalSite {
       .get_result::<Self>(conn)
       .await
   }
-  pub async fn read(pool: &mut DbPool<'_>) -> Result<Self, Error> {
-    let conn = &mut get_conn(pool).await?;
-    local_site.first::<Self>(conn).await
+  pub async fn read(pool: &mut DbPool<'_>) -> Result<Self, LemmyError> {
+    static CACHE: Lazy<Cache<(), LocalSite>> = Lazy::new(|| {
+      Cache::builder()
+        .max_capacity(1)
+        .time_to_live(Duration::from_secs(1))
+        .build()
+    });
+    Ok(
+      CACHE
+        .try_get_with((), async {
+          let conn = &mut get_conn(pool).await?;
+          local_site.first::<Self>(conn).await
+        })
+        .await?,
+    )
   }
   pub async fn update(pool: &mut DbPool<'_>, form: &LocalSiteUpdateForm) -> Result<Self, Error> {
     let conn = &mut get_conn(pool).await?;
