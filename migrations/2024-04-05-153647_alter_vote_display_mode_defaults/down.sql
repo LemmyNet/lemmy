6efd025470c761fe29d18eec1fc789c53fa675f9
Dessalines <tyhou13@gmx.com>
2024-04-09 19:44:52 -0400

    Drop and re-add columns instead.

--- migrations/2024-04-05-153647_alter_vote_display_mode_defaults/down.sql
+++ migrations/2024-04-05-153647_alter_vote_display_mode_defaults/down.sql
@@ -1,6 +1,10 @@
 ALTER TABLE local_user_vote_display_mode
-    ALTER COLUMN upvotes SET DEFAULT FALSE,
-    ALTER COLUMN downvotes SET DEFAULT FALSE,
-    ALTER COLUMN score SET DEFAULT TRUE,
-    ALTER COLUMN upvote_percentage SET DEFAULT TRUE;
+    DROP COLUMN score,
+    ADD COLUMN score boolean DEFAULT TRUE NOT NULL,
+    DROP COLUMN upvotes,
+    ADD COLUMN upvotes boolean DEFAULT FALSE NOT NULL,
+    DROP COLUMN downvotes,
+    ADD COLUMN downvotes boolean DEFAULT FALSE NOT NULL,
+    DROP COLUMN upvote_percentage,
+    ADD COLUMN upvote_percentage boolean DEFAULT TRUE NOT NULL;
 
--- migrations/2024-04-05-153647_alter_vote_display_mode_defaults/up.sql
+++ migrations/2024-04-05-153647_alter_vote_display_mode_defaults/up.sql
@@ -3,18 +3,12 @@
 -- Rather than
 -- Score + upvote_percentage
 ALTER TABLE local_user_vote_display_mode
-    ALTER COLUMN upvotes SET DEFAULT TRUE,
-    ALTER COLUMN downvotes SET DEFAULT TRUE,
-    ALTER COLUMN score SET DEFAULT FALSE,
-    ALTER COLUMN upvote_percentage SET DEFAULT FALSE;
-
--- Regenerate the rows with the new default
-DELETE FROM local_user_vote_display_mode;
-
--- Re-insert them
-INSERT INTO local_user_vote_display_mode (local_user_id)
-SELECT
-    id
-FROM
-    local_user;
+    DROP COLUMN score,
+    ADD COLUMN score boolean DEFAULT FALSE NOT NULL,
+    DROP COLUMN upvotes,
+    ADD COLUMN upvotes boolean DEFAULT TRUE NOT NULL,
+    DROP COLUMN downvotes,
+    ADD COLUMN downvotes boolean DEFAULT TRUE NOT NULL,
+    DROP COLUMN upvote_percentage,
+    ADD COLUMN upvote_percentage boolean DEFAULT FALSE NOT NULL;
 
