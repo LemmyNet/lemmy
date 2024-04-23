f92fde11891f9496aa9c2a3b113637d386d91ed1
Kroese <kroese@users.noreply.github.com>
2024-04-23 16:52:21 +0200

    Keep cargo_fmt happy

--- crates/api_common/src/request.rs
+++ crates/api_common/src/request.rs
@@ -103,9 +103,13 @@ pub fn generate_post_link_metadata(
     else if federated_thumbnail.is_some() {
       federated_thumbnail
     }
-    // Generate a local thumbnail if allowed
+    // Generate local thumbnail if allowed
     else if allow_generate_thumbnail {
-      match post.url.filter(|_| is_image_post).or(metadata.opengraph_data.image) {
+      match post
+        .url
+        .filter(|_| is_image_post)
+        .or(metadata.opengraph_data.image)
+      {
         Some(url) => generate_pictrs_thumbnail(&url, &context).await.ok(),
         None => None,
       }
