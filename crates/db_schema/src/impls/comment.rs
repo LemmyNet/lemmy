7c00bcc8a535a0ab75b57859f2be225b04d117fa
dullbananas <dull.bananas0@gmail.com>
2024-05-24 20:11:40 -0700

    Update comment.rs

--- crates/db_schema/src/impls/comment.rs
+++ crates/db_schema/src/impls/comment.rs
@@ -375,7 +375,6 @@ mod tests {
 
     let expected_comment_like = CommentLike {
       comment_id: inserted_comment.id,
-      post_id: inserted_post.id,
       person_id: inserted_person.id,
       published: inserted_comment_like.published,
       score: 1,
