b2d0ff5c83d3d879a1bfac2dd0378169b7df75ae
dullbananas <dull.bananas0@gmail.com>
2024-05-25 08:55:21 -0700

    Update down.sql

--- migrations/2024-03-17-134311_smoosh-tables-together/down.sql
+++ migrations/2024-03-17-134311_smoosh-tables-together/down.sql
@@ -38,9 +38,16 @@ ALTER TABLE comment_like
     DROP COLUMN saved,
     ADD COLUMN post_id int REFERENCES post ON UPDATE CASCADE ON DELETE CASCADE;
 
-UPDATE comment_like
+UPDATE
+    comment_like
 SET
-    post_id = (SELECT post_id FROM comment WHERE comment.id = comment_like.comment_id);
+    post_id = (
+        SELECT
+            post_id
+        FROM
+            comment
+        WHERE
+            comment.id = comment_like.comment_id);
 
 ALTER TABLE comment_like
     ALTER COLUMN post_id SET NOT NULL;
