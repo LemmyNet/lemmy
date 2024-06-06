eafa00b5e138915323f44f6e00a05ef3fdc26d72
dullbananas <dull.bananas0@gmail.com>
2024-06-05 19:44:25 -0700

    Update community_moderators.rs

--- crates/apub/src/collections/community_moderators.rs
+++ crates/apub/src/collections/community_moderators.rs
@@ -129,7 +129,7 @@ mod tests {
     let inserted_instance =
       Instance::read_or_create(&mut context.pool(), "my_domain.tld".to_string()).await?;
 
-    let old_mod = PersonInsertForm::new_local("holly", inserted_instance.id);
+    let old_mod = PersonInsertForm::new_local("holly", inserted_instance.id)?;
 
     let old_mod = Person::create(&mut context.pool(), &old_mod).await?;
     let community_moderator_form = CommunityModeratorForm {
