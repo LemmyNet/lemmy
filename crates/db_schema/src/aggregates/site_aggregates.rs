c5fb09aa3736c0d5de1753dba2f39569b6371731
dullbananas <dull.bananas0@gmail.com>
2024-06-05 19:06:28 -0700

    Update site_aggregates.rs

--- crates/db_schema/src/aggregates/site_aggregates.rs
+++ crates/db_schema/src/aggregates/site_aggregates.rs
@@ -42,11 +42,7 @@ mod tests {
       .await
       .unwrap();
 
-    let new_person = PersonInsertForm::builder()
-      .name("thommy_site_agg".into())
-      .public_key("pubkey".to_string())
-      .instance_id(inserted_instance.id)
-      .build();
+    let new_person = PersonInsertForm::new_local("thommy_site_agg", inserted_instance.id);
 
     let inserted_person = Person::create(pool, &new_person).await.unwrap();
 
