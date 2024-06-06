151d99d0884e25193de52c18714923532182e53e
dullbananas <dull.bananas0@gmail.com>
2024-06-05 19:19:29 -0700

    Update person_aggregates.rs

--- crates/db_schema/src/aggregates/person_aggregates.rs
+++ crates/db_schema/src/aggregates/person_aggregates.rs
@@ -49,19 +49,11 @@ mod tests {
       .await
       .unwrap();
 
-    let new_person = PersonInsertForm::builder()
-      .name("thommy_user_agg".into())
-      .public_key("pubkey".to_string())
-      .instance_id(inserted_instance.id)
-      .build();
+    let new_person = PersonInsertForm::new_local("thommy_user_agg", inserted_instance.id);
 
     let inserted_person = Person::create(pool, &new_person).await.unwrap();
 
-    let another_person = PersonInsertForm::builder()
-      .name("jerry_user_agg".into())
-      .public_key("pubkey".to_string())
-      .instance_id(inserted_instance.id)
-      .build();
+    let another_person = PersonInsertForm::new_local("jerry_user_agg", inserted_instance.id);
 
     let another_inserted_person = Person::create(pool, &another_person).await.unwrap();
 
