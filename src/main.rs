691872892001078fba4bf683daddddef60b708e2
Jax Gauthier <jax@gauthier.id>
2024-06-17 14:40:17 -0400

    Update main.rs
    rustls not rusttls

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ pub async fn main() -> LemmyResult<()> {
   init_logging(&SETTINGS.opentelemetry_url)?;
   let args = CmdArgs::parse();
 
-  rusttls::crypto::ring::default_provider()
+  rustls::crypto::ring::default_provider()
     .install_default()
-    .expect("Failed to install rusttls crypto provider");
+    .expect("Failed to install rustls crypto provider");
 
   #[cfg(not(feature = "embed-pictrs"))]
   start_lemmy_server(args).await?;
