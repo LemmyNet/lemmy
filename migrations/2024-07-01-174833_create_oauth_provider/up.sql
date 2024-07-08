d422844d711198f3f58af4a581225a133271f9bc
privacyguard <privacyguard@users.noreply.github.com>
2024-07-09 02:54:06 +0300

    merge migrations into a single file

--- migrations/2024-07-01-174833_create_oauth_provider/up.sql
+++ migrations/2024-07-01-174833_create_oauth_provider/up.sql
@@ -22,3 +22,14 @@ CREATE TABLE oauth_provider (
 ALTER TABLE local_site
     ADD COLUMN oauth_registration boolean DEFAULT FALSE NOT NULL;
 
+CREATE TABLE oauth_account (
+    id serial PRIMARY KEY,
+    local_user_id int REFERENCES local_user ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    oauth_provider_id bigint NOT NULL,
+    oauth_user_id text NOT NULL,
+    published timestamp with time zone DEFAULT now() NOT NULL,
+    updated timestamp with time zone,
+    UNIQUE (oauth_provider_id, oauth_user_id),
+    UNIQUE (oauth_provider_id, local_user_id)
+);
+
