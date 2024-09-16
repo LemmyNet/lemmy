5febf2b8fb59b7c477828d0c1a0e8dc74e69f0dd
Dessalines <dessalines@users.noreply.github.com>
2024-09-16 11:15:41 -0400

    Adding clearurls crate to clean tracking params from links and markdown. (#5018)
    * Adding clearurls crate to clean tracking params from links and markdown.
    
    - Thanks to @jenrdikw for creating this
    - Fixes #4905
    
    * Upgrading to new version of clearurls
    
    * Fix clippy

--- crates/api_common/src/utils.rs
+++ crates/api_common/src/utils.rs
@@ -49,6 +49,7 @@ use lemmy_utils::{
   utils::{
     markdown::{markdown_check_for_blocked_urls, markdown_rewrite_image_links},
     slurs::{build_slur_regex, remove_slurs},
+    validation::clean_urls_in_text,
   },
   CACHE_DURATION_FEDERATION,
 };
@@ -947,6 +948,7 @@ pub async fn process_markdown(
   context: &LemmyContext,
 ) -> LemmyResult<String> {
   let text = remove_slurs(text, slur_regex);
+  let text = clean_urls_in_text(&text);
 
   markdown_check_for_blocked_urls(&text, url_blocklist)?;
 
