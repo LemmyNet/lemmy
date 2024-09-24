71f1dd9c499d589f7e23949ae6ea6a54b213a0d6
Felix Ableitner <me@nutomic.com>
2024-09-24 11:49:42 +0200

    Cleanup remaining use of Result<bool, Error> (fixes #4862)

--- crates/db_schema/src/impls/oauth_account.rs
+++ crates/db_schema/src/impls/oauth_account.rs
@@ -1,48 +1,18 @@
 use crate::{
-  newtypes::{LocalUserId, OAuthProviderId},
+  newtypes::LocalUserId,
   schema::{oauth_account, oauth_account::dsl::local_user_id},
   source::oauth_account::{OAuthAccount, OAuthAccountInsertForm},
   utils::{get_conn, DbPool},
 };
-use diesel::{
-  dsl::{exists, insert_into},
-  result::Error,
-  select,
-  ExpressionMethods,
-  QueryDsl,
-};
+use diesel::{insert_into, result::Error, ExpressionMethods, QueryDsl};
 use diesel_async::RunQueryDsl;
 
 impl OAuthAccount {
-  pub async fn read(
-    pool: &mut DbPool<'_>,
-    for_oauth_provider_id: OAuthProviderId,
-    for_local_user_id: LocalUserId,
-  ) -> Result<bool, Error> {
-    let conn = &mut get_conn(pool).await?;
-    select(exists(
-      oauth_account::table.find((for_oauth_provider_id, for_local_user_id)),
-    ))
-    .get_result(conn)
-    .await
-  }
-
   pub async fn create(pool: &mut DbPool<'_>, form: &OAuthAccountInsertForm) -> Result<Self, Error> {
     let conn = &mut get_conn(pool).await?;
     insert_into(oauth_account::table)
       .values(form)
       .get_result::<Self>(conn)
-      .await
-  }
-
-  pub async fn delete(
-    pool: &mut DbPool<'_>,
-    for_oauth_provider_id: OAuthProviderId,
-    for_local_user_id: LocalUserId,
-  ) -> Result<usize, Error> {
-    let conn = &mut get_conn(pool).await?;
-    diesel::delete(oauth_account::table.find((for_oauth_provider_id, for_local_user_id)))
-      .execute(conn)
       .await
   }
 
