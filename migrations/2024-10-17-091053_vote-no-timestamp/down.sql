719efb28bc35405a991bcba46a7b22929d7d514d
Felix Ableitner <me@nutomic.com>
2024-10-17 12:06:55 +0200

    restore value of comment_like.post_id

--- migrations/2024-10-17-091053_vote-no-timestamp/down.sql
+++ migrations/2024-10-17-091053_vote-no-timestamp/down.sql
@@ -1,6 +1,23 @@
 ALTER TABLE post_like
     ADD COLUMN published timestamptz NOT NULL DEFAULT now();
+
 ALTER TABLE comment_like
     ADD COLUMN published timestamptz NOT NULL DEFAULT now();
+
 ALTER TABLE comment_like
-    ADD COLUMN post_id int NOT NULL;
+    ADD COLUMN post_id int REFERENCES post ON UPDATE CASCADE ON DELETE CASCADE;
+
+UPDATE
+    comment_like
+SET
+    post_id = (
+        SELECT
+            post_id
+        FROM
+            comment
+        WHERE
+            comment.id = comment_like.comment_id);
+
+ALTER TABLE comment_like
+    ALTER COLUMN post_id SET NOT NULL;
+
--- migrations/2024-10-17-091053_vote-no-timestamp/up.sql
+++ migrations/2024-10-17-091053_vote-no-timestamp/up.sql
@@ -1,7 +1,9 @@
 ALTER TABLE post_like
     DROP published;
+
 ALTER TABLE comment_like
     DROP published;
+
 ALTER TABLE comment_like
     DROP post_id;
 
