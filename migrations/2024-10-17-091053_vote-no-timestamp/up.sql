019bf67de012d6daca05a56d7f62cb0f1508dded
Felix Ableitner <me@nutomic.com>
2024-10-17 14:22:36 +0200

    set columns null before dropping

--- migrations/2024-10-17-091053_vote-no-timestamp/up.sql
+++ migrations/2024-10-17-091053_vote-no-timestamp/up.sql
@@ -1,3 +1,30 @@
+-- set all column values to null to reclaim disk space
+-- https://dba.stackexchange.com/a/117513
+ALTER TABLE post_like
+    ALTER COLUMN published DROP NOT NULL;
+
+UPDATE
+    post_like
+SET
+    published = NULL;
+
+ALTER TABLE comment_like
+    ALTER COLUMN published DROP NOT NULL;
+
+UPDATE
+    comment_like
+SET
+    published = NULL;
+
+ALTER TABLE comment_like
+    ALTER COLUMN post_id DROP NOT NULL;
+
+UPDATE
+    post_like
+SET
+    post_id = NULL;
+
+-- drop the columns
 ALTER TABLE post_like
     DROP published;
 
