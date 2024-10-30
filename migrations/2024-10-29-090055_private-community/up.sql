879dc4a9ea05fadd51482be16515d007de32f168
Felix Ableitner <me@nutomic.com>
2024-10-30 11:26:10 +0100

    references

--- migrations/2024-10-29-090055_private-community/up.sql
+++ migrations/2024-10-29-090055_private-community/up.sql
@@ -34,7 +34,7 @@ ALTER TABLE community_follower RENAME COLUMN pending TO state;
 -- Add column for mod who approved the private community follower
 -- Dont use foreign key here, otherwise joining to person table doesnt work easily
 ALTER TABLE community_follower
-    ADD COLUMN approver_id int;
+    ADD COLUMN approver_id int REFERENCES person ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- Enable signed fetch, necessary to fetch content in private communities
 ALTER TABLE ONLY local_site
