f8f035f3842a402476da3b18b17be87dc17e393d
Dessalines <dessalines@users.noreply.github.com>
2024-11-13 03:45:17 -0500

    Fixing cors origin wildcard. (#5194)
    * Fixing cors origin wildcard.
    
    - Fixes #5185
    
    * Add other allows to specified origin block.
    
    * Fix clippy.

--- src/lib.rs
+++ src/lib.rs
@@ -353,24 +353,32 @@ fn create_http_server(
 fn cors_config(settings: &Settings) -> Cors {
   let self_origin = settings.get_protocol_and_hostname();
   let cors_origin_setting = settings.cors_origin();
+
+  // A default setting for either wildcard, or None
+  let cors_default = Cors::default()
+    .allow_any_origin()
+    .allow_any_method()
+    .allow_any_header()
+    .expose_any_header()
+    .max_age(3600);
+
   match (cors_origin_setting.clone(), cfg!(debug_assertions)) {
     (Some(origin), false) => {
       // Need to call send_wildcard() explicitly, passing this into allowed_origin() results in
       // error
-      if cors_origin_setting.as_deref() == Some("*") {
-        Cors::default().allow_any_origin().send_wildcard()
+      if origin == "*" {
+        cors_default
       } else {
         Cors::default()
           .allowed_origin(&origin)
           .allowed_origin(&self_origin)
+          .allow_any_method()
+          .allow_any_header()
+          .expose_any_header()
+          .max_age(3600)
       }
     }
-    _ => Cors::default()
-      .allow_any_origin()
-      .allow_any_method()
-      .allow_any_header()
-      .expose_any_header()
-      .max_age(3600),
+    _ => cors_default,
   }
 }
 
