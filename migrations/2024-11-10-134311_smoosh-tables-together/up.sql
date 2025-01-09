86c2108f5cb7b3e8fff755f14aa8168187971952
Dull Bananas <dull.bananas0@gmail.com>
2025-01-08 23:23:26 -0700

    update smoosh migration

--- migrations/2024-11-10-134311_smoosh-tables-together/up.sql
+++ migrations/2024-11-10-134311_smoosh-tables-together/up.sql
@@ -87,26 +87,6 @@ ON CONFLICT (person_id,
     DO UPDATE SET
         saved = excluded.saved;
 
-INSERT INTO community_actions (person_id, community_id, blocked, became_moderator, received_ban, ban_expires)
-SELECT
-    person_id,
-    community_id,
-    community_block.published,
-    community_moderator.published,
-    community_person_ban.published,
-    community_person_ban.expires
-FROM
-    community_block
-    FULL JOIN community_moderator USING (person_id, community_id)
-    FULL JOIN community_person_ban USING (person_id, community_id)
-ON CONFLICT (person_id,
-    community_id)
-    DO UPDATE SET
-        blocked = excluded.blocked,
-        became_moderator = excluded.became_moderator,
-        received_ban = excluded.received_ban,
-        ban_expires = excluded.ban_expires;
-
 INSERT INTO person_actions (person_id, target_id, blocked)
 SELECT
     person_id,
@@ -119,30 +99,163 @@ ON CONFLICT (person_id,
     DO UPDATE SET
         blocked = excluded.blocked;
 
-INSERT INTO post_actions (person_id, post_id, read_comments, read_comments_amount, hidden, liked, like_score, saved)
+UPDATE
+    community_actions AS a
+SET
+    blocked = (
+        SELECT
+            published
+        FROM
+            community_block AS b
+        WHERE (b.person_id, b.community_id) = (a.person_id, a.community_id)),
+became_moderator = (
+    SELECT
+        published
+    FROM
+        community_moderator AS b
+    WHERE (b.person_id, b.community_id) = (a.person_id, a.community_id)),
+(received_ban,
+    ban_expires) = (
+    SELECT
+        published,
+        expires
+    FROM
+        community_person_ban AS b
+    WHERE (b.person_id, b.community_id) = (a.person_id, a.community_id));
+
+INSERT INTO community_actions (person_id, community_id, received_ban, ban_expires)
+SELECT
+    person_id,
+    community_id,
+    published,
+    expires
+FROM
+    community_person_ban AS b
+WHERE
+    NOT EXISTS (
+        SELECT
+        FROM
+            community_actions AS a
+        WHERE (a.person_id, a.community_id) = (b.person_id, b.community_id));
+
+INSERT INTO community_actions (person_id, community_id, blocked)
+SELECT
+    person_id,
+    community_id,
+    published
+FROM
+    community_block
+ON CONFLICT (person_id,
+    community_id)
+    DO UPDATE SET
+        blocked = excluded.blocked
+    WHERE
+        community_actions.blocked IS NULL;
+
+INSERT INTO community_actions (person_id, community_id, became_moderator)
+SELECT
+    person_id,
+    community_id,
+    published
+FROM
+    community_moderator
+ON CONFLICT (person_id,
+    community_id)
+    DO UPDATE SET
+        became_moderator = excluded.became_moderator
+    WHERE
+        community_actions.became_moderator IS NULL;
+
+UPDATE
+    post_actions AS a
+SET
+    (read_comments,
+        read_comments_amount) = (
+        SELECT
+            published,
+            read_comments
+        FROM
+            person_post_aggregates AS b
+        WHERE (b.person_id, b.post_id) = (a.person_id, a.post_id)),
+hidden = (
+    SELECT
+        published
+    FROM
+        post_hide AS b
+    WHERE (b.person_id, b.post_id) = (a.person_id, a.post_id)),
+(liked,
+    like_score) = (
+    SELECT
+        published,
+        score
+    FROM
+        post_like AS b
+    WHERE (b.person_id, b.post_id) = (a.person_id, a.post_id)),
+saved = (
+    SELECT
+        published
+    FROM
+        post_saved AS b
+    WHERE (b.person_id, b.post_id) = (a.person_id, a.post_id));
+
+INSERT INTO post_actions (person_id, post_id, liked, like_score)
 SELECT
     person_id,
     post_id,
-    person_post_aggregates.published,
-    person_post_aggregates.read_comments,
-    post_hide.published,
-    post_like.published,
-    post_like.score,
-    post_saved.published
+    published,
+    score
+FROM
+    post_like AS b
+WHERE
+    NOT EXISTS (
+        SELECT
+        FROM
+            post_actions AS a
+        WHERE (a.person_id, a.post_id) = (b.person_id, b.post_id));
+
+INSERT INTO post_actions (person_id, post_id, read_comments, read_comments_amount)
+SELECT
+    person_id,
+    post_id,
+    published,
+    read_comments
 FROM
     person_post_aggregates
-    FULL JOIN post_hide USING (person_id, post_id)
-    FULL JOIN post_like USING (person_id, post_id)
-    FULL JOIN post_saved USING (person_id, post_id)
 ON CONFLICT (person_id,
     post_id)
     DO UPDATE SET
         read_comments = excluded.read_comments,
-        read_comments_amount = excluded.read_comments_amount,
-        hidden = excluded.hidden,
-        liked = excluded.liked,
-        like_score = excluded.like_score,
-        saved = excluded.saved;
+        read_comments_amount = excluded.read_comments_amount
+    WHERE
+        post_actions.read_comments IS NULL;
+
+INSERT INTO post_actions (person_id, post_id, saved)
+SELECT
+    person_id,
+    post_id,
+    published
+FROM
+    post_saved
+ON CONFLICT (person_id,
+    post_id)
+    DO UPDATE SET
+        saved = excluded.saved
+    WHERE
+        post_actions.saved IS NULL;
+
+INSERT INTO post_actions (person_id, post_id, hidden)
+SELECT
+    person_id,
+    post_id,
+    published
+FROM
+    post_hide
+ON CONFLICT (person_id,
+    post_id)
+    DO UPDATE SET
+        hidden = excluded.hidden
+    WHERE
+        post_actions.hidden IS NULL;
 
 -- Drop old tables
 DROP TABLE comment_saved, community_block, community_moderator, community_person_ban, person_block, person_post_aggregates, post_hide, post_like, post_saved;
