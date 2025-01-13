95bd69b9c645ccbf4dab9e8ca5e9e735ebdda69e
Felix Ableitner <me@nutomic.com>
2025-01-13 15:15:29 +0100

    drop default

--- migrations/2025-01-09-144233_no-image-token/down.sql
+++ migrations/2025-01-09-144233_no-image-token/down.sql
@@ -2,6 +2,9 @@ ALTER TABLE local_image
     ADD COLUMN pictrs_delete_token text NOT NULL DEFAULT '';
 
 ALTER TABLE local_image
+    ALTER COLUMN pictrs_delete_token DROP DEFAULT;
+
+ALTER TABLE local_image
     ADD COLUMN published_new timestamp with time zone DEFAULT now() NOT NULL;
 
 UPDATE
