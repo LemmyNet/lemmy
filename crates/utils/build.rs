496ae58cb61d5898ff17d8d39335eb48562be49c
Nutomic <me@nutomic.com>
2025-02-07 16:44:41 +0000

    Enable emails in Chinese (#5400)

--- crates/utils/build.rs
+++ crates/utils/build.rs
@@ -6,7 +6,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   for path in read_dir("translations/email/")? {
     let path = path?.path();
     if let Some(name) = path.file_name() {
-      let lang = name.to_string_lossy().to_string().replace(".json", "");
+      let mut lang = name.to_string_lossy().to_string().replace(".json", "");
+
+      // Rename Chinese simplified variant because there is no translation zh
+      if lang == "zh_Hans" {
+        lang = "zh".to_string();
+      }
       // Rosetta doesnt support these language variants.
       if lang.contains('_') {
         continue;
