09accfae3e8caf888eb3fe5e83120e849dcac6ca
Dull Bananas <dull.bananas0@gmail.com>
2025-03-03 15:27:56 -0700

    fix use of check_report_reason

--- crates/api/src/reports/community_report/create.rs
+++ crates/api/src/reports/community_report/create.rs
@@ -2,18 +2,18 @@ use crate::check_report_reason;
 use actix_web::web::{Data, Json};
 use lemmy_api_common::{
   context::LemmyContext,
-  reports::community::{CreateCommunityReport, CommunityReportResponse},
-  utils::send_new_report_email_to_admins,
+  reports::community::{CommunityReportResponse, CreateCommunityReport},
+  utils::{send_new_report_email_to_admins, slur_regex},
 };
 use lemmy_db_schema::{
   source::{
-    local_site::LocalSite,
     community::Community,
     community_report::{CommunityReport, CommunityReportForm},
+    local_site::LocalSite,
   },
   traits::{Crud, Reportable},
 };
-use lemmy_db_views::structs::{LocalUserView, CommunityReportView};
+use lemmy_db_views::structs::{CommunityReportView, LocalUserView};
 use lemmy_utils::error::{LemmyErrorExt, LemmyErrorType, LemmyResult};
 
 pub async fn create_community_report(
@@ -21,10 +21,9 @@ pub async fn create_community_report(
   context: Data<LemmyContext>,
   local_user_view: LocalUserView,
 ) -> LemmyResult<Json<CommunityReportResponse>> {
-  let local_site = LocalSite::read(&mut context.pool()).await?;
-
   let reason = data.reason.trim().to_string();
-  check_report_reason(&reason, &local_site)?;
+  let slur_regex = slur_regex(&context).await?;
+  check_report_reason(&reason, &slur_regex)?;
 
   let person_id = local_user_view.person.id;
   let community_id = data.community_id;
@@ -36,9 +35,9 @@ pub async fn create_community_report(
     original_community_banner: community.banner,
     original_community_description: community.description,
     original_community_icon: community.icon,
-    original_community_name:community.name,
-    original_community_sidebar:community.sidebar,
-    original_community_title:community.title,
+    original_community_name: community.name,
+    original_community_sidebar: community.sidebar,
+    original_community_title: community.title,
     reason,
   };
 
@@ -46,10 +45,10 @@ pub async fn create_community_report(
     .await
     .with_lemmy_type(LemmyErrorType::CouldntCreateReport)?;
 
-  let community_report_view =
-    CommunityReportView::read(&mut context.pool(), report.id).await?;
+  let community_report_view = CommunityReportView::read(&mut context.pool(), report.id).await?;
 
   // Email the admins
+  let local_site = LocalSite::read(&mut context.pool()).await?;
   if local_site.reports_email_admins {
     send_new_report_email_to_admins(
       &community_report_view.creator.name,
