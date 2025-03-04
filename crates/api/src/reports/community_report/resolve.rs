999c9c7c5a7407eb86bb8eecc029c9f9e2d2cb6b
Dull Bananas <dull.bananas0@gmail.com>
2025-03-03 17:10:42 -0700

    fix all compile errors

--- crates/api/src/reports/community_report/resolve.rs
+++ crates/api/src/reports/community_report/resolve.rs
@@ -1,36 +1,36 @@
 use actix_web::web::{Data, Json};
 use lemmy_api_common::{
   context::LemmyContext,
-  reports::private_message::{PrivateMessageReportResponse, ResolvePrivateMessageReport},
+  reports::community::{CommunityReportResponse, ResolveCommunityReport},
   utils::is_admin,
 };
-use lemmy_db_schema::{source::private_message_report::PrivateMessageReport, traits::Reportable};
-use lemmy_db_views::structs::{LocalUserView, PrivateMessageReportView};
+use lemmy_db_schema::{source::community_report::CommunityReport, traits::Reportable};
+use lemmy_db_views::structs::{CommunityReportView, LocalUserView};
 use lemmy_utils::error::{LemmyErrorExt, LemmyErrorType, LemmyResult};
 
-pub async fn resolve_pm_report(
-  data: Json<ResolvePrivateMessageReport>,
+pub async fn resolve_community_report(
+  data: Json<ResolveCommunityReport>,
   context: Data<LemmyContext>,
   local_user_view: LocalUserView,
-) -> LemmyResult<Json<PrivateMessageReportResponse>> {
+) -> LemmyResult<Json<CommunityReportResponse>> {
   is_admin(&local_user_view)?;
 
   let report_id = data.report_id;
   let person_id = local_user_view.person.id;
   if data.resolved {
-    PrivateMessageReport::resolve(&mut context.pool(), report_id, person_id)
+    CommunityReport::resolve(&mut context.pool(), report_id, person_id)
       .await
       .with_lemmy_type(LemmyErrorType::CouldntResolveReport)?;
   } else {
-    PrivateMessageReport::unresolve(&mut context.pool(), report_id, person_id)
+    CommunityReport::unresolve(&mut context.pool(), report_id, person_id)
       .await
       .with_lemmy_type(LemmyErrorType::CouldntResolveReport)?;
   }
 
-  let private_message_report_view =
-    PrivateMessageReportView::read(&mut context.pool(), report_id).await?;
+  let community_report_view =
+    CommunityReportView::read(&mut context.pool(), report_id, person_id).await?;
 
-  Ok(Json(PrivateMessageReportResponse {
-    private_message_report_view,
+  Ok(Json(CommunityReportResponse {
+    community_report_view,
   }))
 }
