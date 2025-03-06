7435069d2dd8e77a252676162e87f2d5b6482ae0
Felix Ableitner <me@nutomic.com>
2025-03-06 11:00:50 +0100

    migration for modlog

--- migrations/2025-03-04-124442_community-hidden-visibility/down.sql
+++ migrations/2025-03-04-124442_community-hidden-visibility/down.sql
@@ -90,5 +90,85 @@ CREATE INDEX idx_community_random_number ON community (random_number) INCLUDE (l
 WHERE
     NOT (deleted OR removed OR visibility = 'Private');
 
+-- revert modlog table changes
+CREATE TABLE mod_hide_community (
+    id serial PRIMARY KEY,
+    community_id int REFERENCES community ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    mod_person_id int REFERENCES person ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    published timestamptz NOT NULL DEFAULT now(),
+    reason text,
+    hidden boolean DEFAULT FALSE NOT NULL
+);
+
+ALTER TABLE modlog_combined
+    DROP COLUMN mod_change_community_visibility_id,
+    ADD COLUMN mod_hide_community_id int REFERENCES mod_hide_community ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    ADD COLUMN mod_lock_post_id_new int,
+    ADD COLUMN mod_remove_comment_id_new int,
+    ADD COLUMN mod_remove_community_id_new int,
+    ADD COLUMN mod_remove_post_id_new int,
+    ADD COLUMN mod_transfer_community_id_new int;
+
+UPDATE
+    modlog_combined
+SET
+    (mod_lock_post_id_new,
+        mod_remove_comment_id_new,
+        mod_remove_community_id_new,
+        mod_remove_post_id_new,
+        mod_transfer_community_id_new) = (mod_lock_post_id,
+        mod_remove_comment_id,
+        mod_remove_community_id,
+        mod_remove_post_id,
+        mod_transfer_community_id);
+
+ALTER TABLE modlog_combined
+    DROP COLUMN mod_lock_post_id,
+    DROP COLUMN mod_remove_comment_id,
+    DROP COLUMN mod_remove_community_id,
+    DROP COLUMN mod_remove_post_id,
+    DROP COLUMN mod_transfer_community_id;
+
+ALTER TABLE modlog_combined RENAME COLUMN mod_lock_post_id_new TO mod_lock_post_id;
+
+ALTER TABLE modlog_combined RENAME COLUMN mod_remove_comment_id_new TO mod_remove_comment_id;
+
+ALTER TABLE modlog_combined RENAME COLUMN mod_remove_community_id_new TO mod_remove_community_id;
+
+ALTER TABLE modlog_combined RENAME COLUMN mod_remove_post_id_new TO mod_remove_post_id;
+
+ALTER TABLE modlog_combined RENAME COLUMN mod_transfer_community_id_new TO mod_transfer_community_id;
+
+ALTER TABLE modlog_combined
+    ADD CONSTRAINT modlog_combined_mod_hide_community_id_key UNIQUE (mod_hide_community_id),
+    ADD CONSTRAINT modlog_combined_mod_lock_post_id_key UNIQUE (mod_lock_post_id),
+    ADD CONSTRAINT modlog_combined_mod_remove_comment_id_key UNIQUE (mod_remove_comment_id),
+    ADD CONSTRAINT modlog_combined_mod_remove_community_id_key UNIQUE (mod_remove_community_id),
+    ADD CONSTRAINT modlog_combined_mod_remove_post_id_key UNIQUE (mod_remove_post_id),
+    ADD CONSTRAINT modlog_combined_mod_transfer_community_id_key UNIQUE (mod_transfer_community_id),
+    ADD CONSTRAINT modlog_combined_mod_lock_post_id_fkey FOREIGN KEY (mod_lock_post_id) REFERENCES mod_lock_post (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    ADD CONSTRAINT modlog_combined_mod_remove_comment_id_fkey FOREIGN KEY (mod_remove_comment_id) REFERENCES mod_remove_comment (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    ADD CONSTRAINT modlog_combined_mod_remove_community_id_fkey FOREIGN KEY (mod_remove_community_id) REFERENCES mod_remove_community (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    ADD CONSTRAINT modlog_combined_mod_remove_post_id_fkey FOREIGN KEY (mod_remove_post_id) REFERENCES mod_remove_post (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    ADD CONSTRAINT modlog_combined_mod_transfer_community_id_fkey FOREIGN KEY (mod_transfer_community_id) REFERENCES mod_transfer_community (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    ADD CONSTRAINT modlog_combined_check CHECK ((num_nonnulls (admin_allow_instance_id, admin_block_instance_id, admin_purge_comment_id, admin_purge_community_id, admin_purge_person_id, admin_purge_post_id, mod_add_id, mod_add_community_id, mod_ban_id, mod_ban_from_community_id, mod_feature_post_id, mod_hide_community_id, mod_lock_post_id, mod_remove_comment_id, mod_remove_community_id, mod_remove_post_id, mod_transfer_community_id) = 1));
+
+ALTER TABLE modlog_combined
+    ALTER COLUMN mod_lock_post_id SET NOT NULL;
+
+ALTER TABLE modlog_combined
+    ALTER COLUMN mod_remove_comment_id SET NOT NULL;
+
+ALTER TABLE modlog_combined
+    ALTER COLUMN mod_remove_community_id SET NOT NULL;
+
+ALTER TABLE modlog_combined
+    ALTER COLUMN mod_remove_post_id SET NOT NULL;
+
+ALTER TABLE modlog_combined
+    ALTER COLUMN mod_transfer_community_id SET NOT NULL;
+
+DROP TABLE mod_change_community_visibility;
+
 DROP TYPE community_visibility__;
 
--- migrations/2025-03-04-124442_community-hidden-visibility/up.sql
+++ migrations/2025-03-04-124442_community-hidden-visibility/up.sql
@@ -30,7 +30,7 @@ CREATE INDEX idx_community_random_number ON community (random_number) INCLUDE (l
 WHERE
     NOT (deleted OR removed OR visibility = 'Private' OR visibility = 'Hidden');
 
-DROP TYPE community_visibility__;
+DROP TYPE community_visibility__ CASCADE;
 
 ALTER TYPE community_visibility RENAME VALUE 'LocalOnly' TO 'LocalOnlyPrivate';
 
@@ -45,4 +45,23 @@ WHERE
 -- drop the old hidden column
 ALTER TABLE community
     DROP COLUMN hidden;
+
+-- change modlog tables
+ALTER TABLE modlog_combined
+    DROP COLUMN mod_hide_community_id;
+
+DROP TABLE mod_hide_community;
+
+CREATE TABLE mod_change_community_visibility (
+    id serial PRIMARY KEY,
+    community_id int REFERENCES community ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    mod_person_id int REFERENCES person ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    published timestamptz NOT NULL DEFAULT now(),
+    reason text,
+    new_visibility community_visibility NOT NULL
+);
+
+ALTER TABLE modlog_combined
+    ADD COLUMN mod_change_community_visibility_id int NOT NULL REFERENCES mod_change_community_visibility (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    ADD CONSTRAINT modlog_combined_check CHECK ((num_nonnulls (admin_allow_instance_id, admin_block_instance_id, admin_purge_comment_id, admin_purge_community_id, admin_purge_person_id, admin_purge_post_id, mod_add_id, mod_add_community_id, mod_ban_id, mod_ban_from_community_id, mod_feature_post_id, mod_change_community_visibility_id, mod_lock_post_id, mod_remove_comment_id, mod_remove_community_id, mod_remove_post_id, mod_transfer_community_id) = 1));
 
