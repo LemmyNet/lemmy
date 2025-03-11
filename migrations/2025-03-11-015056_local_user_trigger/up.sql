e641d1300531183c4be14593e9b9c1f33b99d920
Dull Bananas <dull.bananas0@gmail.com>
2025-03-10 18:56:31 -0700

    Update user count from local_user instead of person, and only count users with accepted application

---
+++ migrations/2025-03-11-015056_local_user_trigger/up.sql
@@ -0,0 +1,11 @@
+UPDATE
+    local_site
+SET
+    users = users - (
+        SELECT
+            count(*)
+        FROM
+            local_user
+        WHERE
+            NOT accepted_application);
+
