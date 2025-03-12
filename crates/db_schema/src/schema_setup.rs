7d5652945fa48ecba12553bd64634d752df10a02
Dull Bananas <dull.bananas0@gmail.com>
2025-03-11 20:48:56 -0700

    fmt

--- crates/db_schema/src/schema_setup.rs
+++ crates/db_schema/src/schema_setup.rs
@@ -278,10 +278,7 @@ fn run_selected_migrations(
   conn: &mut PgConnection,
   options: &Options,
 ) -> diesel::migration::Result<()> {
-  let mut wrapper = MigrationHarnessWrapper {
-    conn,
-    options,
-  };
+  let mut wrapper = MigrationHarnessWrapper { conn, options };
 
   if options.revert {
     if let Some(limit) = options.limit {
