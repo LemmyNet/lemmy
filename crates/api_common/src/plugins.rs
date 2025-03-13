5ca95d8a574fde1e4a3773354727bfbc06f53a32
Felix Ableitner <me@nutomic.com>
2025-03-13 10:06:57 +0100

    use plugin pool

--- crates/api_common/src/plugins.rs
+++ crates/api_common/src/plugins.rs
@@ -1,25 +1,34 @@
 use crate::LemmyErrorType;
-use extism::{FromBytes, Manifest, Plugin};
+use anyhow::anyhow;
+use extism::{FromBytes, Manifest, PluginBuilder, Pool};
 use extism_convert::Json;
 use lemmy_utils::error::LemmyResult;
+use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use std::{
   env,
   ffi::OsStr,
   fs::{read_dir, File},
   io::BufReader,
+  ops::Deref,
   path::PathBuf,
+  thread::available_parallelism,
+  time::Duration,
 };
 use tracing::{debug, warn};
 use ts_rs::TS;
 use url::Url;
+
+const GET_PLUGIN_TIMEOUT: Duration = Duration::from_secs(0);
 
 /// Call a plugin hook without rewriting data
 pub fn plugin_hook<T>(name: &'static str, data: &T) -> LemmyResult<()>
 where
   T: Clone + Serialize + for<'a> Deserialize<'a>,
 {
-  Plugins::load().call(name, data)?;
+  // TODO: as this doesnt return any data it could run multiple plugins in parallel
+  //       and/or in background thread
+  Plugins::get().call(name, data)?;
   Ok(())
 }
 
@@ -28,7 +37,7 @@ pub fn plugin_hook_mut<T>(name: &'static str, data: &mut T) -> LemmyResult<()>
 where
   T: Clone + Serialize + for<'a> Deserialize<'a>,
 {
-  let res = Plugins::load().call(name, data)?;
+  let res = Plugins::get().call(name, data)?;
   if let Some(res) = res {
     *data = res;
   }
@@ -36,14 +45,7 @@ where
 }
 
 pub fn plugin_metadata() -> Vec<PluginMetadata> {
-  Plugins::load().0.into_iter().map(|p| p.metadata).collect()
-}
-
-struct Plugins(Vec<LemmyPlugin>);
-
-struct LemmyPlugin {
-  plugin: Plugin,
-  metadata: PluginMetadata,
+  Plugins::get().0.into_iter().map(|p| p.metadata).collect()
 }
 
 #[derive(Serialize, Deserialize, FromBytes, Debug, Clone)]
@@ -56,44 +58,59 @@ pub struct PluginMetadata {
   description: String,
 }
 
-fn init_plugin(path: &PathBuf) -> LemmyResult<LemmyPlugin> {
-  let file = File::open(path)?;
-  let reader = BufReader::new(file);
-  let manifest: Manifest = serde_json::from_reader(reader)?;
-  let mut plugin = Plugin::new(manifest, [], true)?;
-  let metadata = plugin.call("metadata", 0)?;
-  Ok(LemmyPlugin { plugin, metadata })
+#[derive(Clone)]
+struct Plugins(Vec<LemmyPlugin>);
+
+#[derive(Clone)]
+struct LemmyPlugin {
+  plugin_pool: Pool<()>,
+  metadata: PluginMetadata,
+}
+
+impl LemmyPlugin {
+  fn init(path: &PathBuf) -> LemmyResult<Self> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let manifest: Manifest = serde_json::from_reader(reader)?;
+    let plugin_pool: Pool<()> = Pool::new(available_parallelism()?.into());
+    let builder = PluginBuilder::new(manifest).with_wasi(true);
+    let metadata: PluginMetadata = builder.clone().build()?.call("metadata", 0)?;
+    plugin_pool.add_builder((), builder);
+    Ok(LemmyPlugin {
+      plugin_pool,
+      metadata,
+    })
+  }
 }
 
 impl Plugins {
   /// Load and initialize all plugins
-  fn load() -> Self {
+  fn get() -> Self {
     // TODO: use std::sync::OnceLock once get_mut_or_init() is stabilized
     // https://doc.rust-lang.org/std/sync/struct.OnceLock.html#method.get_mut_or_init
-    //static PLUGINS: Lazy<Plugins> = Lazy::new(|| {
-    let dir = env::var("LEMMY_PLUGIN_PATH").unwrap_or("plugins".to_string());
-    let plugin_paths = match read_dir(dir) {
-      Ok(r) => r,
-      Err(e) => {
-        warn!("Failed to read plugin folder: {e}");
-        return Plugins(vec![]);
-      }
-    };
+    static PLUGINS: Lazy<Plugins> = Lazy::new(|| {
+      let dir = env::var("LEMMY_PLUGIN_PATH").unwrap_or("plugins".to_string());
+      let plugin_paths = match read_dir(dir) {
+        Ok(r) => r,
+        Err(e) => {
+          warn!("Failed to read plugin folder: {e}");
+          return Plugins(vec![]);
+        }
+      };
 
-    let plugins = plugin_paths
-      .flat_map(Result::ok)
-      .map(|p| p.path())
-      .filter(|p| p.extension() == Some(OsStr::new("json")))
-      .flat_map(|p| {
-        init_plugin(&p)
-          .inspect_err(|e| warn!("Failed to load plugin {}: {e}", p.to_string_lossy()))
-          .ok()
-      })
-      .collect();
-    Plugins(plugins)
-    //});
-    // TODO: avoid cloning
-    //PLUGINS.deref().clone()
+      let plugins = plugin_paths
+        .flat_map(Result::ok)
+        .map(|p| p.path())
+        .filter(|p| p.extension() == Some(OsStr::new("json")))
+        .flat_map(|p| {
+          LemmyPlugin::init(&p)
+            .inspect_err(|e| warn!("Failed to load plugin {}: {e}", p.to_string_lossy()))
+            .ok()
+        })
+        .collect();
+      Plugins(plugins)
+    });
+    PLUGINS.deref().clone()
   }
 
   /// Call all plugins for a given hook name, taking care not to clone data unnnecessarily.
@@ -102,15 +119,22 @@ impl Plugins {
     T: Serialize + for<'de> Deserialize<'de> + Clone,
   {
     debug!("Calling plugin hook {name}");
-    if !self.0.iter().any(|p| p.plugin.function_exists(name)) {
+    // Check if there is any plugin active for this hook, to avoid unnecessary data cloning
+    // TODO: not currently supported by pool
+    /*
+    if !self.0.iter().any(|p| p.plugin_pool.function_exists(name)) {
       return Ok(None);
     }
+    */
 
     let mut res: Json<T> = data.clone().into();
     for p in &mut self.0 {
-      if p.plugin.function_exists(name) {
-        let r = p
-          .plugin
+      let plugin = p
+        .plugin_pool
+        .get(&(), GET_PLUGIN_TIMEOUT)?
+        .ok_or(anyhow!("plugin timeout"))?;
+      if plugin.plugin().function_exists(name) {
+        let r = plugin
           .call(name, res)
           .map_err(|e| LemmyErrorType::PluginError(e.to_string()))?;
         res = r;
