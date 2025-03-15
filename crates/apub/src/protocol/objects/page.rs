0db0761831527013da1e64e869122f8d278071ed
flamingos-cant <flamingos-cant@flamingos-cant.xyz>
2025-03-15 06:24:19 +0000

    Use backslash to prevent breaks instead

--- crates/apub/src/protocol/objects/page.rs
+++ crates/apub/src/protocol/objects/page.rs
@@ -133,7 +133,8 @@ impl Attachment {
 
     let is_image =
       media_type.is_some_and(|media| media.starts_with("video") || media.starts_with("image"));
-    let name = name.map(|n| n.replace("\n", " "));
+    // To prevent newlines from creating broken image embeds, we need to add a backslash before the newline
+    let name = name.map(|n| n.replace("\r\n", "\n").replace("\n", "\\\n"));
 
     if is_image {
       let url = proxy_image_link(url, context).await?;
