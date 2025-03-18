858621ad7f38ab34a2c07855cb6138af0f56e8a0
Felix Ableitner <me@nutomic.com>
2025-03-18 11:10:00 +0100

    remove unused

--- crates/db_schema/src/impls/person.rs
+++ crates/db_schema/src/impls/person.rs
@@ -1,7 +1,7 @@
 use crate::{
   diesel::OptionalExtension,
   newtypes::{CommunityId, DbUrl, InstanceId, PersonId},
-  schema::{comment, community, instance, local_user, person, person_actions, post},
+  schema::{instance, local_user, person, person_actions},
   source::person::{
     Person,
     PersonActions,
@@ -18,7 +18,6 @@ use diesel::{
   dsl::{exists, insert_into, not, select},
   expression::SelectableHelper,
   result::Error,
-  CombineDsl,
   ExpressionMethods,
   JoinOnDsl,
   QueryDsl,
@@ -100,31 +99,6 @@ impl Person {
         person::updated.eq(Utc::now()),
       ))
       .get_result::<Self>(conn)
-      .await
-  }
-
-  /// Lists local community ids for all posts and comments for a given creator.
-  pub async fn list_local_community_ids(
-    pool: &mut DbPool<'_>,
-    for_creator_id: PersonId,
-  ) -> Result<Vec<CommunityId>, Error> {
-    let conn = &mut get_conn(pool).await?;
-    comment::table
-      .inner_join(post::table)
-      .inner_join(community::table.on(post::community_id.eq(community::id)))
-      .filter(community::local.eq(true))
-      .filter(not(community::deleted))
-      .filter(not(community::removed))
-      .filter(comment::creator_id.eq(for_creator_id))
-      .select(community::id)
-      .union(
-        post::table
-          .inner_join(community::table)
-          .filter(community::local.eq(true))
-          .filter(post::creator_id.eq(for_creator_id))
-          .select(community::id),
-      )
-      .load::<CommunityId>(conn)
       .await
   }
 
