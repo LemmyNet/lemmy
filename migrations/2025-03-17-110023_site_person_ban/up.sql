2ad4cafd3b16cecbbf56007ec55ae6537ce3ba52
Felix Ableitner <me@nutomic.com>
2025-03-18 14:28:35 +0100

    fmt, keep banned status

--- migrations/2025-03-17-110023_site_person_ban/up.sql
+++ migrations/2025-03-17-110023_site_person_ban/up.sql
@@ -7,10 +7,21 @@ ALTER TABLE instance_actions
 ALTER TABLE mod_ban
     ADD COLUMN instance_id int NOT NULL REFERENCES instance ON UPDATE CASCADE ON DELETE CASCADE;
 
+-- insert existing bans into instance_actions table, assuming they were all banned from home instance
+INSERT INTO instance_actions (person_id, instance_id, received_ban, ban_expires)
+SELECT
+    id,
+    instance_id,
+    now(),
+    ban_expires
+FROM
+    person
+WHERE
+    banned;
+
 ALTER TABLE person
     DROP COLUMN banned;
 
 ALTER TABLE person
     DROP COLUMN ban_expires;
 
--- TODO: insert bans into instance_actions table
