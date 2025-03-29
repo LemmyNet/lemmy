557ef091972fd95fe19f77e16994e4bb34746489
lseeger <leoseeger@proton.me>
2025-03-29 17:37:45 +0100

    #3710: Fixxed merging error

--- crates/api_crud/src/user/my_user.rs
+++ crates/api_crud/src/user/my_user.rs
@@ -24,14 +24,14 @@ pub async fn get_my_user(
   let local_user_id = local_user_view.local_user.id;
   let pool = &mut context.pool();
 
-  let (follows, community_blocks, instance_blocks, keyword_blocks, person_blocks, moderates, discussion_languages) =
+  let (follows, community_blocks, instance_blocks, person_blocks, moderates, keyword_blocks, discussion_languages) =
     lemmy_db_schema::try_join_with_pool!(pool => (
       |pool| CommunityFollowerView::for_person(pool, person_id),
       |pool| CommunityActions::read_blocks_for_person(pool, person_id),
       |pool| InstanceActions::read_blocks_for_person(pool, person_id),
       |pool| PersonActions::read_blocks_for_person(pool, person_id),
-        |pool| LocalUserKeywordBlock::read(pool, local_user_id),
       |pool| CommunityModeratorView::for_person(pool, person_id, Some(&local_user_view.local_user)),
+      |pool| LocalUserKeywordBlock::read(pool, local_user_id),
       |pool| LocalUserLanguage::read(pool, local_user_id)
     ))
     .with_lemmy_type(LemmyErrorType::SystemErrLogin)?;
