bd906f83819f204fa6e064e19f5eb2ebc658c4b0
Felix Ableitner <me@nutomic.com>
2025-04-07 12:38:19 +0200

    Implement multi-community (fixes #818, fixes #5340)

---
+++ migrations/2025-04-07-103308_multi-community/down.sql
@@ -0,0 +1,6 @@
+ALTER TABLE local_site
+    DROP COLUMN featured_communities;
+
+DROP TABLE multi_community_entry;
+
+DROP TABLE multi_community
---
+++ migrations/2025-04-07-103308_multi-community/up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE multi_community (
+    id serial PRIMARY KEY,
+    owner_id int NOT NULL REFERENCES person ON UPDATE CASCADE ON DELETE CASCADE,
+    name text NOT NULL
+);
+
+CREATE TABLE multi_community_entry (
+    multi_community_id int NOT NULL REFERENCES multi_community ON UPDATE CASCADE ON DELETE CASCADE,
+    community_id int NOT NULL REFERENCES person ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (multi_community_id, community_id)
+);
+
+ALTER TABLE local_site
+    ADD COLUMN featured_communities int REFERENCES multi_community ON UPDATE CASCADE ON DELETE CASCADE;
+
