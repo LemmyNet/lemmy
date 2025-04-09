229f38b8094300bb87d1e71d7364bfd0c1988d9e
Dessalines <tyhou13@gmx.com>
2025-04-09 14:36:47 -0400

    sql fmt

--- migrations/2025-04-09-123352_cursor_pagination_indexes/up.sql
+++ migrations/2025-04-09-123352_cursor_pagination_indexes/up.sql
@@ -1,6 +1,12 @@
 -- Taglines
-CREATE INDEX idx_tagline_published_id on tagline (published desc, id desc);
+CREATE INDEX idx_tagline_published_id ON tagline (published DESC, id DESC);
 
 -- Some for the vote views
-CREATE INDEX idx_comment_actions_like_score on comment_actions (comment_id, like_score, person_id) where like_score is not null;
-CREATE INDEX idx_post_actions_like_score on post_actions (post_id, like_score, person_id) where like_score is not null;
+CREATE INDEX idx_comment_actions_like_score ON comment_actions (comment_id, like_score, person_id)
+WHERE
+    like_score IS NOT NULL;
+
+CREATE INDEX idx_post_actions_like_score ON post_actions (post_id, like_score, person_id)
+WHERE
+    like_score IS NOT NULL;
+
