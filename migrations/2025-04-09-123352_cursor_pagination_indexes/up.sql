d08a75e2dda1379b8235c2f51da73c3cb734bbfa
Dessalines <tyhou13@gmx.com>
2025-04-14 15:54:52 -0400

    Using id desc.

--- migrations/2025-04-09-123352_cursor_pagination_indexes/up.sql
+++ migrations/2025-04-09-123352_cursor_pagination_indexes/up.sql
@@ -23,28 +23,28 @@ DROP INDEX idx_community_title;
 
 DROP INDEX idx_community_users_active_month;
 
-CREATE INDEX idx_community_lower_name ON community USING btree (lower((name)::text), id);
+CREATE INDEX idx_community_lower_name ON community USING btree (lower((name)::text), id DESC);
 
-CREATE INDEX idx_community_hot ON community USING btree (hot_rank DESC, id);
+CREATE INDEX idx_community_hot ON community USING btree (hot_rank DESC, id DESC);
 
-CREATE INDEX idx_community_published ON community USING btree (published DESC, id);
+CREATE INDEX idx_community_published ON community USING btree (published DESC, id DESC);
 
-CREATE INDEX idx_community_subscribers ON community USING btree (subscribers DESC, id);
+CREATE INDEX idx_community_subscribers ON community USING btree (subscribers DESC, id DESC);
 
-CREATE INDEX idx_community_title ON community USING btree (title, id);
+CREATE INDEX idx_community_title ON community USING btree (title, id DESC);
 
-CREATE INDEX idx_community_users_active_month ON community USING btree (users_active_month DESC, id);
+CREATE INDEX idx_community_users_active_month ON community USING btree (users_active_month DESC, id DESC);
 
 -- Create a few missing ones
-CREATE INDEX idx_community_users_active_half_year ON community USING btree (users_active_half_year DESC, id);
+CREATE INDEX idx_community_users_active_half_year ON community USING btree (users_active_half_year DESC, id DESC);
 
-CREATE INDEX idx_community_users_active_week ON community USING btree (users_active_week DESC, id);
+CREATE INDEX idx_community_users_active_week ON community USING btree (users_active_week DESC, id DESC);
 
-CREATE INDEX idx_community_users_active_day ON community USING btree (users_active_day DESC, id);
+CREATE INDEX idx_community_users_active_day ON community USING btree (users_active_day DESC, id DESC);
 
-CREATE INDEX idx_community_subscribers_local ON community USING btree (subscribers_local DESC, id);
+CREATE INDEX idx_community_subscribers_local ON community USING btree (subscribers_local DESC, id DESC);
 
-CREATE INDEX idx_community_comments ON community USING btree (comments DESC, id);
+CREATE INDEX idx_community_comments ON community USING btree (comments DESC, id DESC);
 
-CREATE INDEX idx_community_posts ON community USING btree (posts DESC, id);
+CREATE INDEX idx_community_posts ON community USING btree (posts DESC, id DESC);
 
