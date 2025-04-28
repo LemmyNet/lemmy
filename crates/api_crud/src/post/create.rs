e62bc476306c1bae2b2d9cf0271290ee80fd6078
Richard Schwab <gitrichardschwab-7a2qxq42kj@central-intelligence.agency>
2025-04-28 11:50:25 +0200

    [0.19] Mark posts in NSFW communities as NSFW (#5646)
    * Automatically marking posts created in an NSFW community as NSFW. (#5310)
    
    * Automatically marking posts created in an NSFW community as NSFW.
    
    - Context: https://github.com/LemmyNet/lemmy-ui/issues/2885
    
    * Update crates/api_crud/src/post/create.rs
    
    Co-authored-by: Nutomic <me@nutomic.com>
    
    ---------
    
    Co-authored-by: Nutomic <me@nutomic.com>
    
    ---------
    
    fixes https://github.com/LemmyNet/lemmy-ui/issues/3114
    
    * Ensure new posts in NSFW communities are marked NSFW
    
    Clients exposing the NSFW setting to users will typically do so by providing a
    checkbox, which is either checked or unchecked, but there is no third option
    to let the server decide. As a result, virtually all posts created through
    user interfaces will have a bool value for NSFW, so this logic likely is used
    only for posts created by scripts/bots.
    
    In #5310 the behavior for new posts was changed to default the NSFW flag to
    the community's NSFW setting, but it only did so as a default if no value was
    provided when creating a post via API.
    
    This changes the logic to force all new posts created via API or received from
    federation to be NSFW if the community they're posted in is marked NSFW,
    otherwise use the value provided by the creator, with a default of false if
    nothing is provided.
    
    It also ensures that this logic is used for filtering out NSFW posts on
    instances blocking NSFW content.
    
    backports #5649
    
    ---------
    
    Co-authored-by: Dessalines <dessalines@users.noreply.github.com>

--- crates/api_crud/src/post/create.rs
+++ crates/api_crud/src/post/create.rs
@@ -95,6 +95,14 @@ pub async fn create_post(
   let community = Community::read(&mut context.pool(), community_id)
     .await?
     .ok_or(LemmyErrorType::CouldntFindCommunity)?;
+
+  // Ensure that all posts in NSFW communities are marked as NSFW
+  let nsfw = if community.nsfw {
+    Some(true)
+  } else {
+    data.nsfw
+  };
+
   if community.posting_restricted_to_mods {
     let community_id = data.community_id;
     let is_mod = CommunityModeratorView::is_community_moderator(
@@ -137,7 +145,7 @@ pub async fn create_post(
     .alt_text(data.alt_text.clone())
     .community_id(data.community_id)
     .creator_id(local_user_view.person.id)
-    .nsfw(data.nsfw)
+    .nsfw(nsfw)
     .language_id(language_id)
     .build();
 
