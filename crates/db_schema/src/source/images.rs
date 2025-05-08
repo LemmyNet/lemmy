ad118d3bb9ac7c37feb0961442d640b26b98cbe6
Dessalines <tyhou13@gmx.com>
2025-05-08 19:54:00 -0400

    Add comment.

--- crates/db_schema/src/source/images.rs
+++ crates/db_schema/src/source/images.rs
@@ -34,6 +34,7 @@ pub struct LocalImage {
   pub published: DateTime<Utc>,
   pub person_id: PersonId,
   #[cfg_attr(feature = "full", ts(optional))]
+  /// This means the image is an auto-generated thumbnail, for a post.
   pub thumbnail_and_post_id: Option<PostId>,
 }
 
