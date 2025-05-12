54520b5c26ce17927a5f2a8045a748d44d54693c
Matthias Camenzind <matc.pub@protonmail.com>
2025-05-12 16:09:27 +0200

    TypeScript type CommunityIdQuery for community icon/banner uploads

--- crates/api_common/src/image.rs
+++ crates/api_common/src/image.rs
@@ -47,6 +47,8 @@ pub struct UploadImageResponse {
 /// Parameter for setting community icon or banner. Can't use POST data here as it already contains
 /// the image data.
 #[derive(Debug, Serialize, Deserialize, Clone, Default, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "full", derive(TS))]
+#[cfg_attr(feature = "full", ts(export))]
 pub struct CommunityIdQuery {
   pub id: CommunityId,
 }
