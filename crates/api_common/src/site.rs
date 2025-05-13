3d6dad4afcc9d0d36142b28834071159a0281c3b
Nutomic <me@nutomic.com>
2025-05-13 14:27:42 +0000

    Add list of local user accounts for admins (fixes #4419) (#5668)
    * Add list of local user accounts for admins (fixes #4419)
    
    * add api route
    
    * Merge list_banned_users into admin_list_users
    
    * revert sql

--- crates/api_common/src/site.rs
+++ crates/api_common/src/site.rs
@@ -655,3 +655,30 @@ pub struct PluginMetadata {
   url: Url,
   description: String,
 }
+
+#[skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize, Clone, Default, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "full", derive(TS))]
+#[cfg_attr(feature = "full", ts(export))]
+pub struct AdminListUsers {
+  #[cfg_attr(feature = "full", ts(optional))]
+  pub banned_only: Option<bool>,
+  #[cfg_attr(feature = "full", ts(optional))]
+  pub page_cursor: Option<PaginationCursor>,
+  #[cfg_attr(feature = "full", ts(optional))]
+  pub page_back: Option<bool>,
+  #[cfg_attr(feature = "full", ts(optional))]
+  pub limit: Option<i64>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[cfg_attr(feature = "full", derive(TS))]
+#[cfg_attr(feature = "full", ts(export))]
+pub struct AdminListUsersResponse {
+  pub users: Vec<LocalUserView>,
+  /// the pagination cursor to use to fetch the next page
+  #[cfg_attr(feature = "full", ts(optional))]
+  pub next_page: Option<PaginationCursor>,
+  #[cfg_attr(feature = "full", ts(optional))]
+  pub prev_page: Option<PaginationCursor>,
+}
