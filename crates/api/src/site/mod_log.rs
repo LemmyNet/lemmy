38b2d1437b09bea72d567fb386a4aaee764e216e
Dessalines <tyhou13@gmx.com>
2025-05-13 12:23:01 -0400

    Simplify modlog name removals.
    - #5614

--- crates/api/src/site/mod_log.rs
+++ crates/api/src/site/mod_log.rs
@@ -2,7 +2,7 @@ use actix_web::web::{Data, Json, Query};
 use lemmy_api_common::{
   context::LemmyContext,
   site::GetModlog,
-  utils::{check_community_mod_of_any_or_admin_action, check_private_instance},
+  utils::{check_private_instance, is_admin_opt, is_mod_or_admin_opt},
 };
 use lemmy_db_schema::traits::PaginationCursorBuilder;
 use lemmy_db_views_local_user::LocalUserView;
@@ -23,15 +23,25 @@ pub async fn get_mod_log(
 
   check_private_instance(&local_user_view, &local_site)?;
 
-  let is_mod_or_admin = if let Some(local_user_view) = &local_user_view {
-    check_community_mod_of_any_or_admin_action(local_user_view, &mut context.pool())
+  // Only show the modlog names if:
+  // !local_site.hide_modlog_mod_names or
+  // You're an admin or
+  // You're fetching the modlog for a single community, and you're a mod
+  // (Alternatively hide && !admin/mod)
+  let hide_modlog_names = local_site.hide_modlog_mod_names
+    && if let Some(community_id) = data.community_id {
+      is_mod_or_admin_opt(
+        &mut context.pool(),
+        local_user_view.as_ref(),
+        Some(community_id),
+      )
       .await
-      .is_ok()
-  } else {
-    false
-  };
-  let hide_modlog_names = local_site.hide_modlog_mod_names && !is_mod_or_admin;
+      .is_err()
+    } else {
+      is_admin_opt(local_user_view.as_ref()).is_err()
+    };
 
+  // Only allow mod person id filters if its not hidden
   let mod_person_id = if hide_modlog_names {
     None
   } else {
