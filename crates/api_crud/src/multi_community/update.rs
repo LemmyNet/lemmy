9bf67963789a14669cffaccb7403ebe67d5b4744
Felix Ableitner <me@nutomic.com>
2025-05-13 15:09:22 +0200

    disallow removed, deleted, private

--- crates/api_crud/src/multi_community/update.rs
+++ crates/api_crud/src/multi_community/update.rs
@@ -1,5 +1,6 @@
 use activitypub_federation::config::Data;
 use actix_web::web::Json;
+use futures::future::try_join_all;
 use lemmy_api_common::{
   community::UpdateMultiCommunity,
   context::LemmyContext,
@@ -8,10 +9,12 @@ use lemmy_api_common::{
 };
 use lemmy_db_schema::{
   impls::multi_community::ReadParams,
-  source::multi_community::MultiCommunity,
+  source::{community::Community, multi_community::MultiCommunity},
+  traits::Crud,
 };
+use lemmy_db_schema_file::enums::CommunityVisibility;
 use lemmy_db_views_local_user::LocalUserView;
-use lemmy_utils::error::LemmyResult;
+use lemmy_utils::error::{LemmyError, LemmyResult};
 
 pub async fn update_multi_community(
   data: Json<UpdateMultiCommunity>,
@@ -23,7 +26,15 @@ pub async fn update_multi_community(
   if read.multi.owner_id != local_user_view.person.id {
     return Err(LemmyErrorType::NotFound.into());
   }
-  // TODO: disallow removed/deleted communities
+  // Disallow removed/deleted communities
+  try_join_all(data.communities.iter().map(|id| async {
+    let c = Community::read(&mut context.pool(), *id).await?;
+    if c.removed || c.deleted || c.visibility != CommunityVisibility::Public {
+      return Err(LemmyErrorType::NotFound.into());
+    }
+    Ok::<_, LemmyError>(c)
+  }))
+  .await?;
   MultiCommunity::update(&mut context.pool(), data.id, data.communities.clone()).await?;
   Ok(Json(SuccessResponse::default()))
 }
