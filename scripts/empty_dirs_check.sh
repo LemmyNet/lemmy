37812fef28adee7628b537d9f8ab01f85d36da00
Dessalines <tyhou13@gmx.com>
2025-05-13 13:21:37 -0400

    Try to fix CI errors 9.

--- scripts/empty_dirs_check.sh
+++ scripts/empty_dirs_check.sh
@@ -3,5 +3,6 @@ set -e
 
 EMPTY_DIRS=$(find crates migrations api_tests/src -type d -empty)
 if [[ "$EMPTY_DIRS" ]]; then
-  logger -s "Empty dirs present:\n$EMPTY_DIRS\n"
+  echo "Empty dirs present:\n$EMPTY_DIRS\n"
+  ERR
 fi
