6b04b568f33f514ac51bdb58320d1269500d20a2
aboitsov <anton.boitsov@zalando.fi>
2025-05-15 01:36:02 +0300

    fmt fix

--- crates/api_common/src/utils.rs
+++ crates/api_common/src/utils.rs
@@ -1,11 +1,7 @@
 use crate::{
   claims::Claims,
   context::LemmyContext,
-  request::{
-    delete_image_alias,
-    fetch_pictrs_proxied_image_details,
-    purge_image_from_pictrs_url,
-  },
+  request::{delete_image_alias, fetch_pictrs_proxied_image_details, purge_image_from_pictrs_url},
   site::{FederatedInstances, InstanceWithFederationState},
 };
 use actix_web::{http::header::Header, HttpRequest};
