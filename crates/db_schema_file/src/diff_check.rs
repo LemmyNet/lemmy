d606b100f136eb14ed3145404af556e11ceddda8
Dull Bananas <dull.bananas0@gmail.com>
2025-05-19 23:02:11 -0700

    clippy

--- crates/db_schema_file/src/diff_check.rs
+++ crates/db_schema_file/src/diff_check.rs
@@ -4,11 +4,7 @@ use itertools::Itertools;
 use lemmy_utils::settings::SETTINGS;
 use std::{
   borrow::Cow,
-  collections::{
-    btree_set::{self, BTreeSet},
-    HashSet,
-  },
-  num::NonZero,
+  collections::{btree_set::BTreeSet, HashSet},
   process::{Command, Stdio},
 };
 
@@ -49,8 +45,6 @@ pub fn get_dump() -> String {
   String::from_utf8(output.stdout).expect("pg_dump output is not valid UTF-8 text")
 }
 
-const PATTERN_LEN: usize = 19;
-
 pub fn check_dump_diff(before_change: String, after_change: String, change_description: &str) {
   // Performance optimization
   if after_change == before_change {
@@ -58,12 +52,12 @@ pub fn check_dump_diff(before_change: String, after_change: String, change_descr
   }
 
   let len_of_match_at_beginning = count_bytes_until_chunks_dont_match(
-    before_change.as_bytes().into_iter(),
-    after_change.as_bytes().into_iter(),
+    before_change.as_bytes().iter(),
+    after_change.as_bytes().iter(),
   );
   let len_of_match_at_end = count_bytes_until_chunks_dont_match(
-    before_change.as_bytes().into_iter().rev(),
-    after_change.as_bytes().into_iter().rev(),
+    before_change.as_bytes().iter().rev(),
+    after_change.as_bytes().iter().rev(),
   );
   let [before_chunks, after_chunks] = [&before_change, &after_change].map(|dump| {
     dump
@@ -74,14 +68,14 @@ pub fn check_dump_diff(before_change: String, after_change: String, change_descr
       .collect::<Vec<_>>()
   });
   let diff_results = diff::slice(&before_chunks, &after_chunks);
-  dbg!(diff_results.len());
+  diff_results.len();
   let mut before_diff = BTreeSet::new();
   let mut after_diff = BTreeSet::new();
   for res in diff_results {
     let not_duplicate = match res {
       diff::Result::Both(_, _) => true,
-      diff::Result::Left(chunk) => before_diff.insert((&**chunk)),
-      diff::Result::Right(chunk) => after_diff.insert((&**chunk)),
+      diff::Result::Left(chunk) => before_diff.insert(&**chunk),
+      diff::Result::Right(chunk) => after_diff.insert(&**chunk),
     };
     assert!(
       not_duplicate,
@@ -200,18 +194,6 @@ fn match_end_aligns_with_chunk_end<
     .eq(b"\n\n")
 }
 
-trait Differences<T> {
-  fn differences(&self) -> [btree_set::Difference<'_, T>; 2];
-}
-
-impl<T: Ord> Differences<T> for [BTreeSet<T>; 2] {
-  /// Items only in `a`, and items only in `b`
-  fn differences(&self) -> [btree_set::Difference<'_, T>; 2] {
-    let [a, b] = self;
-    [a.difference(b), b.difference(a)]
-  }
-}
-
 fn chunk_difference_amount(a: &str, b: &str) -> Vec<usize> {
   // Prioritize similarity of specific parts by returning the difference amount in each
   // part, starting with higher priority
@@ -232,27 +214,6 @@ fn chunk_parts_for_pair_selection(chunk: &str) -> [&str; 3] {
     .unwrap_or(("", after_command_name));
   [command_name, remainder_of_first_line, after_first_line]
 }
-
-/*fn similarity(chunk: &str, other_chunk: &str) -> usize {
-  diff::chars(chunk, other_chunk)
-    .into_iter()
-    .filter(|i| {
-      match i {
-        diff::Result::Both(c, _) => {
-          // Prevent whitespace from affecting similarity level
-          !c.is_whitespace()
-            && (
-              // Increase accuracy for some trigger function diffs
-              c.is_lowercase()
-                  // Preserve differences in names that contain a number
-                  || c.is_numeric()
-            )
-        }
-        _ => false,
-      }
-    })
-    .count()
-}*/
 
 fn count_inserted_or_deleted_chars_in_diff(a: &str, b: &str) -> usize {
   diff::chars(a, b)
@@ -306,47 +267,6 @@ fn remove_ignored_details_from_chunk(mut chunk: &str) -> Option<Cow<'_, str>> {
     chunk = Cow::Owned(lines.join("\n"));
   }
 
-  // Replace timestamps with a constant string, so differences in timestamps are ignored
-  /*for index in 0.. {
-    // Performance optimization
-    let Some(byte) = chunk.as_bytes().get(index) else {
-      break;
-    };
-    if !byte.is_ascii_digit() {
-      continue;
-    }
-
-    // Check for this pattern: 0000-00-00 00:00:00
-    let Some((
-      &[a0, a1, a2, a3, b0, a4, a5, b1, a6, a7, b2, a8, a9, b3, a10, a11, b4, a12, a13],
-      remaining,
-    )) = chunk
-      .get(index..)
-      .and_then(|s| s.as_bytes().split_first_chunk::<PATTERN_LEN>())
-    else {
-      break;
-    };
-
-    if [a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13]
-      .into_iter()
-      .all(|byte| byte.is_ascii_digit())
-      && [b0, b1, b2, b3, b4] == *b"-- ::"
-    {
-      // Replace the part of the string that has the checked pattern and an optional fractional part
-      let len_after = if let Some((b'.', s)) = remaining.split_first() {
-        1 + s.iter().position(|c| !c.is_ascii_digit()).unwrap_or(0)
-      } else {
-        0
-      };
-      // Length of replacement string is likely to match previous string
-      // (there's up to 6 digits after the decimal point)
-      chunk.to_mut().replace_range(
-        index..(index + PATTERN_LEN + len_after),
-        "AAAAAAAAAAAAAAAAAAAAAAAAAA",
-      );
-    }
-  }*/
-
   Some(chunk)
 }
 
@@ -391,23 +311,23 @@ mod tests {
     let c = b"FOO\n\nFOO\n\nBUNNY";
     let d = b"FOO\n\nFOO\n\n";
     assert_eq!(
-      super::count_bytes_until_chunks_dont_match(a.into_iter(), b.into_iter()),
+      super::count_bytes_until_chunks_dont_match(a.iter(), b.iter()),
       d.len()
     );
     assert_eq!(
-      super::count_bytes_until_chunks_dont_match(a.into_iter(), c.into_iter()),
+      super::count_bytes_until_chunks_dont_match(a.iter(), c.iter()),
       d.len()
     );
     assert_eq!(
-      super::count_bytes_until_chunks_dont_match(a.into_iter(), a.into_iter()),
+      super::count_bytes_until_chunks_dont_match(a.iter(), a.iter()),
       a.len()
     );
     assert_eq!(
-      super::count_bytes_until_chunks_dont_match(b"z".into_iter(), b"z".into_iter()),
+      super::count_bytes_until_chunks_dont_match(b"z".iter(), b"z".iter()),
       1
     );
     assert_eq!(
-      super::count_bytes_until_chunks_dont_match(b"z".into_iter(), b"y".into_iter()),
+      super::count_bytes_until_chunks_dont_match(b"z".iter(), b"y".iter()),
       0
     );
   }
