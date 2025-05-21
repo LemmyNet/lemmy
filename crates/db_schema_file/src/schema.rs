5095092d3a6b0c194295e2cf3034d2b9abf8db54
Dessalines <dessalines@users.noreply.github.com>
2025-05-21 03:49:22 -0400

    Removing hide_modlog_mod_names setting. (#5681)
    * Simplify modlog name removals.
    
    - #5614
    
    * Removing hide_modlog_mod_names setting.
    
    * Addressing PR comments 2

--- crates/db_schema_file/src/schema.rs
+++ crates/db_schema_file/src/schema.rs
@@ -420,7 +420,6 @@ diesel::table! {
         default_theme -> Text,
         default_post_listing_type -> ListingTypeEnum,
         legal_information -> Nullable<Text>,
-        hide_modlog_mod_names -> Bool,
         application_email_admins -> Bool,
         slur_filter_regex -> Nullable<Text>,
         actor_name_max_length -> Int4,
