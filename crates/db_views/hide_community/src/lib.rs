215e5ab5106901c6c98fcc0d7e4d539bd32f846f
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2025-06-02 20:52:56 -0400

    Ensure that exporting typescript bindings works

--- crates/db_views/hide_community/src/lib.rs
+++ crates/db_views/hide_community/src/lib.rs
@@ -1,6 +1,8 @@
 use lemmy_db_schema::newtypes::CommunityId;
 use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
+#[cfg(feature = "full")]
+use ts_rs::TS;
 
 #[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone, Default, PartialEq, Eq, Hash)]
--- crates/db_views/success_response/src/lib.rs
+++ crates/db_views/success_response/src/lib.rs
@@ -5,7 +5,7 @@ use ts_rs::TS;
 mod impls;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-#[cfg_attr(feature = "full", derive(ts_rs::TS))]
+#[cfg_attr(feature = "full", derive(TS))]
 #[cfg_attr(feature = "full", ts(export))]
 /// A response that completes successfully.
 pub struct SuccessResponse {
