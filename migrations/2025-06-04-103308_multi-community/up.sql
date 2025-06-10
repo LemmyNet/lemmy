beb9e3cfa11ffc184cfcacecd3608ad1592c39ff
Felix Ableitner <me@nutomic.com>
2025-06-10 16:20:10 +0200

    add index

--- migrations/2025-06-04-103308_multi-community/up.sql
+++ migrations/2025-06-04-103308_multi-community/up.sql
@@ -83,6 +83,8 @@ CREATE INDEX idx_multi_community_ap_id ON multi_community (ap_id);
 
 CREATE INDEX idx_multi_creator_id ON multi_community (creator_id);
 
+CREATE INDEX idx_multi_community_follow_multi_id ON multi_community_follow (multi_community_id);
+
 CREATE INDEX idx_multi_followed_by ON multi_community_follow (person_id);
 
 CREATE INDEX idx_multi_community_entry_community_id ON multi_community_entry (community_id);
