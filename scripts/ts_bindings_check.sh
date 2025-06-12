262439fb5566b3265db9bd9c3602a45935370312
Dessalines <tyhou13@gmx.com>
2025-06-12 08:48:43 -0400

    Adding ts-rs feature to ts_bindings script

--- scripts/ts_bindings_check.sh
+++ scripts/ts_bindings_check.sh
@@ -8,7 +8,7 @@ CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 cd "$CWD/../"
 
 # Export the ts-rs bindings
-cargo test --workspace export_bindings
+cargo test --workspace export_bindings --features ts-rs
 
 # Make sure no rows are returned
 ! grep -nr --include=\*.ts ' | null' ./crates/
