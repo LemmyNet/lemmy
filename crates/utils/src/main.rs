06e7ea00b8adf1b60942b96173aa6acd24f8383f
Nutomic <me@nutomic.com>
2025-06-20 12:42:37 +0000

    Move config updated check to unit test, use diffy (#5803)
    * Move config updated check to test
    
    * use diffy
    
    * clippy
    
    * Fixes for "Move config updated check to unit test, use diffy" PR (#5812)
    
    * replace diffy with unified_diff in test_config_defaults_updated
    
    * revert changes to lemmy_db_schema_file
    
    * use unified_diff in diff_check
    
    * simplify
    
    ---------
    
    Co-authored-by: dullbananas <dull.bananas0@gmail.com>

--- crates/utils/src/main.rs
+++ crates/utils/src/main.rs
@@ -3,22 +3,44 @@ use cfg_if::cfg_if;
 fn main() {
   cfg_if! {
     if #[cfg(feature = "full")] {
-      use doku::json::{AutoComments, CommentsStyle, Formatting, ObjectsStyle};
-      use lemmy_utils::settings::structs::Settings;
-      let fmt = Formatting {
-        auto_comments: AutoComments::none(),
-        comments_style: CommentsStyle {
-          separator: "#".to_owned(),
-        },
-        objects_style: ObjectsStyle {
-          surround_keys_with_quotes: false,
-          use_comma_as_separator: false,
-        },
-        ..Default::default()
-      };
-      println!("{}", doku::to_json_fmt_val(&fmt, &Settings::default()));
+      println!("{}", config_to_string())
     } else {
-
     }
   }
+}
+
+#[cfg(feature = "full")]
+fn config_to_string() -> String {
+  use doku::json::{AutoComments, CommentsStyle, Formatting, ObjectsStyle};
+  use lemmy_utils::settings::structs::Settings;
+  let fmt = Formatting {
+    auto_comments: AutoComments::none(),
+    comments_style: CommentsStyle {
+      separator: "#".to_owned(),
+    },
+    objects_style: ObjectsStyle {
+      surround_keys_with_quotes: false,
+      use_comma_as_separator: false,
+    },
+    ..Default::default()
+  };
+  doku::to_json_fmt_val(&fmt, &Settings::default())
+}
+
+#[test]
+fn test_config_defaults_updated() -> lemmy_utils::error::LemmyResult<()> {
+  let current_config = std::fs::read_to_string("../../config/defaults.hjson")?;
+  let mut updated_config = config_to_string();
+  updated_config.push('\n');
+  if current_config != updated_config {
+    let diff = unified_diff::diff(
+      current_config.as_bytes(),
+      "current",
+      updated_config.as_bytes(),
+      "expected",
+      3,
+    );
+    panic!("{}", String::from_utf8_lossy(&diff));
+  }
+  Ok(())
 }
