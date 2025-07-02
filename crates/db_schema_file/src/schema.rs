bf3f040de8d8a194ea8e273d5a7841fef2a73241
Dull Bananas <dull.bananas0@gmail.com>
2025-07-02 00:26:16 -0700

    run diesel print-schema

--- crates/db_schema_file/src/schema.rs
+++ crates/db_schema_file/src/schema.rs
@@ -982,13 +982,6 @@ diesel::table! {
 }
 
 diesel::table! {
-    previously_run_sql (id) {
-        id -> Bool,
-        content -> Text,
-    }
-}
-
-diesel::table! {
     private_message (id) {
         id -> Int4,
         creator_id -> Int4,
@@ -1334,7 +1327,6 @@ diesel::allow_tables_to_appear_in_same_query!(
   post_actions,
   post_report,
   post_tag,
-  previously_run_sql,
   private_message,
   private_message_report,
   received_activity,
