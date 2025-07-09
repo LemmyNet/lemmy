fd1e0b01e5d3279b73c0e288bac7a808c6fb3aab
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2025-07-09 03:48:14 -0400

    Add missing TS derives that were causing errors when generating Typescript bindings (#5853)

--- crates/db_schema/src/source/combined/inbox.rs
+++ crates/db_schema/src/source/combined/inbox.rs
@@ -22,6 +22,8 @@ use serde_with::skip_serializing_none;
 #[cfg_attr(feature = "full", diesel(table_name = inbox_combined))]
 #[cfg_attr(feature = "full", diesel(check_for_backend(diesel::pg::Pg)))]
 #[cfg_attr(feature = "full", cursor_keys_module(name = inbox_combined_keys))]
+#[cfg_attr(feature = "ts-rs", derive(ts_rs::TS))]
+#[cfg_attr(feature = "ts-rs", ts(optional_fields, export))]
 /// A combined inbox table.
 pub struct InboxCombined {
   pub id: InboxCombinedId,
