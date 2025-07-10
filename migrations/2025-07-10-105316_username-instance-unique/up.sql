ffb1807ea9a214edff93cb369067b3cfc795e31e
Felix Ableitner <me@nutomic.com>
2025-07-10 12:56:53 +0200

    Add unique constraint for person(name, instance) (fixes #5045)

---
+++ migrations/2025-07-10-105316_username-instance-unique/up.sql
@@ -0,0 +1,9 @@
+-- lemmy requires (username + instance_id) to be unique
+DELETE FROM person p1 USING person p2
+WHERE p1.id < p2.id
+    AND p1.name = p2.name
+    AND p1.instance_id = p2.instance_id;
+
+ALTER TABLE person
+    ADD CONSTRAINT person_name_instance_unique UNIQUE (name, instance_id);
+
