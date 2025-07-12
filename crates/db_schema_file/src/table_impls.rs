7e8c94ff5941a2abd4788797b0f7970cdff4f0f8
Dull Bananas <dull.bananas0@gmail.com>
2025-07-11 23:34:00 -0700

    import table modules in table_impls.rs

--- crates/db_schema_file/src/table_impls.rs
+++ crates/db_schema_file/src/table_impls.rs
@@ -1,41 +1,35 @@
-use crate::schema;
+use crate::schema::{
+  comment_actions,
+  community_actions,
+  instance_actions,
+  person_actions,
+  post_actions,
+};
 
-impl diesel_uplete::SupportedTable for schema::comment_actions::table {
+impl diesel_uplete::SupportedTable for comment_actions::table {
+  type Key = (comment_actions::person_id, comment_actions::comment_id);
+  type AdditionalIgnoredColumns = ();
+}
+
+impl diesel_uplete::SupportedTable for community_actions::table {
   type Key = (
-    schema::comment_actions::person_id,
-    schema::comment_actions::comment_id,
+    community_actions::person_id,
+    community_actions::community_id,
   );
   type AdditionalIgnoredColumns = ();
 }
 
-impl diesel_uplete::SupportedTable for schema::community_actions::table {
-  type Key = (
-    schema::community_actions::person_id,
-    schema::community_actions::community_id,
-  );
+impl diesel_uplete::SupportedTable for instance_actions::table {
+  type Key = (instance_actions::person_id, instance_actions::instance_id);
   type AdditionalIgnoredColumns = ();
 }
 
-impl diesel_uplete::SupportedTable for schema::instance_actions::table {
-  type Key = (
-    schema::instance_actions::person_id,
-    schema::instance_actions::instance_id,
-  );
+impl diesel_uplete::SupportedTable for person_actions::table {
+  type Key = (person_actions::person_id, person_actions::target_id);
   type AdditionalIgnoredColumns = ();
 }
 
-impl diesel_uplete::SupportedTable for schema::person_actions::table {
-  type Key = (
-    schema::person_actions::person_id,
-    schema::person_actions::target_id,
-  );
-  type AdditionalIgnoredColumns = ();
-}
-
-impl diesel_uplete::SupportedTable for schema::post_actions::table {
-  type Key = (
-    schema::post_actions::person_id,
-    schema::post_actions::post_id,
-  );
+impl diesel_uplete::SupportedTable for post_actions::table {
+  type Key = (post_actions::person_id, post_actions::post_id);
   type AdditionalIgnoredColumns = ();
 }
