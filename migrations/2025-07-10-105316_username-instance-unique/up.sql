66058739f5e91a57af6750d7cc864aae6f21bcf2
Nutomic <me@nutomic.com>
2025-07-14 09:56:51 +0200

    not local
    Co-authored-by: dullbananas <dull.bananas0@gmail.com>

--- migrations/2025-07-10-105316_username-instance-unique/up.sql
+++ migrations/2025-07-10-105316_username-instance-unique/up.sql
@@ -3,7 +3,9 @@
 DELETE FROM person p1 USING person p2
 WHERE p1.id > p2.id
     AND p1.name = p2.name
-    AND p1.instance_id = p2.instance_id;
+    AND p1.instance_id = p2.instance_id
+    AND NOT (p1.local
+        OR p2.local);
 
 ALTER TABLE person
     ADD CONSTRAINT person_name_instance_unique UNIQUE (name, instance_id);
