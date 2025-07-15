68944c8491746f230efd3420516c1f327383a6be
Dessalines <tyhou13@gmx.com>
2025-07-15 15:55:56 -0400

    Fixing username-instance duplicate migration.
    Context: #5045 , #5858

--- migrations/2025-07-10-105316_username-instance-unique/up.sql
+++ migrations/2025-07-10-105316_username-instance-unique/up.sql
@@ -1,11 +1,21 @@
 -- lemmy requires (username + instance_id) to be unique
 -- delete any existing duplicates
-DELETE FROM person p1 USING person p2
-WHERE p1.id > p2.id
-    AND p1.name = p2.name
+DELETE FROM person p1 USING (
+    SELECT
+        min(id) AS id,
+        name,
+        instance_id
+    FROM
+        person
+    GROUP BY
+        name,
+        instance_id
+    HAVING
+        count(*) > 1) p2
+WHERE
+    p1.name = p2.name
     AND p1.instance_id = p2.instance_id
-    AND NOT (p1.local
-        OR p2.local);
+    AND p1.id <> p2.id;
 
 ALTER TABLE person
     ADD CONSTRAINT person_name_instance_unique UNIQUE (name, instance_id);
