70d0174f4dfffa2947c71bdd18c2e5e114d60835
Dull Bananas <dull.bananas0@gmail.com>
2025-07-19 21:31:37 -0700

    change 5000 back to 50

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -47,7 +47,7 @@ pub const CACHE_DURATION_LARGEST_COMMUNITY: Duration = Duration::from_secs(0);
 #[cfg(not(debug_assertions))]
 pub const CACHE_DURATION_LARGEST_COMMUNITY: Duration = DAY;
 
-pub const MAX_COMMENT_DEPTH_LIMIT: usize = 5000;
+pub const MAX_COMMENT_DEPTH_LIMIT: usize = 50;
 
 #[macro_export]
 macro_rules! location_info {
