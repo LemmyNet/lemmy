e2f967c8c5e3c947a4d380dd37a0c237d2d8b712
Dessalines <tyhou13@gmx.com>
2025-07-20 12:14:58 -0400

    Fast person content combined history

--- migrations/2024-12-05-233704_add_person_content_combined_table/up.sql
+++ migrations/2024-12-05-233704_add_person_content_combined_table/up.sql
@@ -1,118 +1,79 @@
 -- Creates combined tables for
 -- person_content: (comment, post)
 -- person_saved: (comment, post)
-SET session_replication_role = replica;
-
 CREATE TABLE person_content_combined (
-    id serial PRIMARY KEY,
+    id int GENERATED ALWAYS AS IDENTITY,
     published timestamptz NOT NULL,
     post_id int UNIQUE REFERENCES post ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE,
     comment_id int UNIQUE REFERENCES COMMENT ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE
 );
 
-CREATE INDEX idx_person_content_combined_published ON person_content_combined (published DESC, id DESC);
+-- Disable the triggers temporarily
+ALTER TABLE person_content_combined DISABLE TRIGGER ALL;
 
--- Updating the history
-INSERT INTO person_content_combined (published, post_id)
+INSERT INTO person_content_combined (published, post_id, comment_id)
 SELECT
     published,
-    id
+    id AS post_id,
+    NULL::int AS comment_id
 FROM
     post
-WHERE
-    published > CURRENT_DATE - interval '1 month'
-ON CONFLICT (post_id)
-    DO UPDATE SET
-        published = excluded.published;
-
--- Update history status
-INSERT INTO history_status (source, dest, last_scanned_id)
-SELECT
-    'post',
-    'person_content_combined',
-    min(id)
-FROM
-    post
-WHERE
-    published > CURRENT_DATE - interval '1 month';
-
-INSERT INTO person_content_combined (published, comment_id)
+UNION ALL
 SELECT
     published,
+    NULL::int,
     id
 FROM
-    comment
-WHERE
-    published > CURRENT_DATE - interval '1 month'
-ON CONFLICT (comment_id)
-    DO UPDATE SET
-        published = excluded.published;
+    comment;
 
--- Update history status
-INSERT INTO history_status (source, dest, last_scanned_id)
-SELECT
-    'comment',
-    'person_content_combined',
-    min(id)
-FROM
-    comment
-WHERE
-    published > CURRENT_DATE - interval '1 month';
+-- Re-enable triggers after upserts
+ALTER TABLE person_content_combined ENABLE TRIGGER ALL;
+
+-- add the primary key
+ALTER TABLE person_content_combined
+    ADD PRIMARY KEY (id);
 
 -- This one is special, because you use the saved date, not the ordinary published
 CREATE TABLE person_saved_combined (
-    id serial PRIMARY KEY,
+    id int GENERATED ALWAYS AS IDENTITY,
     saved timestamptz NOT NULL,
     person_id int NOT NULL REFERENCES person ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE,
     post_id int REFERENCES post ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE,
-    comment_id int REFERENCES COMMENT ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE,
-    UNIQUE (person_id, post_id),
-    UNIQUE (person_id, comment_id)
+    comment_id int REFERENCES COMMENT ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE
 );
 
--- Updating the history
+-- Disable the triggers temporarily
+ALTER TABLE person_saved_combined DISABLE TRIGGER ALL;
+
 -- This is for local_users only
-INSERT INTO person_saved_combined (saved, person_id, post_id)
+INSERT INTO person_saved_combined (saved, person_id, post_id, comment_id)
 SELECT
     pa.saved,
     pa.person_id,
-    pa.post_id
+    pa.post_id,
+    NULL::int AS comment_id
 FROM
     post_actions pa,
     local_user lu
 WHERE
     pa.person_id = lu.person_id
     AND pa.saved IS NOT NULL
-    AND pa.saved > CURRENT_DATE - interval '1 month'
-ON CONFLICT (person_id,
-    post_id)
-    DO UPDATE SET
-        saved = excluded.saved;
-
--- Leave the last_scanned_ids blank, since post_actions might not be filled yet.
--- You need the post_actions table to be filled entirely before filling this history.
-INSERT INTO history_status (source, dest)
-    VALUES ('post_actions', 'person_saved_combined');
-
-INSERT INTO person_saved_combined (saved, person_id, comment_id)
+UNION ALL
 SELECT
     ca.saved,
     ca.person_id,
+    NULL::int,
     ca.comment_id
 FROM
     comment_actions ca,
     local_user lu
 WHERE
     ca.person_id = lu.person_id
-    AND ca.saved IS NOT NULL
-    AND ca.saved > CURRENT_DATE - interval '1 month'
-ON CONFLICT (person_id,
-    comment_id)
-    DO UPDATE SET
-        saved = excluded.saved;
+    AND ca.saved IS NOT NULL;
 
-INSERT INTO history_status (source, dest)
-    VALUES ('comment_actions', 'person_saved_combined');
+-- add the primary key
+ALTER TABLE person_saved_combined
+    ADD PRIMARY KEY (id);
 
 CREATE INDEX idx_person_saved_combined_published ON person_saved_combined (saved DESC, id DESC);
 
@@ -122,7 +83,9 @@ ALTER TABLE person_saved_combined
     ALTER CONSTRAINT person_saved_combined_person_id_fkey NOT DEFERRABLE,
     ALTER CONSTRAINT person_saved_combined_post_id_fkey NOT DEFERRABLE,
     ALTER CONSTRAINT person_saved_combined_comment_id_fkey NOT DEFERRABLE,
-    ADD CONSTRAINT person_saved_combined_check CHECK (num_nonnulls (post_id, comment_id) = 1);
+    ADD CONSTRAINT person_saved_combined_check CHECK (num_nonnulls (post_id, comment_id) = 1),
+    ADD CONSTRAINT person_post_uniq UNIQUE (person_id, post_id),
+    ADD CONSTRAINT person_comment_uniq UNIQUE (person_id, comment_id);
 
 ALTER TABLE person_content_combined
     ALTER CONSTRAINT person_content_combined_post_id_fkey NOT DEFERRABLE,
