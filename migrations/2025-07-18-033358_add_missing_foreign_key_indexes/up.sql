40c89f99ca17961e398d8a58cf81002095722e2c
Nutomic <me@nutomic.com>
2025-07-21 15:02:51 +0000

    Fix foreign key indexes migration (#5875)

--- migrations/2025-07-18-033358_add_missing_foreign_key_indexes/up.sql
+++ migrations/2025-07-18-033358_add_missing_foreign_key_indexes/up.sql
@@ -14,8 +14,6 @@ CREATE INDEX idx_admin_purge_post_admin ON admin_purge_post (admin_person_id);
 
 CREATE INDEX idx_mod_remove_comment_comment ON mod_remove_comment (comment_id);
 
-CREATE INDEX idx_person_comment_mention_comment ON person_comment_mention (comment_id);
-
 CREATE INDEX idx_person_liked_combined_comment ON person_liked_combined (comment_id);
 
 CREATE INDEX idx_person_saved_combined_comment ON person_saved_combined (comment_id);
@@ -119,8 +117,6 @@ CREATE INDEX idx_mod_lock_post_post ON mod_lock_post (post_id);
 CREATE INDEX idx_mod_remove_post_post ON mod_remove_post (post_id);
 
 CREATE INDEX idx_person_liked_combined_post ON person_liked_combined (post_id);
-
-CREATE INDEX idx_person_post_mention_post ON person_post_mention (post_id);
 
 CREATE INDEX idx_person_saved_combined_post ON person_saved_combined (post_id);
 
