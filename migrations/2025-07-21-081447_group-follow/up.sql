b5a9a70e5ae2ac2bd4d6a66e6a6fddc162a1537a
Felix Ableitner <me@nutomic.com>
2025-07-22 11:57:38 +0200

    primary key

--- migrations/2025-07-21-081447_group-follow/up.sql
+++ migrations/2025-07-21-081447_group-follow/up.sql
@@ -2,7 +2,7 @@ CREATE TABLE community_community_follow (
     target_id int REFERENCES community (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
     community_id int REFERENCES community (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
     published_at timestamptz NOT NULL DEFAULT now(),
-    UNIQUE (community_id, target_id)
+    PRIMARY KEY (community_id, target_id)
 );
 
 CREATE INDEX idx_community_community_follow_target ON community_community_follow (target_id);
