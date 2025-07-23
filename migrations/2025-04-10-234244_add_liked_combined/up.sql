f9d9a3e21c858a4cddb6080d73742d75065099d8
Dessalines <tyhou13@gmx.com>
2025-07-23 14:03:43 -0400

    create table as for liked_combined

--- migrations/2025-04-10-234244_add_liked_combined/up.sql
+++ migrations/2025-04-10-234244_add_liked_combined/up.sql
@@ -1,20 +1,8 @@
 -- Creates combined tables for
 -- person_liked: (comment, post)
 -- This one is special, because you use the liked date, not the ordinary published
-CREATE TABLE person_liked_combined (
-    id int GENERATED ALWAYS AS IDENTITY,
-    liked timestamptz NOT NULL,
-    like_score smallint NOT NULL,
-    person_id int NOT NULL REFERENCES person ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE,
-    post_id int REFERENCES post ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE,
-    comment_id int REFERENCES COMMENT ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE
-);
-
--- Disable the triggers temporarily
-ALTER TABLE person_liked_combined DISABLE TRIGGER ALL;
-
 -- Updating the history
-INSERT INTO person_liked_combined (liked, like_score, person_id, post_id, comment_id)
+CREATE TABLE person_liked_combined AS
 SELECT
     pa.liked,
     pa.like_score,
@@ -41,21 +29,17 @@ WHERE
     liked IS NOT NULL
     AND p.local = TRUE;
 
--- Re-enable triggers after upserts
-ALTER TABLE person_liked_combined ENABLE TRIGGER ALL;
-
--- add the primary key
 ALTER TABLE person_liked_combined
-    ADD PRIMARY KEY (id);
+    ADD COLUMN id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    ALTER COLUMN liked SET NOT NULL,
+    ALTER COLUMN like_score SET NOT NULL,
+    ALTER COLUMN person_id SET NOT NULL,
+    ADD CONSTRAINT person_liked_combined_person_id_fkey FOREIGN KEY (person_id) REFERENCES person ON UPDATE CASCADE ON DELETE CASCADE,
+    ADD CONSTRAINT person_liked_combined_post_id_fkey FOREIGN KEY (post_id) REFERENCES post ON UPDATE CASCADE ON DELETE CASCADE,
+    ADD CONSTRAINT person_liked_combined_comment_id_fkey FOREIGN KEY (comment_id) REFERENCES COMMENT ON UPDATE CASCADE ON DELETE CASCADE,
+    ADD UNIQUE (person_id, post_id),
+    ADD UNIQUE (person_id, comment_id),
+    ADD CONSTRAINT person_liked_combined_check CHECK (num_nonnulls (post_id, comment_id) = 1);
 
 CREATE INDEX idx_person_liked_combined ON person_liked_combined (person_id);
-
--- Make sure only one of the columns is not null
-ALTER TABLE person_liked_combined
-    ADD CONSTRAINT person_liked_combined_check CHECK (num_nonnulls (post_id, comment_id) = 1),
-    ALTER CONSTRAINT person_liked_combined_person_id_fkey NOT DEFERRABLE,
-    ALTER CONSTRAINT person_liked_combined_post_id_fkey NOT DEFERRABLE,
-    ALTER CONSTRAINT person_liked_combined_comment_id_fkey NOT DEFERRABLE,
-    ADD CONSTRAINT person_liked_combined_person_comment_uniq UNIQUE (person_id, comment_id),
-    ADD CONSTRAINT person_liked_combined_person_post_uniq UNIQUE (person_id, post_id);
 
