da8e88118984ba7aecee52b7ac30e6e5643e9973
Dessalines <tyhou13@gmx.com>
2025-07-24 12:00:12 -0400

    Adding alias.

--- crates/db_schema/src/lib.rs
+++ crates/db_schema/src/lib.rs
@@ -34,7 +34,10 @@ pub mod utils;
 use serde::{Deserialize, Serialize};
 use strum::{Display, EnumString};
 #[cfg(feature = "full")]
-use {diesel::query_source::AliasedField, lemmy_db_schema_file::schema::person};
+use {
+  diesel::query_source::AliasedField,
+  lemmy_db_schema_file::schema::{instance_actions, person},
+};
 
 #[derive(
   EnumString, Display, Debug, Serialize, Deserialize, Clone, Copy, PartialEq, Eq, Default, Hash,
@@ -233,4 +236,15 @@ pub type Person2AliasAllColumnsTuple = (
   AliasedField<aliases::Person2, person::post_score>,
   AliasedField<aliases::Person2, person::comment_count>,
   AliasedField<aliases::Person2, person::comment_score>,
+);
+
+#[cfg(feature = "full")]
+/// A helper tuple for more my instance persons actions
+pub type MyInstancePersonsActionsAllColumnsTuple = (
+  AliasedField<aliases::MyInstancePersonsActions, instance_actions::person_id>,
+  AliasedField<aliases::MyInstancePersonsActions, instance_actions::instance_id>,
+  AliasedField<aliases::MyInstancePersonsActions, instance_actions::blocked_communities_at>,
+  AliasedField<aliases::MyInstancePersonsActions, instance_actions::received_ban_at>,
+  AliasedField<aliases::MyInstancePersonsActions, instance_actions::ban_expires_at>,
+  AliasedField<aliases::MyInstancePersonsActions, instance_actions::blocked_persons_at>,
 );
