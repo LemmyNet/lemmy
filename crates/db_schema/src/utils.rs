6534cbc22f56070bfe46544696f3422923717c1c
momentary-lapse <aboitsov@proton.me>
2025-08-13 01:43:13 +0300

    fmt

--- crates/db_schema/src/utils.rs
+++ crates/db_schema/src/utils.rs
@@ -4,7 +4,10 @@ use crate::newtypes::DbUrl;
 use chrono::TimeDelta;
 use db_pool::{
   r#async::{
-    DatabasePool, DatabasePoolBuilderTrait, DieselAsyncPostgresBackend, DieselDeadpool,
+    DatabasePool,
+    DatabasePoolBuilderTrait,
+    DieselAsyncPostgresBackend,
+    DieselDeadpool,
     ReusableConnectionPool,
   },
   PrivilegedPostgresConfig,
@@ -17,18 +20,21 @@ use diesel::{
   query_builder::{Query, QueryFragment},
   query_dsl::methods::LimitDsl,
   result::{
-    ConnectionError, ConnectionResult,
+    ConnectionError,
+    ConnectionResult,
     Error::{self as DieselError, QueryBuilderError},
   },
   sql_types::{self, Timestamptz},
-  Expression, IntoSql,
+  Expression,
+  IntoSql,
 };
 use diesel_async::{
   async_connection_wrapper::AsyncConnectionWrapper,
   pg::AsyncPgConnection,
   pooled_connection::{
     deadpool::{Hook, HookError, Object as PooledConnection, Pool},
-    AsyncDieselConnectionManager, ManagerConfig,
+    AsyncDieselConnectionManager,
+    ManagerConfig,
   },
   scoped_futures::ScopedBoxFuture,
   AsyncConnection,
@@ -42,11 +48,16 @@ use lemmy_utils::{
 };
 use rustls::{
   client::danger::{
-    DangerousClientConfigBuilder, HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier,
+    DangerousClientConfigBuilder,
+    HandshakeSignatureValid,
+    ServerCertVerified,
+    ServerCertVerifier,
   },
   crypto::{self, verify_tls12_signature, verify_tls13_signature},
   pki_types::{CertificateDer, ServerName, UnixTime},
-  ClientConfig, DigitallySignedStruct, SignatureScheme,
+  ClientConfig,
+  DigitallySignedStruct,
+  SignatureScheme,
 };
 use std::{
   ops::{Deref, DerefMut},
