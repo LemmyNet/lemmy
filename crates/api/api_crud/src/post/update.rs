8569efbbce80109b214578518993c23dd275c2fa
Felix Ableitner <me@nutomic.com>
2025-09-03 12:13:56 +0200

    Reenable plugin hook (ref #5934)

--- crates/api/api_crud/src/post/update.rs
+++ crates/api/api_crud/src/post/update.rs
@@ -6,7 +6,7 @@ use lemmy_api_utils::{
   build_response::build_post_response,
   context::LemmyContext,
   notify::NotifyData,
-  plugins::plugin_hook_after,
+  plugins::{plugin_hook_after, plugin_hook_before},
   request::generate_post_link_metadata,
   send_activity::SendActivityData,
   utils::{
@@ -136,7 +136,7 @@ pub async fn update_post(
     (_, _) => None,
   };
 
-  let post_form = PostUpdateForm {
+  let mut post_form = PostUpdateForm {
     name: data.name.clone(),
     url,
     body,
@@ -147,8 +147,7 @@ pub async fn update_post(
     scheduled_publish_time_at,
     ..Default::default()
   };
-  // TODO this is currently broken
-  // post_form = plugin_hook_before("before_update_local_post", post_form).await?;
+  post_form = plugin_hook_before("before_update_local_post", post_form).await?;
 
   let post_id = data.post_id;
   let updated_post = Post::update(&mut context.pool(), post_id, &post_form).await?;
