be5158d305d64edea511b878a7c44514426f9e6f
Nutomic <me@nutomic.com>
2025-09-04 18:16:13 +0000

    Sorting for instance list (#5946)
    * Sorting for instance list
    
    * nulls last
    
    * remove comment
    
    * fix

--- crates/db_schema/src/impls/instance.rs
+++ crates/db_schema/src/impls/instance.rs
@@ -1,5 +1,5 @@
 use crate::{
-  diesel::dsl::IntervalDsl,
+  diesel::{dsl::IntervalDsl, PgSortExpressionMethods},
   newtypes::{InstanceId, PersonId},
   source::{
     federation_queue_state::FederationQueueState,
@@ -201,6 +201,11 @@ impl Instance {
       .left_join(federation_blocklist::table)
       .left_join(federation_allowlist::table)
       .left_join(federation_queue_state::table)
+      // Show recently updated instances and those with valid metadata first
+      .order((
+        instance::updated_at.desc(),
+        instance::software.asc().nulls_last(),
+      ))
       .select((
         Self::as_select(),
         Option::<FederationQueueState>::as_select(),
