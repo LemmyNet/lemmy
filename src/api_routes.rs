7e3f4ebac533939852e71916963e191f02e4f5a1
Felix Ableitner <me@nutomic.com>
2025-09-10 12:25:47 +0200

    Remove leave_admin endpoint (fixes #5971)

--- src/api_routes.rs
+++ src/api_routes.rs
@@ -82,7 +82,6 @@ use lemmy_api::{
     admin_block_instance::admin_block_instance,
     admin_list_users::admin_list_users,
     federated_instances::get_federated_instances,
-    leave_admin::leave_admin,
     list_all_media::list_all_media,
     mod_log::get_mod_log,
     purge::{
@@ -451,7 +450,6 @@ pub fn config(cfg: &mut ServiceConfig, rate_limit: &RateLimit) {
           )
           .route("/ban", post().to(ban_from_site))
           .route("/users", get().to(admin_list_users))
-          .route("/leave", post().to(leave_admin))
           .service(
             scope("/instance")
               .route("/block", post().to(admin_block_instance))
