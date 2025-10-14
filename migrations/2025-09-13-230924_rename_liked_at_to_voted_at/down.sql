5f95e5f40909fa3aaf6b11700be91e85b9c33211
Dull Bananas <dull.bananas0@gmail.com>
2025-10-13 21:15:37 -0700

    move "voted_at" rename to "rename_timestamp_add_at" migration

--- migrations/2025-09-13-230924_rename_liked_at_to_voted_at/down.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE comment_actions RENAME COLUMN voted_at TO liked_at;
-
-ALTER TABLE post_actions RENAME COLUMN voted_at TO liked_at;
-
--- migrations/2025-09-13-230924_rename_liked_at_to_voted_at/up.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE comment_actions RENAME COLUMN liked_at TO voted_at;
-
-ALTER TABLE post_actions RENAME COLUMN liked_at TO voted_at;
-
-ALTER TABLE person_liked_combined RENAME COLUMN liked_at TO voted_at;
-
