253dbe04906eea811bb5b227cff1a712090a758a
Dessalines <tyhou13@gmx.com>
2025-12-01 13:28:32 -0500

    Make sure it fails

--- migrations/2025-08-01-000038_ap_id/up.sql
+++ migrations/2025-08-01-000038_ap_id/up.sql
@@ -1,6 +1,5 @@
-ALTER TABLE person RENAME actor_id TO ap_id;
-
-ALTER TABLE community RENAME actor_id TO ap_id;
+ALTER TABLE person RENAME actor_id TO a
+    ALTER TABLE community RENAME actor_id TO ap_id;
 
 ALTER TABLE site RENAME actor_id TO ap_id;
 
