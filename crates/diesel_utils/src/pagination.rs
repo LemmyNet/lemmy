e8c9a344e7ae4b71781c3fcaf3bc5555e29a2292
Felix Ableitner <me@nutomic.com>
2026-01-09 12:46:49 +0100

    Various clippy fixes

--- crates/diesel_utils/src/pagination.rs
+++ crates/diesel_utils/src/pagination.rs
@@ -223,6 +223,8 @@ impl<#[cfg(feature = "ts-rs")] T: ts_rs::TS, #[cfg(not(feature = "ts-rs"))] T> I
 
 /// Add prev/next cursors to query result.
 #[cfg(feature = "full")]
+// https://github.com/rust-lang/rust/issues/115590
+#[allow(clippy::multiple_bound_locations)]
 pub fn paginate_response<#[cfg(feature = "ts-rs")] T: ts_rs::TS, #[cfg(not(feature = "ts-rs"))] T>(
   data: Vec<T>,
   limit: i64,
