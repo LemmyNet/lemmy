a8da520b6db9652936f4b215a9a46f6cce9d2a6b
Dessalines <tyhou13@gmx.com>
2026-02-19 15:14:18 -0500

    Fixing

--- crates/api/api_crud/src/site/create.rs
+++ crates/api/api_crud/src/site/create.rs
@@ -68,7 +68,7 @@ pub async fn create_site(
 
   let suggested_multi_community_id =
     diesel_opt_number_update(data.suggested_multi_community_id.map(|id| id.0))
-      .map(|id| id.map(|id| T(id)));
+      .map(|id| id.map(|id| MultiCommunityId(id)));
 
   let site_form = SiteUpdateForm {
     name: Some(data.name.clone()),
