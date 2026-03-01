48e12597e10165cbc280fc51b7609c52b86a9899
Dessalines <tyhou13@gmx.com>
2026-03-01 12:00:46 -0500

    Removing platform-specific vars from Dockerfile, to use QEMU for Arm builds.
    - Fixes #6201

--- docker/test_deploy.sh
+++ docker/test_deploy.sh
@@ -6,7 +6,7 @@ export DOCKER_BUILDKIT=1
 
 # Rebuilding dev docker
 pushd ..
-sudo docker build . -f docker/Dockerfile --build-arg RUST_RELEASE_MODE=release -t "dessalines/lemmy:dev" --platform=linux/amd64 --push
+sudo docker build . -f docker/Dockerfile -t "dessalines/lemmy:dev" --platform=linux/amd64 --push
 
 # Run the playbook
 # pushd ../../../lemmy-ansible
