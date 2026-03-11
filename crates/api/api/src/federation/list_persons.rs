4468136ffb6e2bcaba19e369454bac073797b1f2
Dessalines <tyhou13@gmx.com>
2026-03-11 12:50:26 -0400

    Removing unused imports from list_persons

--- crates/api/api/src/federation/list_persons.rs
+++ crates/api/api/src/federation/list_persons.rs
@@ -1,14 +1,8 @@
-use crate::federation::fetcher::resolve_person_identifier;
 use activitypub_federation::config::Data;
 use actix_web::web::{Json, Query};
 use lemmy_api_utils::{context::LemmyContext, utils::check_private_instance};
 use lemmy_db_views_local_user::LocalUserView;
 use lemmy_db_views_person::{api::ListPersons, impls::PersonQuery};
-use lemmy_db_views_person_content_combined::{
-  ListPersonContent,
-  impls::PersonContentCombinedQuery,
-};
-use lemmy_db_views_post_comment_combined::PostCommentCombinedView;
 use lemmy_db_views_site::SiteView;
 use lemmy_diesel_utils::pagination::PagedResponse;
 use lemmy_utils::error::LemmyResult;
