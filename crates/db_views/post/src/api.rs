3767abf43f17fa0f8d1ba6f21d7f634aabacf97a
Dessalines <tyhou13@gmx.com>
2026-03-11 15:46:55 -0400

    Moving GetPost to post_view

--- crates/db_views/post/src/api.rs
+++ crates/db_views/post/src/api.rs
@@ -1,9 +1,10 @@
 use crate::PostView;
 use lemmy_db_schema::{
   PostFeatureType,
-  newtypes::{CommunityId, CommunityTagId, LanguageId, MultiCommunityId, PostId},
+  newtypes::{CommentId, CommunityId, CommunityTagId, LanguageId, MultiCommunityId, PostId},
 };
 use lemmy_db_schema_file::enums::{ListingType, PostNotificationsMode, PostSortType};
+use lemmy_db_views_community::CommunityView;
 use lemmy_diesel_utils::{dburl::DbUrl, pagination::PaginationCursor};
 use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
@@ -281,4 +282,27 @@ pub struct MarkManyPostsAsRead {
 pub struct CreatePostWarning {
   pub post_id: PostId,
   pub reason: String,
+}
+
+#[skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize, Clone, Copy, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "ts-rs", derive(ts_rs::TS))]
+#[cfg_attr(feature = "ts-rs", ts(optional_fields, export))]
+// TODO this should be made into a tagged enum
+/// Get a post. Needs either the post id, or comment_id.
+pub struct GetPost {
+  pub id: Option<PostId>,
+  pub comment_id: Option<CommentId>,
+}
+
+#[skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[cfg_attr(feature = "ts-rs", derive(ts_rs::TS))]
+#[cfg_attr(feature = "ts-rs", ts(optional_fields, export))]
+/// The post response.
+pub struct GetPostResponse {
+  pub post_view: PostView,
+  pub community_view: CommunityView,
+  /// A list of cross-posts, or other times / communities this link has been posted to.
+  pub cross_posts: Vec<PostView>,
 }
