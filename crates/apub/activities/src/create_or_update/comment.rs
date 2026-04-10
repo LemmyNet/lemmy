5a727200cfe2b330fc39a3ba62c79b887b218dc8
uOJackDu <zdu010@uottawa.ca>
2026-04-10 02:13:01 -0400

    ensure unique activity ids for comments for Create and Update

--- crates/apub/activities/src/create_or_update/comment.rs
+++ crates/apub/activities/src/create_or_update/comment.rs
@@ -55,7 +55,21 @@ impl CreateOrUpdateNote {
       .await?
       .into();
 
-    let id = generate_activity_id(kind.clone(), None, &context)?;
+    // get object_id for activity id generation
+    let ap_id = (*comment.ap_id.0).clone();
+    let object_id = match kind {
+      // for Create, use the comment's ap id
+      CreateOrUpdateType::Create => Some(&ap_id),
+      // for Update, use a timestamp to ensure each Update activity is unique
+      CreateOrUpdateType::Update => {
+        let timestamp = comment.updated_at.unwrap_or(comment.published_at); // use the latest timestamp
+        let mut seed_url = ap_id;
+        seed_url.set_fragment(Some(&timestamp.to_rfc3339()));
+        Some(&seed_url.clone())
+      }
+    };
+
+    let id = generate_activity_id(kind.clone(), object_id, &context)?;
     let note = ApubComment(comment).into_json(&context).await?;
 
     let create_or_update = CreateOrUpdateNote {
