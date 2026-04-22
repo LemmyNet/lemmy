1e0a2662b2fc002d3f89507c3d215d516ddd1d4b
Dessalines <tyhou13@gmx.com>
2026-04-22 10:40:40 -0400

    Fixing data.community.id

--- crates/db_views/comment/src/impls.rs
+++ crates/db_views/comment/src/impls.rs
@@ -993,7 +993,7 @@ mod tests {
 
     // No comments returned without auth
     let read_comment_listing = CommentQuery {
-      community_id: data.community_id,
+      community_id: data.community.id,
       ..Default::default()
     }
     .list(pool, &data.site, &data.local_site)
