29031d1b58c4ed3290ccca0b710a9e4fb8ac9831
Dessalines <tyhou13@gmx.com>
2026-04-22 10:56:06 -0400

    Fixing again

--- api_tests/src/follow.spec.ts
+++ api_tests/src/follow.spec.ts
@@ -81,7 +81,7 @@ test("Follow federated community", async () => {
 
   // check that unfollow was federated
   let communityOnBeta1 = await resolveBetaCommunity(beta);
-  expect(communityOnBeta1?.community.subscribers).toBeGreaterThan(
+  expect(communityOnBeta1?.community.subscribers).toBeGreaterThanOrEqual(
     betaCommunityInitial.community.subscribers + 1,
   );
 
