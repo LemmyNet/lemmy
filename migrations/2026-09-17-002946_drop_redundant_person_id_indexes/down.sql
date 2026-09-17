22b4764b8e828ad6b884ddd62d30cd125ddc8f30
Joseph Silva <dull.bananas0@gmail.com>
2026-09-17 13:23:09 -0700

    sql formatting

--- migrations/2026-09-17-002946_drop_redundant_person_id_indexes/down.sql
+++ migrations/2026-09-17-002946_drop_redundant_person_id_indexes/down.sql
@@ -1,1 +1,3 @@
-SELECT;
+SELECT
+;
+
--- migrations/2026-09-17-002946_drop_redundant_person_id_indexes/up.sql
+++ migrations/2026-09-17-002946_drop_redundant_person_id_indexes/up.sql
@@ -1,2 +1,3 @@
 -- These need to be `IF EXISTS` because the indexes only exist on servers that ran the old version of the smoosh-tables-together migration.
 DROP INDEX IF EXISTS idx_person_actions_person, idx_post_actions_person, idx_comment_actions_person, idx_community_actions_person, idx_instance_actions_person;
+
